vovagorodok/blidraughts

Update script after user agent format change

scripts/bump_version.js
@@ -25,7 +25,7 @@ newConfigText = newConfigText.replace(
 )
 
 newConfigText = newConfigText.replace(
-  /("AppendUserAgent"\svalue="v)([^"]+)(")/,
+  /("AppendUserAgent"\svalue="Lichobile\/)([^"]+)(")/,
   '$1' + newVersion + '$3'
 )
 

RoepStoep/lidrobile

scripts/bump_version.js
@@ -25,7 +25,7 @@ newConfigText = newConfigText.replace(
 )
 
 newConfigText = newConfigText.replace(
-  /("AppendUserAgent"\svalue="v)([^"]+)(")/,
+  /("AppendUserAgent"\svalue="Lidrobile\/)([^"]+)(")/,
   '$1' + newVersion + '$3'
 )
 