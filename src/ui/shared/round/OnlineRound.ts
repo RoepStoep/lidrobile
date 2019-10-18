vovagorodok/blidraughts

Don't call cg stop more than necessary during replay

src/ui/shared/round/OnlineRound.ts
@@ -275,6 +275,7 @@ export default class OnlineRound implements OnlineRoundInterface {
 
   public jump = (ply: number) => {
     if (ply < this.firstPly() || ply > this.lastPly()) return false
+    const wasReplaying = this.replaying()
     const isFwd = ply > this.vm.ply
     this.vm.ply = ply
     const s = this.plyStep(ply)
@@ -289,7 +290,7 @@ export default class OnlineRound implements OnlineRoundInterface {
       config.dests = gameApi.parsePossibleMoves(this.data.possibleMoves)
     }
     this.chessground.set(config)
-    if (this.replaying()) this.chessground.stop()
+    if (!wasReplaying && this.replaying()) this.chessground.stop()
     if (s.san && isFwd) {
       if (s.san.indexOf('x') !== -1) sound.throttledCapture()
       else sound.throttledMove()

RoepStoep/lidrobile

src/ui/shared/round/OnlineRound.ts
@@ -272,6 +272,7 @@ export default class OnlineRound implements OnlineRoundInterface {
   public jump = (ply: number) => {
     if (ply < this.firstPly() || ply > this.lastPly()) return false
     const plyDiff = Math.abs(ply - this.vm.ply)
+    const wasReplaying = this.replaying()
     const isFwd = ply > this.vm.ply
     this.vm.ply = ply
     const s = this.plyStep(ply)
@@ -287,7 +288,7 @@ export default class OnlineRound implements OnlineRoundInterface {
       config.captureLength = this.data.captureLength;
     }
     this.draughtsground.set(config, plyDiff > 1)
-    if (this.replaying()) this.draughtsground.stop()
+    if (!wasReplaying && this.replaying()) this.draughtsground.stop()
     if (s.san && isFwd) {
       if (s.san.indexOf('x') !== -1) sound.throttledCapture()
       else sound.throttledMove()