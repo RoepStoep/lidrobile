vovagorodok/blidraughts

WIP on new home

src/ui/home/HomeCtrl.ts
@@ -1,0 +1,141 @@
+import * as uniqBy from 'lodash/uniqBy'
+import * as Zanimo from 'zanimo'
+import socket from '../../socket'
+import redraw from '../../utils/redraw'
+import settings from '../../settings'
+import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr } from '../../xhr'
+import { hasNetwork, noop } from '../../utils'
+import { isForeground, setForeground } from '../../utils/appMode'
+import { PongMessage, TimelineEntry, DailyPuzzle, CorrespondenceSeek } from '../../lichess/interfaces'
+import { TournamentListItem } from '../../lichess/interfaces/tournament'
+import { PuzzleData } from '../../lichess/interfaces/training'
+import session from '../../session'
+import { supportedTypes as supportedTimelineTypes } from '../timeline'
+import offlinePuzzleDB from '../training/database'
+import { loadNewPuzzle } from '../training/offlineService'
+
+import { dailyPuzzle as dailyPuzzleXhr, featuredTournaments as featuredTournamentsXhr } from './homeXhr'
+
+
+export default class HomeCtrl {
+  public selectedTab: number
+
+  public corresPool?: ReadonlyArray<CorrespondenceSeek>
+  public nbConnectedPlayers?: number
+  public nbGamesInPlay?: number
+  public dailyPuzzle?: DailyPuzzle
+  public featuredTournaments?: ReadonlyArray<TournamentListItem>
+  public timeline?: ReadonlyArray<TimelineEntry>
+  public offlinePuzzle?: PuzzleData | undefined
+
+  constructor(defaultTab?: number) {
+    this.selectedTab = defaultTab || 0
+
+    if (hasNetwork()) {
+      this.init()
+    } else {
+      this.loadOfflinePuzzle()
+    }
+  }
+
+  public init = () => {
+    if (isForeground()) {
+      socket.createLobby('homeLobby', this.reloadCorresPool, {
+        redirect: socket.redirectToGame,
+        reload_seeks: this.reloadCorresPool,
+        resync: () => lobbyXhr().then(d => {
+          socket.setVersion(d.lobby.version)
+        }),
+        n: (_: never, d: PongMessage) => {
+          this.nbConnectedPlayers = d.d
+          this.nbGamesInPlay = d.r
+          // TODO self redrawing component
+          // redraw()
+        }
+      })
+
+      Promise.all([
+        dailyPuzzleXhr(),
+        featuredTournamentsXhr()
+      ])
+      .then(results => {
+        const [dailyData, featuredTournamentsData] = results
+        this.dailyPuzzle = dailyData.puzzle
+        this.featuredTournaments = featuredTournamentsData.featured
+        redraw()
+      })
+      .catch(noop)
+
+      timelineXhr()
+      .then(data => {
+        this.timeline = data.entries
+          .filter((o: TimelineEntry) => supportedTimelineTypes.indexOf(o.type) !== -1)
+          .slice(0, 10)
+        redraw()
+      })
+      .catch(noop)
+    }
+  }
+
+  public onResume = () => {
+    setForeground()
+    this.init()
+  }
+
+  public loadOfflinePuzzle = () => {
+    const user = session.get()
+    if (user) {
+      loadNewPuzzle(offlinePuzzleDB, user)
+      .then(data => {
+        this.offlinePuzzle = data
+        redraw()
+      })
+    }
+  }
+
+  public onTabChange = (i: number) => {
+    const loc = window.location.search.replace(/\?tab\=\w+$/, '')
+    try {
+      window.history.replaceState(window.history.state, '', loc + '?tab=' + i)
+    } catch (e) { console.error(e) }
+    this.selectedTab = i
+    redraw()
+  }
+
+  public cancelCorresSeek = (seekId: string) => {
+    return Zanimo(document.getElementById(seekId), 'opacity', '0', '300', 'ease-out')
+      .then(() => socket.send('cancelSeek', seekId))
+      .catch(console.log.bind(console))
+  }
+
+  public joinCorresSeek = (seekId: string) => {
+    socket.send('joinSeek', seekId)
+  }
+
+  private reloadCorresPool = () => {
+    corresSeeksXhr(false)
+    .then(d => {
+      this.corresPool = fixSeeks(d).filter(s => settings.game.supportedVariants.indexOf(s.variant.key) !== -1)
+      redraw()
+    })
+  }
+}
+
+function seekUserId(seek: CorrespondenceSeek) {
+  return seek.username.toLowerCase()
+}
+
+function fixSeeks(seeks: CorrespondenceSeek[]): CorrespondenceSeek[] {
+  console.log(seeks)
+  const userId = session.getUserId()
+  if (userId) seeks.sort((a, b) => {
+    if (seekUserId(a) === userId) return -1
+    if (seekUserId(b) === userId) return 1
+    return 0
+  })
+  return uniqBy(seeks, s => {
+    const username = seekUserId(s) === userId ? s.id : s.username
+    const key = username + s.mode + s.variant.key + s.days + s.color
+    return key
+  })
+}

src/ui/home/homeView.tsx
@@ -4,24 +4,27 @@ import { emptyFen } from '../../utils/fen'
 import { hasNetwork } from '../../utils'
 import i18n from '../../i18n'
 import session from '../../session'
+import spinner from '../../spinner'
+import { CorrespondenceSeek } from '../../lichess/interfaces'
 import * as helper from '../helper'
 import { renderTimelineEntry, timelineOnTap } from '../timeline'
 import MiniBoard from '../shared/miniBoard'
+import TabNavigation from '../shared/TabNavigation'
+import TabView from '../shared/TabView'
 import { renderQuickSetup } from '../newGameForm'
 import newGameForm from '../newGameForm'
-import { TournamentListItem } from '../../lichess/interfaces/tournament'
 import { renderTournamentList } from '../tournament/tournamentsListView'
 
-import { Ctrl } from '.'
+import HomeCtrl from './HomeCtrl'
 
-export function body(ctrl: Ctrl) {
-  const nbPlayers = i18n('nbConnectedPlayers', ctrl.nbConnectedPlayers() || '?')
-  const nbGames = i18n('nbGamesInPlay', ctrl.nbGamesInPlay() || '?')
+export function body(ctrl: HomeCtrl) {
+  const nbPlayers = i18n('nbConnectedPlayers', ctrl.nbConnectedPlayers || '?')
+  const nbGames = i18n('nbGamesInPlay', ctrl.nbGamesInPlay || '?')
 
   const playbanEndsAt = session.currentBan()
 
   if (!hasNetwork()) {
-    const puzzleData = ctrl.offlinePuzzle()
+    const puzzleData = ctrl.offlinePuzzle
     const boardConf = puzzleData ? {
       fen: puzzleData.puzzle.fen,
       orientation: puzzleData.puzzle.color,
@@ -50,14 +53,14 @@ export function body(ctrl: Ctrl) {
   return (
     <div className="native_scroller page">
       <div className="home">
+        {playbanEndsAt && ((playbanEndsAt.valueOf() - Date.now()) / 1000) > 1 ?
+          renderPlayban(playbanEndsAt) : renderLobby(ctrl)
+        }
         <div className="stats">
           <div className="numPlayers">{nbPlayers}</div>
           <div className="numGames">{nbGames}</div>
         </div>
-        {playbanEndsAt && ((playbanEndsAt.valueOf() - Date.now()) / 1000) > 1 ?
-          renderPlayban(playbanEndsAt) : renderLobby()
-        }
-        {renderFeaturedTournaments(ctrl.featuredTournaments())}
+        {renderFeaturedTournaments(ctrl)}
         {renderDailyPuzzle(ctrl)}
         {renderTimeline(ctrl)}
       </div>
@@ -65,27 +68,92 @@ export function body(ctrl: Ctrl) {
   )
 }
 
-function renderLobby() {
+function renderLobby(ctrl: HomeCtrl) {
+  const tabsContent = [
+    () => renderQuickSetup(() => newGameForm.openRealTime('custom')),
+    () => renderCorresPool(ctrl),
+  ]
+
   return h('div.homeCreate', [
-    h('h2.homeTitle', 'Quick Game'),
-    renderQuickSetup(() => newGameForm.openRealTime('custom')),
+    h(TabNavigation, {
+      buttons: [
+        {
+          label: 'Quick setup'
+        },
+        {
+          label: i18n('correspondence')
+        }
+      ],
+      selectedIndex: ctrl.selectedTab,
+      onTabChange: ctrl.onTabChange,
+      wrapperClass: 'homeSetup',
+      withBottomBorder: true,
+    }),
+    h(TabView, {
+      selectedIndex: ctrl.selectedTab,
+      contentRenderers: tabsContent,
+      onTabChange: ctrl.onTabChange,
+      className: 'setupTabView',
+      withWrapper: true,
+    }),
   ])
 }
 
-function renderFeaturedTournaments(tournaments: TournamentListItem[]) {
-  if (tournaments.length)
+function renderCorresPool(ctrl: HomeCtrl) {
+  return h('div.corresPoolWrapper.native_scroller', ctrl.corresPool ?
+    ctrl.corresPool.length ?
+      h('table.corres_seeks', [
+        h('thead', [
+          h('tr', [
+            h('th', ''),
+            h('th', 'Player'),
+            h('th', 'Rating'),
+            h('th', 'Time'),
+            h('th', 'Mode'),
+          ]),
+        ]),
+        h('tbody', ctrl.corresPool.map(s => renderSeek(ctrl, s)))
+      ]) :
+      h('div.corres_empty_seeks_list', 'Oops! Nothing here.') :
+    h('div.corres_empty_seeks_list', spinner.getVdom('monochrome')))
+}
+
+function renderSeek(ctrl: HomeCtrl, seek: CorrespondenceSeek) {
+  console.log(seek)
+  const action = seek.username.toLowerCase() === session.getUserId() ?
+    'cancelCorresSeek' :
+    'joinCorresSeek'
+
+  const icon = seek.color === '' ? 'random' :
+      seek.color === 'white' ? 'white' : 'black'
+
+  return h('tr', {
+    key: 'seek' + seek.id,
+    'id': seek.id,
+    className: 'corres_seek ' + action,
+    oncreate: helper.ontapY(() => ctrl[action](seek.id))
+  }, [
+    h('td', h('span.color-icon.' + icon)),
+    h('td', seek.username),
+    h('td', seek.rating + (seek.provisional ? '?' : '')),
+    h('td', seek.days ? i18n(seek.days === 1 ? 'oneDay' : 'nbDays', seek.days) : '∞'),
+    h('td', [h(`span[data-icon=${seek.perf.icon}]`), i18n(seek.mode === 1 ? 'rated' : 'casual')]),
+  ])
+}
+
+function renderFeaturedTournaments(ctrl: HomeCtrl) {
+  if (ctrl.featuredTournaments && ctrl.featuredTournaments.length)
     return (
       <div className="homeTournament">
-        <h2 className="homeTitle">Featured Tournaments</h2>
-        {renderTournamentList(tournaments)}
+        {renderTournamentList(ctrl.featuredTournaments)}
       </div>
     )
   else
     return null
 }
 
-function renderDailyPuzzle(ctrl: Ctrl) {
-  const puzzle = ctrl.dailyPuzzle()
+function renderDailyPuzzle(ctrl: HomeCtrl) {
+  const puzzle = ctrl.dailyPuzzle
   const boardConf = puzzle ? {
     fen: puzzle.fen,
     orientation: puzzle.color,
@@ -103,9 +171,9 @@ function renderDailyPuzzle(ctrl: Ctrl) {
   )
 }
 
-function renderTimeline(ctrl: Ctrl) {
-  const timeline = ctrl.timeline()
-  if (timeline.length === 0) return null
+function renderTimeline(ctrl: HomeCtrl) {
+  const timeline = ctrl.timeline
+  if (!timeline || timeline.length === 0) return null
 
   return (
     <section id="timeline">

RoepStoep/lidrobile

src/ui/home/HomeCtrl.ts
@@ -1,0 +1,141 @@
+import * as uniqBy from 'lodash/uniqBy'
+import * as Zanimo from 'zanimo'
+import socket from '../../socket'
+import redraw from '../../utils/redraw'
+import settings from '../../settings'
+import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr } from '../../xhr'
+import { hasNetwork, noop } from '../../utils'
+import { isForeground, setForeground } from '../../utils/appMode'
+import { PongMessage, TimelineEntry, DailyPuzzleData, CorrespondenceSeek } from '../../lidraughts/interfaces'
+import { TournamentListItem } from '../../lidraughts/interfaces/tournament'
+import { PuzzleData } from '../../lidraughts/interfaces/training'
+import session from '../../session'
+import { supportedTypes as supportedTimelineTypes } from '../timeline'
+import offlinePuzzleDB from '../training/database'
+import { loadNewPuzzle } from '../training/offlineService'
+
+import { dailyPuzzle as dailyPuzzleXhr, featuredTournaments as featuredTournamentsXhr } from './homeXhr'
+
+
+export default class HomeCtrl {
+  public selectedTab: number
+
+  public corresPool?: ReadonlyArray<CorrespondenceSeek>
+  public nbConnectedPlayers?: number
+  public nbGamesInPlay?: number
+  public dailyPuzzle?: DailyPuzzleData
+  public featuredTournaments?: ReadonlyArray<TournamentListItem>
+  public timeline?: ReadonlyArray<TimelineEntry>
+  public offlinePuzzle?: PuzzleData | undefined
+
+  constructor(defaultTab?: number) {
+    this.selectedTab = defaultTab || 0
+
+    if (hasNetwork()) {
+      this.init()
+    } else {
+      this.loadOfflinePuzzle()
+    }
+  }
+
+  public init = () => {
+    if (isForeground()) {
+      socket.createLobby('homeLobby', this.reloadCorresPool, {
+        redirect: socket.redirectToGame,
+        reload_seeks: this.reloadCorresPool,
+        resync: () => lobbyXhr().then(d => {
+          socket.setVersion(d.lobby.version)
+        }),
+        n: (_: never, d: PongMessage) => {
+          this.nbConnectedPlayers = d.d
+          this.nbGamesInPlay = d.r
+          // TODO self redrawing component
+          // redraw()
+        }
+      })
+
+      Promise.all([
+        dailyPuzzleXhr(),
+        featuredTournamentsXhr()
+      ])
+      .then(results => {
+        const [dailyData, featuredTournamentsData] = results
+        this.dailyPuzzle = dailyData
+        this.featuredTournaments = featuredTournamentsData.featured
+        redraw()
+      })
+      .catch(noop)
+
+      timelineXhr()
+      .then(data => {
+        this.timeline = data.entries
+          .filter((o: TimelineEntry) => supportedTimelineTypes.indexOf(o.type) !== -1)
+          .slice(0, 10)
+        redraw()
+      })
+      .catch(noop)
+    }
+  }
+
+  public onResume = () => {
+    setForeground()
+    this.init()
+  }
+
+  public loadOfflinePuzzle = () => {
+    const user = session.get()
+    if (user) {
+      loadNewPuzzle(offlinePuzzleDB, user, (user.prefs.puzzleVariant as VariantKey) || 'standard')
+      .then(data => {
+        this.offlinePuzzle = data
+        redraw()
+      })
+    }
+  }
+
+  public onTabChange = (i: number) => {
+    const loc = window.location.search.replace(/\?tab\=\w+$/, '')
+    try {
+      window.history.replaceState(window.history.state, '', loc + '?tab=' + i)
+    } catch (e) { console.error(e) }
+    this.selectedTab = i
+    redraw()
+  }
+
+  public cancelCorresSeek = (seekId: string) => {
+    return Zanimo(document.getElementById(seekId), 'opacity', '0', '300', 'ease-out')
+      .then(() => socket.send('cancelSeek', seekId))
+      .catch(console.log.bind(console))
+  }
+
+  public joinCorresSeek = (seekId: string) => {
+    socket.send('joinSeek', seekId)
+  }
+
+  private reloadCorresPool = () => {
+    corresSeeksXhr(false)
+    .then(d => {
+      this.corresPool = fixSeeks(d).filter(s => settings.game.supportedVariants.indexOf(s.variant.key) !== -1)
+      redraw()
+    })
+  }
+}
+
+function seekUserId(seek: CorrespondenceSeek) {
+  return seek.username.toLowerCase()
+}
+
+function fixSeeks(seeks: CorrespondenceSeek[]): CorrespondenceSeek[] {
+  console.log(seeks)
+  const userId = session.getUserId()
+  if (userId) seeks.sort((a, b) => {
+    if (seekUserId(a) === userId) return -1
+    if (seekUserId(b) === userId) return 1
+    return 0
+  })
+  return uniqBy(seeks, s => {
+    const username = seekUserId(s) === userId ? s.id : s.username
+    const key = username + s.mode + s.variant.key + s.days + s.color
+    return key
+  })
+}

src/ui/home/homeView.tsx
@@ -4,24 +4,27 @@ import { emptyFen } from '../../utils/fen'
 import { hasNetwork } from '../../utils'
 import i18n from '../../i18n'
 import session from '../../session'
+import spinner from '../../spinner'
+import { CorrespondenceSeek } from '../../lidraughts/interfaces'
 import * as helper from '../helper'
 import { renderTimelineEntry, timelineOnTap } from '../timeline'
 import MiniBoard from '../shared/miniBoard'
+import TabNavigation from '../shared/TabNavigation'
+import TabView from '../shared/TabView'
 import { renderQuickSetup } from '../newGameForm'
 import newGameForm from '../newGameForm'
-import { TournamentListItem } from '../../lidraughts/interfaces/tournament'
 import { renderTournamentList } from '../tournament/tournamentsListView'
 
-import { Ctrl } from '.'
+import HomeCtrl from './HomeCtrl'
 
-export function body(ctrl: Ctrl) {
-  const nbPlayers = i18n('nbPlayers', ctrl.nbConnectedPlayers() || '?')
-  const nbGames = i18n('nbGamesInPlay', ctrl.nbGamesInPlay() || '?')
+export function body(ctrl: HomeCtrl) {
+  const nbPlayers = i18n('nbPlayers', ctrl.nbConnectedPlayers || '?')
+  const nbGames = i18n('nbGamesInPlay', ctrl.nbGamesInPlay || '?')
 
   const playbanEndsAt = session.currentBan()
 
   if (!hasNetwork()) {
-    const puzzleData = ctrl.offlinePuzzle()
+    const puzzleData = ctrl.offlinePuzzle
     const boardConf = puzzleData ? {
       fen: puzzleData.puzzle.fen,
       variant: puzzleData.puzzle.variant.key,
@@ -51,14 +54,14 @@ export function body(ctrl: Ctrl) {
   return (
     <div className="native_scroller page">
       <div className="home">
+        {playbanEndsAt && ((playbanEndsAt.valueOf() - Date.now()) / 1000) > 1 ?
+          renderPlayban(playbanEndsAt) : renderLobby(ctrl)
+        }
         <div className="stats">
           <div className="numPlayers">{nbPlayers}</div>
           <div className="numGames">{nbGames}</div>
         </div>
-        {playbanEndsAt && ((playbanEndsAt.valueOf() - Date.now()) / 1000) > 1 ?
-          renderPlayban(playbanEndsAt) : renderLobby()
-        }
-        {renderFeaturedTournaments(ctrl.featuredTournaments())}
+        {renderFeaturedTournaments(ctrl)}
         {renderDailyPuzzle(ctrl)}
         {renderTimeline(ctrl)}
       </div>
@@ -66,27 +69,92 @@ export function body(ctrl: Ctrl) {
   )
 }
 
-function renderLobby() {
+function renderLobby(ctrl: HomeCtrl) {
+  const tabsContent = [
+    () => renderQuickSetup(() => newGameForm.openRealTime('custom')),
+    () => renderCorresPool(ctrl),
+  ]
+
   return h('div.homeCreate', [
-    h('h2.homeTitle', i18n('quickPairing')),
-    renderQuickSetup(() => newGameForm.openRealTime('custom')),
+    h(TabNavigation, {
+      buttons: [
+        {
+          label: i18n('quickPairing')
+        },
+        {
+          label: i18n('correspondence')
+        }
+      ],
+      selectedIndex: ctrl.selectedTab,
+      onTabChange: ctrl.onTabChange,
+      wrapperClass: 'homeSetup',
+      withBottomBorder: true,
+    }),
+    h(TabView, {
+      selectedIndex: ctrl.selectedTab,
+      contentRenderers: tabsContent,
+      onTabChange: ctrl.onTabChange,
+      className: 'setupTabView',
+      withWrapper: true,
+    }),
   ])
 }
 
-function renderFeaturedTournaments(tournaments: TournamentListItem[]) {
-  if (tournaments.length)
+function renderCorresPool(ctrl: HomeCtrl) {
+  return h('div.corresPoolWrapper.native_scroller', ctrl.corresPool ?
+    ctrl.corresPool.length ?
+      h('table.corres_seeks', [
+        h('thead', [
+          h('tr', [
+            h('th', ''),
+            h('th', 'Player'),
+            h('th', 'Rating'),
+            h('th', 'Time'),
+            h('th', 'Mode'),
+          ]),
+        ]),
+        h('tbody', ctrl.corresPool.map(s => renderSeek(ctrl, s)))
+      ]) :
+      h('div.corres_empty_seeks_list', 'Oops! Nothing here.') :
+    h('div.corres_empty_seeks_list', spinner.getVdom('monochrome')))
+}
+
+function renderSeek(ctrl: HomeCtrl, seek: CorrespondenceSeek) {
+  console.log(seek)
+  const action = seek.username.toLowerCase() === session.getUserId() ?
+    'cancelCorresSeek' :
+    'joinCorresSeek'
+
+  const icon = seek.color === '' ? 'random' :
+      seek.color === 'white' ? 'white' : 'black'
+
+  return h('tr', {
+    key: 'seek' + seek.id,
+    'id': seek.id,
+    className: 'corres_seek ' + action,
+    oncreate: helper.ontapY(() => ctrl[action](seek.id))
+  }, [
+    h('td', h('span.color-icon.' + icon)),
+    h('td', seek.username),
+    h('td', seek.rating + (seek.provisional ? '?' : '')),
+    h('td', seek.days ? i18n(seek.days === 1 ? 'oneDay' : 'nbDays', seek.days) : '∞'),
+    h('td', [h(`span[data-icon=${seek.perf.icon}]`), i18n(seek.mode === 1 ? 'rated' : 'casual')]),
+  ])
+}
+
+function renderFeaturedTournaments(ctrl: HomeCtrl) {
+  if (ctrl.featuredTournaments && ctrl.featuredTournaments.length)
     return (
       <div className="homeTournament">
-        <h2 className="homeTitle">{i18n('openTournaments')}</h2>
-        {renderTournamentList(tournaments)}
+        {renderTournamentList(ctrl.featuredTournaments)}
       </div>
     )
   else
     return null
 }
 
-function renderDailyPuzzle(ctrl: Ctrl) {
-  const puzzleData = ctrl.dailyPuzzle()
+function renderDailyPuzzle(ctrl: HomeCtrl) {
+  const puzzleData = ctrl.dailyPuzzle
   const puzzle = puzzleData && (puzzleData.history ? puzzleData.history : puzzleData.puzzle)
   if (puzzle && puzzleData && puzzleData.puzzle) {
     puzzle.id = puzzleData.puzzle.id;
@@ -113,9 +181,9 @@ function renderDailyPuzzle(ctrl: Ctrl) {
   )
 }
 
-function renderTimeline(ctrl: Ctrl) {
-  const timeline = ctrl.timeline()
-  if (timeline.length === 0) return null
+function renderTimeline(ctrl: HomeCtrl) {
+  const timeline = ctrl.timeline
+  if (!timeline || timeline.length === 0) return null
 
   return (
     <section id="timeline">