vovagorodok/blidraughts

Fix height on some screens

src/ui/shared/offlineRound/view.tsx
@@ -7,12 +7,13 @@ import { fixCrazySan } from '../../../utils/chessFormat'
 import { renderMaterial } from '../../shared/round/view/roundView'
 import * as helper from '../../helper'
 import CrazyPocket from '../../shared/round/crazy/CrazyPocket'
+import { Bounds } from '../../shared/Board'
 import { OfflineRoundInterface, Position, Material } from '../round'
 import settings from '../../../settings'
 import Replay from './Replay'
 import { IChessClock, IStageClock } from '../clock/interfaces'
 import { formatClockTime } from '../round/clock/clockView'
-import { autoScroll, autoScrollInline, onReplayTap, getMoveEl } from '../round/util'
+import { autoScroll, autoScrollInline, onReplayTap, getMoveEl, isReducedTableHeight } from '../round/util'
 
 let pieceNotation: boolean
 
@@ -25,7 +26,18 @@ function getChecksCount(ctrl: OfflineRoundInterface, color: Color) {
     return 0
 }
 
-export function renderAntagonist(ctrl: OfflineRoundInterface, content: Mithril.Children, material: Material, position: Position, isPortrait: boolean, otbFlip?: boolean, customPieceTheme?: string, clock?: IChessClock) {
+export function renderAntagonist(
+  ctrl: OfflineRoundInterface,
+  content: Mithril.Children,
+  material: Material,
+  position: Position,
+  isPortrait: boolean,
+  vd: helper.ViewportDim,
+  bounds: Bounds,
+  otbFlip?: boolean,
+  customPieceTheme?: string,
+  clock?: IChessClock,
+) {
   const sit = ctrl.replay.situation()
   const isCrazy = !!sit.crazyhouse
   const key = isPortrait ? position + '-portrait' : position + '-landscape'
@@ -36,9 +48,10 @@ export function renderAntagonist(ctrl: OfflineRoundInterface, content: Mithril.C
     'playTable',
     'offline',
     position,
+    isReducedTableHeight(vd, bounds) ? 'reducedHeight' : '',
     isCrazy ? 'crazy' : '',
     otbFlip !== undefined ? otbFlip ? 'mode_flip' : 'mode_facing' : '',
-    ctrl.chessground.state.turnColor === ctrl.data.player.color ? 'player_turn' : 'opponent_turn'
+    ctrl.chessground.state.turnColor === ctrl.data.player.color ? 'player_turn' : 'opponent_turn',
   ].join(' ')
 
   return (

src/ui/shared/round/view/roundView.tsx
@@ -20,7 +20,7 @@ import PlayerPopup from '../../../shared/PlayerPopup'
 import GameTitle from '../../../shared/GameTitle'
 import CountdownTimer from '../../../shared/CountdownTimer'
 import ViewOnlyBoard from '../../../shared/ViewOnlyBoard'
-import Board from '../../../shared/Board'
+import Board, { Bounds } from '../../../shared/Board'
 import popupWidget from '../../../shared/popup'
 import Clock from '../clock/clockView'
 import promotion from '../promotion'
@@ -31,7 +31,7 @@ import CrazyPocket from '../crazy/CrazyPocket'
 import { view as renderCorrespondenceClock } from '../correspondenceClock/corresClockView'
 import { renderInlineReplay, renderReplay } from './replay'
 import OnlineRound from '../OnlineRound'
-import { hasSpaceForReplay, hasSpaceForInlineReplay } from '../util'
+import { isReducedTableHeight, hasSpaceForReplay, hasSpaceForInlineReplay } from '../util'
 import { Position, Material } from '../'
 
 export default function view(ctrl: OnlineRound) {
@@ -200,11 +200,13 @@ function renderHeader(ctrl: OnlineRound) {
 }
 
 function renderContent(ctrl: OnlineRound, isPortrait: boolean) {
-  const material = ctrl.chessground.getMaterialDiff()
-  const player = renderPlayTable(ctrl, ctrl.data.player, material[ctrl.data.player.color], 'player', isPortrait)
-  const opponent = renderPlayTable(ctrl, ctrl.data.opponent, material[ctrl.data.opponent.color], 'opponent', isPortrait)
   const vd = helper.viewportDim()
   const bounds = helper.getBoardBounds(vd, isPortrait)
+
+  const material = ctrl.chessground.getMaterialDiff()
+
+  const player = renderPlayTable(ctrl, ctrl.data.player, material[ctrl.data.player.color], 'player', isPortrait, vd, bounds)
+  const opponent = renderPlayTable(ctrl, ctrl.data.opponent, material[ctrl.data.opponent.color], 'opponent', isPortrait, vd, bounds)
 
   const board = h(Board, {
     variant: ctrl.data.game.variant.key,
@@ -349,14 +351,27 @@ function renderAntagonistInfo(ctrl: OnlineRound, player: Player, material: Mater
   )
 }
 
-function renderPlayTable(ctrl: OnlineRound, player: Player, material: Material, position: Position, isPortrait: boolean) {
+function renderPlayTable(
+  ctrl: OnlineRound,
+  player: Player,
+  material: Material,
+  position: Position,
+  isPortrait: boolean,
+  vd: helper.ViewportDim,
+  bounds: Bounds,
+) {
   const runningColor = ctrl.isClockRunning() ? ctrl.data.game.player : undefined
   const step = ctrl.plyStep(ctrl.vm.ply)
   const isCrazy = !!step.crazy
   const playable = gameApi.playable(ctrl.data)
   const myTurn = gameApi.isPlayerTurn(ctrl.data)
-  const classN = 'playTable' + (isCrazy ? ' crazy' : '') +
-    (ctrl.vm.clockPosition === 'left' ? ' clockOnLeft' : '')
+
+  const classN = helper.classSet({
+    playTable: true,
+    crazy: isCrazy,
+    clockOnLeft: ctrl.vm.clockPosition === 'left',
+    reducedHeight: isReducedTableHeight(vd, bounds),
+  })
 
   return (
     <section className={classN}>

RoepStoep/lidrobile

src/ui/shared/offlineRound/view.tsx
@@ -6,11 +6,12 @@ import gameStatusApi from '../../../lidraughts/status'
 import { san2alg } from '../../../utils/draughtsFormat'
 import { renderMaterial } from '../../shared/round/view/roundView'
 import * as helper from '../../helper'
+import { Bounds } from '../../shared/Board'
 import { OfflineRoundInterface, Position, Material } from '../round'
 import Replay from './Replay'
 import { IDraughtsClock, IStageClock } from '../clock/interfaces'
 import { formatClockTime } from '../round/clock/clockView'
-import { autoScroll, autoScrollInline, onReplayTap, getMoveEl } from '../round/util'
+import { autoScroll, autoScrollInline, onReplayTap, getMoveEl, isReducedTableHeight } from '../round/util'
 
 /*function getChecksCount(ctrl: OfflineRoundInterface, color: Color) {
   const sit = ctrl.replay.situation()
@@ -21,7 +22,17 @@ import { autoScroll, autoScrollInline, onReplayTap, getMoveEl } from '../round/u
     return 0
 }*/
 
-export function renderAntagonist(ctrl: OfflineRoundInterface, content: Mithril.Children, material: Material, position: Position, isPortrait: boolean, otbFlip?: boolean, _?: string, clock?: IDraughtsClock) {
+export function renderAntagonist(
+  ctrl: OfflineRoundInterface,
+  content: Mithril.Children,
+  material: Material,
+  position: Position,
+  isPortrait: boolean,
+  vd: helper.ViewportDim,
+  bounds: Bounds,
+  otbFlip?: boolean,
+  clock?: IDraughtsClock,
+) {
   const isCrazy = false
   const key = isPortrait ? position + '-portrait' : position + '-landscape'
   const antagonist = position === 'player' ? ctrl.data.player : ctrl.data.opponent
@@ -31,9 +42,10 @@ export function renderAntagonist(ctrl: OfflineRoundInterface, content: Mithril.C
     'playTable',
     'offline',
     position,
+    isReducedTableHeight(vd, bounds) ? 'reducedHeight' : '',
     isCrazy ? 'crazy' : '',
     otbFlip !== undefined ? otbFlip ? 'mode_flip' : 'mode_facing' : '',
-    ctrl.draughtsground.state.turnColor === ctrl.data.player.color ? 'player_turn' : 'opponent_turn'
+    ctrl.draughtsground.state.turnColor === ctrl.data.player.color ? 'player_turn' : 'opponent_turn',
   ].join(' ')
 
   return (

src/ui/shared/round/view/roundView.tsx
@@ -20,7 +20,7 @@ import PlayerPopup from '../../../shared/PlayerPopup'
 import GameTitle from '../../../shared/GameTitle'
 import CountdownTimer from '../../../shared/CountdownTimer'
 import ViewOnlyBoard from '../../../shared/ViewOnlyBoard'
-import Board from '../../../shared/Board'
+import Board, { Bounds } from '../../../shared/Board'
 import popupWidget from '../../../shared/popup'
 import Clock from '../clock/clockView'
 import gameButton from './button'
@@ -30,7 +30,7 @@ import { notesView } from '../notes'
 import { view as renderCorrespondenceClock } from '../correspondenceClock/corresClockView'
 import { renderInlineReplay, renderReplay } from './replay'
 import OnlineRound from '../OnlineRound'
-import { hasSpaceForReplay, hasSpaceForInlineReplay } from '../util'
+import { isReducedTableHeight, hasSpaceForReplay, hasSpaceForInlineReplay } from '../util'
 import { Position, Material } from '../'
 import getVariant from '../../../../lidraughts/variant'
 
@@ -239,11 +239,13 @@ function renderEmptyHeader(channel?: string, onTVChannelChange?: () => void) {
 }
 
 function renderContent(ctrl: OnlineRound, isPortrait: boolean) {
-  const material = ctrl.draughtsground.getMaterialDiff()
-  const player = renderPlayTable(ctrl, ctrl.data.player, material[ctrl.data.player.color], 'player', isPortrait)
-  const opponent = renderPlayTable(ctrl, ctrl.data.opponent, material[ctrl.data.opponent.color], 'opponent', isPortrait)
   const vd = helper.viewportDim()
   const bounds = helper.getBoardBounds(vd, isPortrait)
+
+  const material = ctrl.draughtsground.getMaterialDiff()
+
+  const player = renderPlayTable(ctrl, ctrl.data.player, material[ctrl.data.player.color], 'player', isPortrait, vd, bounds)
+  const opponent = renderPlayTable(ctrl, ctrl.data.opponent, material[ctrl.data.opponent.color], 'opponent', isPortrait, vd, bounds)
 
   const board = h(Board, {
     variant: ctrl.data.game.variant.key,
@@ -391,13 +393,26 @@ function renderAntagonistInfo(ctrl: OnlineRound, player: Player, material: Mater
   )
 }
 
-function renderPlayTable(ctrl: OnlineRound, player: Player, material: Material, position: Position, isPortrait: boolean) {
+function renderPlayTable(
+  ctrl: OnlineRound,
+  player: Player,
+  material: Material,
+  position: Position,
+  isPortrait: boolean,
+  vd: helper.ViewportDim,
+  bounds: Bounds,
+) {
   const runningColor = ctrl.isClockRunning() ? ctrl.data.game.player : undefined
   const isCrazy = false
   const playable = gameApi.playable(ctrl.data)
   const myTurn = gameApi.isPlayerTurn(ctrl.data)
-  const classN = 'playTable' + (isCrazy ? ' crazy' : '') +
-    (ctrl.vm.clockPosition === 'left' ? ' clockOnLeft' : '')
+
+  const classN = helper.classSet({
+    playTable: true,
+    crazy: isCrazy,
+    clockOnLeft: ctrl.vm.clockPosition === 'left',
+    reducedHeight: isReducedTableHeight(vd, bounds),
+  })
 
   return (
     <section className={classN}>