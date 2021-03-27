vovagorodok/blidraughts

Refactor follow(er-ing) screens

src/ui/user/related/RelatedCtrl.ts
@@ -1,0 +1,82 @@
+import redraw from '../../../utils/redraw'
+import * as xhr from '../userXhr'
+import { handleXhrError } from '../../../utils'
+import socket from '../../../socket'
+import { Related } from '../../../lichess/interfaces/user'
+import { Paginator } from '../../../lichess/interfaces'
+import challengeForm from '../../challengeForm'
+
+export default class RelatedCtrl {
+  currentTab: number
+
+  isLoadingNextPage: boolean = false
+
+  followers?: readonly Related[]
+  followersPaginator?: Paginator<Related>
+
+  following?: readonly Related[]
+  followingPaginator?: Paginator<Related>
+
+  constructor(readonly userId: string, readonly defaultTab?: number) {
+    this.currentTab = defaultTab || 0
+    socket.createDefault()
+
+    this.getData(this.userId, 1)
+    .then(redraw)
+    .catch(handleXhrError)
+  }
+
+  public loadNextPage = (page: number) => {
+    this.isLoadingNextPage = true
+    this.getData(this.userId, page)
+    .then(() => {
+      this.isLoadingNextPage = false
+      redraw()
+    })
+    .catch(handleXhrError)
+    redraw()
+  }
+
+  public toggleFollowing = (obj: Related) => {
+    if (obj.relation) xhr.unfollow(obj.user).then(d => this.setNewUserState(obj, d))
+    else xhr.follow(obj.user).then(d => this.setNewUserState(obj, d))
+  }
+
+  public challenge = (id: string) => {
+    challengeForm.open(id)
+  }
+
+  public onTabChange = (tabIndex: number) => {
+    const loc = window.location.search.replace(/\?tab\=\w+$/, '')
+    try {
+      window.history.replaceState(window.history.state, '', loc + '?tab=' + tabIndex)
+    } catch (e) { console.error(e) }
+    this.currentTab = tabIndex
+    if ((this.currentTab === 0 && !this.followers) ||
+      (this.currentTab === 1 && !this.following)) {
+      this.getData(this.userId, 1)
+      .then(redraw)
+    }
+
+    redraw()
+  }
+
+  private getData(userId: string, page: number): Promise<void> {
+    return this.currentTab === 0 ?
+      xhr.followers(userId, page)
+      .then(d => {
+        this.followers = (this.followers || []).concat(d.paginator.currentPageResults)
+        this.followersPaginator = d.paginator
+      }) :
+      xhr.following(userId, page)
+      .then(d => {
+        this.following = (this.following || []).concat(d.paginator.currentPageResults)
+        this.followingPaginator = d.paginator
+      })
+  }
+
+  private setNewUserState(obj: Related, newData: xhr.RelationActionResult) {
+    obj.relation = newData.following
+    redraw()
+  }
+}

src/ui/user/related/relatedView.tsx
@@ -1,33 +1,66 @@
-import router from '../../router'
-import { gameIcon } from '../../utils'
-import i18n from '../../i18n'
-import spinner from '../../spinner'
-import { Related } from '../../lichess/interfaces/user'
-import * as helper from '../helper'
-import { IRelationCtrl } from './interfaces'
+import * as h from 'mithril/hyperscript'
+import router from '../../../router'
+import { gameIcon } from '../../../utils'
+import i18n from '../../../i18n'
+import spinner from '../../../spinner'
+import { Related } from '../../../lichess/interfaces/user'
+import { Paginator } from '../../../lichess/interfaces'
+import * as helper from '../../helper'
+import TabNavigation from '../../shared/TabNavigation'
+import TabView from '../../shared/TabView'
 
-export function renderBody(ctrl: IRelationCtrl) {
-  const rel = ctrl.related()
+import RelatedCtrl from './RelatedCtrl'
 
-  if (!rel) {
+export function renderBody(ctrl: RelatedCtrl) {
+  const tabsContent = [
+    () => renderContent(ctrl, ctrl.followers, ctrl.followersPaginator),
+    () => renderContent(ctrl, ctrl.following, ctrl.followingPaginator),
+  ]
+
+  return [
+    h('div.tabs-nav-header.subHeader',
+      h(TabNavigation, {
+        buttons: [
+          { label: i18n('nbFollowers', ctrl.followersPaginator ? ctrl.followersPaginator.nbResults : '') },
+          { label: i18n('nbFollowing', ctrl.followingPaginator ? ctrl.followingPaginator.nbResults : '') },
+        ],
+        selectedIndex: ctrl.currentTab,
+        onTabChange: ctrl.onTabChange
+      }),
+    ),
+    h(TabView, {
+      selectedIndex: ctrl.currentTab,
+      contentRenderers: tabsContent,
+      onTabChange: ctrl.onTabChange,
+      withWrapper: true,
+    })
+  ]
+}
+
+function renderContent(
+  ctrl: RelatedCtrl,
+  content?: readonly Related[],
+  paginator?: Paginator<Related>
+) {
+
+  if (!content) {
     return (
       <div className="followingListEmpty">
         {spinner.getVdom('monochrome')}
       </div>
     )
   }
-  else if (rel.length) {
-    const paginator = ctrl.paginator()
+  else if (content.length) {
     const nextPage = paginator && paginator.nextPage
     return (
       <ul className="native_scroller page">
-        {rel.map((p, i) => renderPlayer(ctrl, p, i))}
+        {content.map((p, i) => renderPlayer(ctrl, p, i))}
         {nextPage ?
           <li
             className="list_item followingList moreFollow"
             oncreate={helper.ontapY(() => ctrl.loadNextPage(nextPage))}
           >
-          {ctrl.isLoadingNextPage() ? spinner.getVdom('monochrome') : '...'}
+          {ctrl.isLoadingNextPage ? spinner.getVdom('monochrome') : '...'}
           </li> : null
         }
       </ul>
@@ -41,7 +74,7 @@ export function renderBody(ctrl: IRelationCtrl) {
   }
 }
 
-function renderPlayer(ctrl: IRelationCtrl, obj: Related, i: number) {
+function renderPlayer(ctrl: RelatedCtrl, obj: Related, i: number) {
   const status = obj.online ? 'online' : 'offline'
   const perfKey = obj.perfs && Object.keys(obj.perfs)[0] as PerfKey
   const perf = obj.perfs && obj.perfs[perfKey]

RoepStoep/lidrobile

src/ui/user/related/RelatedCtrl.ts
@@ -1,0 +1,82 @@
+import redraw from '../../../utils/redraw'
+import * as xhr from '../userXhr'
+import { handleXhrError } from '../../../utils'
+import socket from '../../../socket'
+import { Related } from '../../../lidraughts/interfaces/user'
+import { Paginator } from '../../../lidraughts/interfaces'
+import challengeForm from '../../challengeForm'
+
+export default class RelatedCtrl {
+  currentTab: number
+
+  isLoadingNextPage: boolean = false
+
+  followers?: readonly Related[]
+  followersPaginator?: Paginator<Related>
+
+  following?: readonly Related[]
+  followingPaginator?: Paginator<Related>
+
+  constructor(readonly userId: string, readonly defaultTab?: number) {
+    this.currentTab = defaultTab || 0
+    socket.createDefault()
+
+    this.getData(this.userId, 1)
+    .then(redraw)
+    .catch(handleXhrError)
+  }
+
+  public loadNextPage = (page: number) => {
+    this.isLoadingNextPage = true
+    this.getData(this.userId, page)
+    .then(() => {
+      this.isLoadingNextPage = false
+      redraw()
+    })
+    .catch(handleXhrError)
+    redraw()
+  }
+
+  public toggleFollowing = (obj: Related) => {
+    if (obj.relation) xhr.unfollow(obj.user).then(d => this.setNewUserState(obj, d))
+    else xhr.follow(obj.user).then(d => this.setNewUserState(obj, d))
+  }
+
+  public challenge = (id: string) => {
+    challengeForm.open(id)
+  }
+
+  public onTabChange = (tabIndex: number) => {
+    const loc = window.location.search.replace(/\?tab\=\w+$/, '')
+    try {
+      window.history.replaceState(window.history.state, '', loc + '?tab=' + tabIndex)
+    } catch (e) { console.error(e) }
+    this.currentTab = tabIndex
+    if ((this.currentTab === 0 && !this.followers) ||
+      (this.currentTab === 1 && !this.following)) {
+      this.getData(this.userId, 1)
+      .then(redraw)
+    }
+
+    redraw()
+  }
+
+  private getData(userId: string, page: number): Promise<void> {
+    return this.currentTab === 0 ?
+      xhr.followers(userId, page)
+      .then(d => {
+        this.followers = (this.followers || []).concat(d.paginator.currentPageResults)
+        this.followersPaginator = d.paginator
+      }) :
+      xhr.following(userId, page)
+      .then(d => {
+        this.following = (this.following || []).concat(d.paginator.currentPageResults)
+        this.followingPaginator = d.paginator
+      })
+  }
+
+  private setNewUserState(obj: Related, newData: xhr.RelationActionResult) {
+    obj.relation = newData.following
+    redraw()
+  }
+}

src/ui/user/related/relatedView.tsx
@@ -1,33 +1,66 @@
-import router from '../../router'
-import { gameIcon } from '../../utils'
-import i18n from '../../i18n'
-import spinner from '../../spinner'
-import { Related } from '../../lidraughts/interfaces/user'
-import * as helper from '../helper'
-import { IRelationCtrl } from './interfaces'
+import * as h from 'mithril/hyperscript'
+import router from '../../../router'
+import { gameIcon } from '../../../utils'
+import i18n from '../../../i18n'
+import spinner from '../../../spinner'
+import { Related } from '../../../lidraughts/interfaces/user'
+import { Paginator } from '../../../lidraughts/interfaces'
+import * as helper from '../../helper'
+import TabNavigation from '../../shared/TabNavigation'
+import TabView from '../../shared/TabView'
 
-export function renderBody(ctrl: IRelationCtrl) {
-  const rel = ctrl.related()
+import RelatedCtrl from './RelatedCtrl'
 
-  if (!rel) {
+export function renderBody(ctrl: RelatedCtrl) {
+  const tabsContent = [
+    () => renderContent(ctrl, ctrl.followers, ctrl.followersPaginator),
+    () => renderContent(ctrl, ctrl.following, ctrl.followingPaginator),
+  ]
+
+  return [
+    h('div.tabs-nav-header.subHeader',
+      h(TabNavigation, {
+        buttons: [
+          { label: i18n('nbFollowers', ctrl.followersPaginator ? ctrl.followersPaginator.nbResults : '') },
+          { label: i18n('nbFollowing', ctrl.followingPaginator ? ctrl.followingPaginator.nbResults : '') },
+        ],
+        selectedIndex: ctrl.currentTab,
+        onTabChange: ctrl.onTabChange
+      }),
+    ),
+    h(TabView, {
+      selectedIndex: ctrl.currentTab,
+      contentRenderers: tabsContent,
+      onTabChange: ctrl.onTabChange,
+      withWrapper: true,
+    })
+  ]
+}
+
+function renderContent(
+  ctrl: RelatedCtrl,
+  content?: readonly Related[],
+  paginator?: Paginator<Related>
+) {
+
+  if (!content) {
     return (
       <div className="followingListEmpty">
         {spinner.getVdom('monochrome')}
       </div>
     )
   }
-  else if (rel.length) {
-    const paginator = ctrl.paginator()
+  else if (content.length) {
     const nextPage = paginator && paginator.nextPage
     return (
       <ul className="native_scroller page">
-        {rel.map((p, i) => renderPlayer(ctrl, p, i))}
+        {content.map((p, i) => renderPlayer(ctrl, p, i))}
         {nextPage ?
           <li
             className="list_item followingList moreFollow"
             oncreate={helper.ontapY(() => ctrl.loadNextPage(nextPage))}
           >
-          {ctrl.isLoadingNextPage() ? spinner.getVdom('monochrome') : '...'}
+          {ctrl.isLoadingNextPage ? spinner.getVdom('monochrome') : '...'}
           </li> : null
         }
       </ul>
@@ -41,13 +74,11 @@ export function renderBody(ctrl: IRelationCtrl) {
   }
 }
 
-function renderPlayer(ctrl: IRelationCtrl, obj: Related, i: number) {
+function renderPlayer(ctrl: RelatedCtrl, obj: Related, i: number) {
   const status = obj.online ? 'online' : 'offline'
   const perfKey = obj.perfs && Object.keys(obj.perfs)[0] as PerfKey
   const perf = obj.perfs && obj.perfs[perfKey]
   const userLink = helper.ontapY(() => router.set(`/@/${obj.user}`))
-  const title64 = obj.title && obj.title.endsWith('-64'),
-    titleClass = 'userTitle' + (obj.title == 'BOT' ? ' titleBot' : (title64 ? ' title64' : ''));
   const evenOrOdd = i % 2 === 0 ? 'even' : 'odd'
   return (
     <li className={`list_item followingList ${evenOrOdd}`}>
@@ -57,7 +88,7 @@ function renderPlayer(ctrl: IRelationCtrl, obj: Related, i: number) {
             <span className={'patron userStatus ' + status} data-icon="" /> :
             <span className={'fa fa-circle userStatus ' + status} />
           }
-          {obj.title ? <span className={titleClass}>{title64 ? obj.title.slice(0, obj.title.length - 3) : obj.title}&nbsp;</span> : null}
+          {obj.title ? <span className="userTitle">{obj.title}&nbsp;</span> : null}
           {obj.user}
         </div>
         { perfKey ?