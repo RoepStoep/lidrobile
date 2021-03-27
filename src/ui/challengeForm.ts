vovagorodok/blidraughts

One screen for players and leaderboard

src/ui/challengeForm.ts
@@ -1,18 +1,20 @@
+import * as h from 'mithril/hyperscript'
+import * as stream from 'mithril/stream'
 import * as utils from '../utils'
 import router from '../router'
 import redraw from '../utils/redraw'
+import i18n from '../i18n'
+import challengesApi from '../lichess/challenges'
 import { challenge as challengeXhr } from '../xhr'
 import { validateFen } from '../utils/fen'
 import settings from '../settings'
 import session from '../session'
 import formWidgets from './shared/form'
 import popupWidget from './shared/popup'
-import i18n from '../i18n'
-import storage from '../storage'
 import ViewOnlyBoard from './shared/ViewOnlyBoard'
+import gamesMenu from './gamesMenu'
 import * as helper from './helper'
-import * as h from 'mithril/hyperscript'
-import * as stream from 'mithril/stream'
+
 
 let actionName = ''
 let userId: string | undefined
@@ -49,13 +51,8 @@ function doChallenge() {
       data.challenge.timeControl.type === 'correspondence' ||
       data.challenge.timeControl.type === 'unlimited')) {
 
-      if (!storage.get('donotshowpersistentchallengeexplanation')) {
-        window.navigator.notification.alert(i18n('persistentChallengeCreated'), function() {
-          storage.set('donotshowpersistentchallengeexplanation', true)
-        })
-      }
-      // TODO put in game menu
-      router.set('/correspondence?tab=1')
+      // see gamesMenu.ts, sending challenges are right after incoming challenges
+      gamesMenu.open(challengesApi.incoming().length + 2)
     }
     if (!data.challenge.destUser || data.challenge.timeControl.type === 'clock') {
       router.set(`/challenge/${data.challenge.id}`)

src/ui/players/PlayersCtrl.ts
@@ -1,92 +1,94 @@
-import * as stream from 'mithril/stream'
 import * as throttle from 'lodash/throttle'
 import redraw from '../../utils/redraw'
 import * as utils from '../../utils'
 import router from '../../router'
-import { User } from '../../lichess/interfaces/user'
+import { User, Rankings } from '../../lichess/interfaces/user'
 import * as xhr from './playerXhr'
 
-export interface IPlayersCtrl {
-  players: Mithril.Stream<Array<User>>
-  isSearchOpen: Mithril.Stream<boolean>
-  searchResults: Mithril.Stream<string[]>
-  onInput: (e: Event) => void
-  closeSearch: (fbb?: string) => void
-  goSearch(): void
-  goToProfile(u: string): void
-  onKeyboardShow(e: Ionic.KeyboardEvent): void,
-  onKeyboardHide(): void
-  unload(): void
-}
+export default class PlayersCtrl {
+  public currentTab: number
 
-export default function PlayersCtrl(): IPlayersCtrl {
+  public isSearchOpen: boolean = false
+  public searchResults: readonly string[] = []
+  public players?: readonly User[]
+  public leaderboard?: Rankings
 
-  const isSearchOpen = stream(false)
-  const searchResults: Mithril.Stream<string[]> = stream([])
-  const players: Mithril.Stream<User[]> = stream([])
-  let listHeight: number
+  private listHeight: number = 0
 
-  function onKeyboardShow(e: Ionic.KeyboardEvent) {
+  constructor(defaultTab?: number) {
+    this.currentTab = defaultTab || 0
+
+    window.addEventListener('native.keyboardshow', this.onKeyboardShow)
+    window.addEventListener('native.keyboardhide', this.onKeyboardHide)
+
+    xhr.onlinePlayers()
+    .then(data => {
+      this.players = data
+      redraw()
+    })
+    .catch(utils.handleXhrError)
+
+    xhr.ranking()
+    .then(data => {
+      this.leaderboard = data
+      redraw()
+    })
+    .catch(utils.handleXhrError)
+  }
+
+  public onTabChange = (tabIndex: number) => {
+    const loc = window.location.search.replace(/\?tab\=\w+$/, '')
+    try {
+      window.history.replaceState(window.history.state, '', loc + '?tab=' + tabIndex)
+    } catch (e) { console.error(e) }
+    this.currentTab = tabIndex
+    redraw()
+  }
+
+  public onKeyboardShow = (e: Ionic.KeyboardEvent) => {
     if (window.cordova.platformId === 'ios') {
       let ul = document.getElementById('players_search_results')
       if (ul) {
-        listHeight = ul.offsetHeight
-        ul.style.height = (listHeight - e.keyboardHeight) + 'px'
+        this.listHeight = ul.offsetHeight
+        ul.style.height = (this.listHeight - e.keyboardHeight) + 'px'
       }
     }
   }
 
-  function onKeyboardHide() {
+  public onKeyboardHide = () => {
     if (window.cordova.platformId === 'ios') {
       let ul = document.getElementById('players_search_results')
-      if (ul) ul.style.height = listHeight + 'px'
+      if (ul) ul.style.height = this.listHeight + 'px'
     }
     let input = document.getElementById('searchPlayers')
     if (input) input.blur()
   }
 
-  function closeSearch(fromBB?: string) {
-    if (fromBB !== 'backbutton' && isSearchOpen()) router.backbutton.stack.pop()
-      isSearchOpen(false)
+  public closeSearch = (fromBB?: string) => {
+    if (fromBB !== 'backbutton' && this.isSearchOpen) router.backbutton.stack.pop()
+      this.isSearchOpen = false
   }
 
-  function unload() {
-    window.removeEventListener('native.keyboardshow', onKeyboardShow)
-    window.removeEventListener('native.keyboardhide', onKeyboardHide)
+  public unload = () => {
+    window.removeEventListener('native.keyboardshow', this.onKeyboardShow)
+    window.removeEventListener('native.keyboardhide', this.onKeyboardHide)
   }
 
-  window.addEventListener('native.keyboardshow', onKeyboardShow)
-  window.addEventListener('native.keyboardhide', onKeyboardHide)
+  public onInput = throttle((e: Event) => {
+    const term = (e.target as HTMLInputElement).value.trim()
+    if (term.length >= 3)
+      xhr.autocomplete(term).then(data => {
+        this.searchResults = data
+        redraw()
+      })
+  }, 250)
 
-  xhr.onlinePlayers()
-  .then(data => {
-    players(data)
-    redraw()
-  })
-  .catch(utils.handleXhrError)
+  public goSearch = () => {
+    router.backbutton.stack.push(this.closeSearch)
+    this.isSearchOpen = true
+  }
 
-  return {
-    players,
-    isSearchOpen,
-    searchResults,
-    onInput: throttle((e: Event) => {
-      const term = (e.target as HTMLInputElement).value.trim()
-      if (term.length >= 3)
-        xhr.autocomplete(term).then(data => {
-          searchResults(data)
-          redraw()
-        })
-    }, 250),
-    closeSearch,
-    goSearch() {
-      router.backbutton.stack.push(closeSearch)
-      isSearchOpen(true)
-    },
-    goToProfile(u) {
-      router.set('/@/' + u)
-    },
-    onKeyboardShow,
-    onKeyboardHide,
-    unload
+  public goToProfile = (u: string) => {
+    router.set('/@/' + u)
   }
 }

src/ui/players/playersView.tsx
@@ -1,3 +1,4 @@
+import * as h from 'mithril/hyperscript'
 import * as utils from '../../utils'
 import router from '../../router'
 import * as helper from '../helper'
@@ -5,11 +6,15 @@ import { menuButton, friendsButton, userStatus } from '../shared/common'
 import { backArrow } from '../shared/icons'
 import settings from '../../settings'
 import i18n from '../../i18n'
-import { User } from '../../lichess/interfaces/user'
+import spinner from '../../spinner'
+import { perfTitle } from '../../lichess/perfs'
+import { User, RankingKey, RankingUser, Rankings } from '../../lichess/interfaces/user'
+import TabNavigation from '../shared/TabNavigation'
+import TabView from '../shared/TabView'
 
-import { IPlayersCtrl } from './PlayersCtrl'
+import PlayersCtrl from './PlayersCtrl'
 
-export function header(ctrl: IPlayersCtrl) {
+export function header(ctrl: PlayersCtrl) {
   return [
     <nav>
       {menuButton()}
@@ -23,18 +28,34 @@ export function header(ctrl: IPlayersCtrl) {
   ]
 }
 
-export function body(ctrl: IPlayersCtrl) {
-  return (
-    <ul className="playersSuggestion native_scroller page"
-      oncreate={helper.ontapY(onPlayerTap, undefined, helper.getLI)}
-    >
-      {ctrl.players().map(renderPlayer)}
-    </ul>
-  )
+export function body(ctrl: PlayersCtrl) {
+  const tabsContent = [
+    () => renderLeaderboard(ctrl),
+    () => onlinePlayers(ctrl),
+  ]
+
+  return [
+    h('div.tabs-nav-header.subHeader',
+      h(TabNavigation, {
+        buttons: [
+          { label: i18n('leaderboard') },
+          { label: i18n('onlinePlayers') },
+        ],
+        selectedIndex: ctrl.currentTab,
+        onTabChange: ctrl.onTabChange
+      }),
+    ),
+    h(TabView, {
+      selectedIndex: ctrl.currentTab,
+      contentRenderers: tabsContent,
+      onTabChange: ctrl.onTabChange,
+      withWrapper: true,
+    })
+  ]
 }
 
-export function searchModal(ctrl: IPlayersCtrl) {
-  if (!ctrl.isSearchOpen())
+export function searchModal(ctrl: PlayersCtrl) {
+  if (!ctrl.isSearchOpen)
     return null
 
   const className = [
@@ -59,7 +80,7 @@ export function searchModal(ctrl: IPlayersCtrl) {
         </div>
       </header>
       <ul id="playersSearchResults" className="modal_content native_scroller">
-      {ctrl.searchResults().map(u => {
+      {ctrl.searchResults.map(u => {
         return (
           <li className="list_item nav" key={u} oncreate={helper.ontapY(() => ctrl.goToProfile(u))}>
           {u}
@@ -69,6 +90,18 @@ export function searchModal(ctrl: IPlayersCtrl) {
       </ul>
     </div>
   )
+}
+
+function onlinePlayers(ctrl: PlayersCtrl) {
+  return ctrl.players ?
+    <ul className="playersSuggestion native_scroller page"
+      oncreate={helper.ontapY(onPlayerTap, undefined, helper.getLI)}
+    >
+      {ctrl.players.map(renderPlayer)}
+    </ul> :
+    <div className="loader_container">
+      {spinner.getVdom('monochrome')}
+    </div>
 }
 
 function onPlayerTap(e: Event) {
@@ -93,6 +126,50 @@ function renderPlayer(user: User) {
       {userStatus(user)}
       <span className="rating" data-icon={utils.gameIcon(perf)}>
         {user.perfs[perf].rating}
+      </span>
+    </li>
+  )
+}
+
+function renderLeaderboard(ctrl: PlayersCtrl) {
+  const leaderboard = ctrl.leaderboard
+  if (!leaderboard) return (
+    <div className="loader_container">
+      {spinner.getVdom('monochrome')}
+    </div>
+  )
+
+  const keys = Object.keys(leaderboard) as readonly RankingKey[]
+  const categories = keys
+    .filter(k => k !== 'online')
+    .map((k: PerfKey) => renderRankingCategory(leaderboard, k))
+  return (
+    <div className="native_scroller page">
+      {categories}
+    </div>
+  )
+}
+
+function renderRankingCategory(ranking: Rankings, key: PerfKey) {
+  return (
+    <section className={'ranking ' + key}>
+      <h3 className="leaderboard_title">
+      <span className="perfIcon" data-icon={utils.gameIcon(key)} />
+        {perfTitle(key)}
+      </h3>
+      <ul className="leaderboard">
+      {ranking[key].map((p: RankingUser) => renderRankingPlayer(p, key))}
+      </ul>
+    </section>
+  )
+}
+
+function renderRankingPlayer(user: RankingUser, key: RankingKey) {
+  return (
+    <li className="leaderboard_player" oncreate={helper.ontapY(() => router.set('/@/' + user.id))}>
+      {userStatus(user)}
+      <span className="rating">
+        {user.perfs[key].rating}
       </span>
     </li>
   )

RoepStoep/lidrobile

src/ui/challengeForm.ts
@@ -1,6 +1,10 @@
+import * as h from 'mithril/hyperscript'
+import * as stream from 'mithril/stream'
 import * as utils from '../utils'
 import router from '../router'
 import redraw from '../utils/redraw'
+import i18n from '../i18n'
+import challengesApi from '../lidraughts/challenges'
 import { challenge as challengeXhr } from '../xhr'
 import { validateFen } from '../utils/fen'
 import { getVariantKeyById } from '../lidraughts/variant'
@@ -8,12 +12,9 @@ import settings from '../settings'
 import session from '../session'
 import formWidgets from './shared/form'
 import popupWidget from './shared/popup'
-import i18n from '../i18n'
-import storage from '../storage'
 import ViewOnlyBoard from './shared/ViewOnlyBoard'
+import gamesMenu from './gamesMenu'
 import * as helper from './helper'
-import * as h from 'mithril/hyperscript'
-import * as stream from 'mithril/stream'
 import { toggleCoordinates } from '../draughtsground/fen'
 import { fenFromTag } from '../utils/draughtsFormat'
 
@@ -52,13 +53,8 @@ function doChallenge() {
       data.challenge.timeControl.type === 'correspondence' ||
       data.challenge.timeControl.type === 'unlimited')) {
 
-      if (!storage.get('donotshowpersistentchallengeexplanation')) {
-        window.navigator.notification.alert(i18n('persistentChallengeCreated'), function() {
-          storage.set('donotshowpersistentchallengeexplanation', true)
-        })
-      }
-      // TODO put in game menu
-      router.set('/correspondence?tab=1')
+      // see gamesMenu.ts, sending challenges are right after incoming challenges
+      gamesMenu.open(challengesApi.incoming().length + 2)
     }
     if (!data.challenge.destUser || data.challenge.timeControl.type === 'clock') {
       router.set(`/challenge/${data.challenge.id}`)

src/ui/players/PlayersCtrl.ts
@@ -1,92 +1,94 @@
-import * as stream from 'mithril/stream'
 import * as throttle from 'lodash/throttle'
 import redraw from '../../utils/redraw'
 import * as utils from '../../utils'
 import router from '../../router'
-import { User } from '../../lidraughts/interfaces/user'
+import { User, Rankings } from '../../lidraughts/interfaces/user'
 import * as xhr from './playerXhr'
 
-export interface IPlayersCtrl {
-  players: Mithril.Stream<Array<User>>
-  isSearchOpen: Mithril.Stream<boolean>
-  searchResults: Mithril.Stream<string[]>
-  onInput: (e: Event) => void
-  closeSearch: (fbb?: string) => void
-  goSearch(): void
-  goToProfile(u: string): void
-  onKeyboardShow(e: Ionic.KeyboardEvent): void,
-  onKeyboardHide(): void
-  unload(): void
-}
+export default class PlayersCtrl {
+  public currentTab: number
 
-export default function PlayersCtrl(): IPlayersCtrl {
+  public isSearchOpen: boolean = false
+  public searchResults: readonly string[] = []
+  public players?: readonly User[]
+  public leaderboard?: Rankings
 
-  const isSearchOpen = stream(false)
-  const searchResults: Mithril.Stream<string[]> = stream([])
-  const players: Mithril.Stream<User[]> = stream([])
-  let listHeight: number
+  private listHeight: number = 0
 
-  function onKeyboardShow(e: Ionic.KeyboardEvent) {
+  constructor(defaultTab?: number) {
+    this.currentTab = defaultTab || 0
+
+    window.addEventListener('native.keyboardshow', this.onKeyboardShow)
+    window.addEventListener('native.keyboardhide', this.onKeyboardHide)
+
+    xhr.onlinePlayers()
+    .then(data => {
+      this.players = data
+      redraw()
+    })
+    .catch(utils.handleXhrError)
+
+    xhr.ranking()
+    .then(data => {
+      this.leaderboard = data
+      redraw()
+    })
+    .catch(utils.handleXhrError)
+  }
+
+  public onTabChange = (tabIndex: number) => {
+    const loc = window.location.search.replace(/\?tab\=\w+$/, '')
+    try {
+      window.history.replaceState(window.history.state, '', loc + '?tab=' + tabIndex)
+    } catch (e) { console.error(e) }
+    this.currentTab = tabIndex
+    redraw()
+  }
+
+  public onKeyboardShow = (e: Ionic.KeyboardEvent) => {
     if (window.cordova.platformId === 'ios') {
       let ul = document.getElementById('players_search_results')
       if (ul) {
-        listHeight = ul.offsetHeight
-        ul.style.height = (listHeight - e.keyboardHeight) + 'px'
+        this.listHeight = ul.offsetHeight
+        ul.style.height = (this.listHeight - e.keyboardHeight) + 'px'
       }
     }
   }
 
-  function onKeyboardHide() {
+  public onKeyboardHide = () => {
     if (window.cordova.platformId === 'ios') {
       let ul = document.getElementById('players_search_results')
-      if (ul) ul.style.height = listHeight + 'px'
+      if (ul) ul.style.height = this.listHeight + 'px'
     }
     let input = document.getElementById('searchPlayers')
     if (input) input.blur()
   }
 
-  function closeSearch(fromBB?: string) {
-    if (fromBB !== 'backbutton' && isSearchOpen()) router.backbutton.stack.pop()
-      isSearchOpen(false)
+  public closeSearch = (fromBB?: string) => {
+    if (fromBB !== 'backbutton' && this.isSearchOpen) router.backbutton.stack.pop()
+      this.isSearchOpen = false
   }
 
-  function unload() {
-    window.removeEventListener('native.keyboardshow', onKeyboardShow)
-    window.removeEventListener('native.keyboardhide', onKeyboardHide)
+  public unload = () => {
+    window.removeEventListener('native.keyboardshow', this.onKeyboardShow)
+    window.removeEventListener('native.keyboardhide', this.onKeyboardHide)
   }
 
-  window.addEventListener('native.keyboardshow', onKeyboardShow)
-  window.addEventListener('native.keyboardhide', onKeyboardHide)
+  public onInput = throttle((e: Event) => {
+    const term = (e.target as HTMLInputElement).value.trim()
+    if (term.length >= 3)
+      xhr.autocomplete(term).then(data => {
+        this.searchResults = data
+        redraw()
+      })
+  }, 250)
 
-  xhr.onlinePlayers()
-  .then(data => {
-    players(data)
-    redraw()
-  })
-  .catch(utils.handleXhrError)
+  public goSearch = () => {
+    router.backbutton.stack.push(this.closeSearch)
+    this.isSearchOpen = true
+  }
 
-  return {
-    players,
-    isSearchOpen,
-    searchResults,
-    onInput: throttle((e: Event) => {
-      const term = (e.target as HTMLInputElement).value.trim()
-      if (term.length >= 3)
-        xhr.autocomplete(term).then(data => {
-          searchResults(data)
-          redraw()
-        })
-    }, 250),
-    closeSearch,
-    goSearch() {
-      router.backbutton.stack.push(closeSearch)
-      isSearchOpen(true)
-    },
-    goToProfile(u) {
-      router.set('/@/' + u)
-    },
-    onKeyboardShow,
-    onKeyboardHide,
-    unload
+  public goToProfile = (u: string) => {
+    router.set('/@/' + u)
   }
 }

src/ui/players/playersView.tsx
@@ -1,3 +1,4 @@
+import * as h from 'mithril/hyperscript'
 import * as utils from '../../utils'
 import router from '../../router'
 import * as helper from '../helper'
@@ -5,11 +6,16 @@ import { menuButton, friendsButton, userStatus } from '../shared/common'
 import { backArrow } from '../shared/icons'
 import settings from '../../settings'
 import i18n from '../../i18n'
-import { User } from '../../lidraughts/interfaces/user'
+import spinner from '../../spinner'
+import { perfTitle } from '../../lidraughts/perfs'
+import { User, RankingKey, RankingUser, Rankings } from '../../lidraughts/interfaces/user'
+import TabNavigation from '../shared/TabNavigation'
+import TabView from '../shared/TabView'
+import { isSupportedPerf } from '../../lidraughts/game'
 
-import { IPlayersCtrl } from './PlayersCtrl'
+import PlayersCtrl from './PlayersCtrl'
 
-export function header(ctrl: IPlayersCtrl) {
+export function header(ctrl: PlayersCtrl) {
   return [
     <nav>
       {menuButton()}
@@ -23,18 +29,34 @@ export function header(ctrl: IPlayersCtrl) {
   ]
 }
 
-export function body(ctrl: IPlayersCtrl) {
-  return (
-    <ul className="playersSuggestion native_scroller page"
-      oncreate={helper.ontapY(onPlayerTap, undefined, helper.getLI)}
-    >
-      {ctrl.players().map(renderPlayer)}
-    </ul>
-  )
+export function body(ctrl: PlayersCtrl) {
+  const tabsContent = [
+    () => renderLeaderboard(ctrl),
+    () => onlinePlayers(ctrl),
+  ]
+
+  return [
+    h('div.tabs-nav-header.subHeader',
+      h(TabNavigation, {
+        buttons: [
+          { label: i18n('leaderboard') },
+          { label: i18n('onlinePlayers') },
+        ],
+        selectedIndex: ctrl.currentTab,
+        onTabChange: ctrl.onTabChange
+      }),
+    ),
+    h(TabView, {
+      selectedIndex: ctrl.currentTab,
+      contentRenderers: tabsContent,
+      onTabChange: ctrl.onTabChange,
+      withWrapper: true,
+    })
+  ]
 }
 
-export function searchModal(ctrl: IPlayersCtrl) {
-  if (!ctrl.isSearchOpen())
+export function searchModal(ctrl: PlayersCtrl) {
+  if (!ctrl.isSearchOpen)
     return null
 
   const className = [
@@ -59,7 +81,7 @@ export function searchModal(ctrl: IPlayersCtrl) {
         </div>
       </header>
       <ul id="playersSearchResults" className="modal_content native_scroller">
-      {ctrl.searchResults().map(u => {
+      {ctrl.searchResults.map(u => {
         return (
           <li className="list_item nav" key={u} oncreate={helper.ontapY(() => ctrl.goToProfile(u))}>
           {u}
@@ -69,6 +91,18 @@ export function searchModal(ctrl: IPlayersCtrl) {
       </ul>
     </div>
   )
+}
+
+function onlinePlayers(ctrl: PlayersCtrl) {
+  return ctrl.players ?
+    <ul className="playersSuggestion native_scroller page"
+      oncreate={helper.ontapY(onPlayerTap, undefined, helper.getLI)}
+    >
+      {ctrl.players.map(renderPlayer)}
+    </ul> :
+    <div className="loader_container">
+      {spinner.getVdom('monochrome')}
+    </div>
 }
 
 function onPlayerTap(e: Event) {
@@ -103,6 +137,50 @@ function renderPlayer(user: User) {
           {perf.rating}
           {perf.prov ? '?' : ''}
         </span> : null }
+    </li>
+  )
+}
+
+function renderLeaderboard(ctrl: PlayersCtrl) {
+  const leaderboard = ctrl.leaderboard
+  if (!leaderboard) return (
+    <div className="loader_container">
+      {spinner.getVdom('monochrome')}
+    </div>
+  )
+
+  const keys = Object.keys(leaderboard) as readonly RankingKey[]
+  const categories = keys
+    .filter(k => k !== 'online' && isSupportedPerf(k))
+    .map((k: PerfKey) => renderRankingCategory(leaderboard, k))
+  return (
+    <div className="native_scroller page">
+      {categories}
+    </div>
+  )
+}
+
+function renderRankingCategory(ranking: Rankings, key: PerfKey) {
+  return (
+    <section className={'ranking ' + key}>
+      <h3 className="leaderboard_title">
+      <span className="perfIcon" data-icon={utils.gameIcon(key)} />
+        {perfTitle(key)}
+      </h3>
+      <ul className="leaderboard">
+      {ranking[key].map((p: RankingUser) => renderRankingPlayer(p, key))}
+      </ul>
+    </section>
+  )
+}
+
+function renderRankingPlayer(user: RankingUser, key: RankingKey) {
+  return (
+    <li className="leaderboard_player" oncreate={helper.ontapY(() => router.set('/@/' + user.id))}>
+      {userStatus(user)}
+      <span className="rating">
+        {user.perfs[key].rating}
+      </span>
     </li>
   )
 }