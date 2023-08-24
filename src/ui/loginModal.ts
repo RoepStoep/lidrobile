vovagorodok/blidraughts

WIP on web app with capacitor

src/ui/loginModal.ts
@@ -1,3 +1,4 @@
+import { Plugins } from '@capacitor/core'
 import * as h from 'mithril/hyperscript'
 import session from '../session'
 import { ErrorResponse } from '../http'
@@ -87,7 +88,7 @@ export default {
           i18n('forgotPassword') + ' ',
           h('br'),
           h('a', {
-            oncreate: helper.ontap(() => window.open(`https://lichess.org/password/reset`, '_blank', 'location=no'))
+            oncreate: helper.ontap(() => Plugins.Browser.open({ url: `https://lichess.org/password/reset` }))
           }, [i18n('passwordReset')])
         ])
       ])
@@ -101,11 +102,11 @@ function submit(form: HTMLFormElement) {
   const token = form['token'] ? form['token'].value : null
   if (!username || !password) return
   redraw()
-  window.Keyboard.hide()
+  Plugins.Keyboard.hide()
   session.login(username, password, token)
   .then(() => {
     close()
-    window.plugins.toast.show(i18n('loginSuccessful'), 'short', 'center')
+    Plugins.Toast.show({ text: i18n('loginSuccessful'), duration: 'short' })
     signals.afterLogin.dispatch()
     redraw()
     // reconnect socket to refresh friends...
@@ -140,7 +141,7 @@ function open() {
 }
 
 function close(fromBB?: string) {
-  window.Keyboard.hide()
+  Plugins.Keyboard.hide()
   if (fromBB !== 'backbutton' && isOpen) router.backbutton.stack.pop()
   isOpen = false
 }

src/ui/shared/round/view/button.tsx
@@ -1,3 +1,4 @@
+import { Plugins } from '@capacitor/core'
 import * as h from 'mithril/hyperscript'
 import * as throttle from 'lodash/throttle'
 import { handleXhrError, hasNetwork } from '../../../../utils'
@@ -34,14 +35,14 @@ export default {
     return h('button', {
       key: 'shareGameLink',
       oncreate: helper.ontap(() => {
-        window.plugins.socialsharing.share(null, null, null, gameApi.publicUrl(ctrl.data))
+        Plugins.Share.share({ url: gameApi.publicUrl(ctrl.data) })
       })
     }, [i18n('shareGameURL')])
   },
   sharePGN(ctrl: OnlineRound) {
     function handler() {
       getPGN(ctrl.data.game.id)
-      .then((PGN: string) => window.plugins.socialsharing.share(PGN))
+      .then((PGN: string) => Plugins.Share.share({ text: PGN }))
       .catch(handleXhrError)
     }
     return (
@@ -315,7 +316,7 @@ export default {
       <button className="action_bar_button fa fa-pencil" key="notes"
         oncreate={helper.ontap(
           () => ctrl.notes && ctrl.notes.open(),
-          () => window.plugins.toast.show(i18n('notes'), 'short', 'bottom')
+          () => Plugins.Toast.show({ text: i18n('notes'), duration: 'short' })
         )} />
     )
   },

RoepStoep/lidrobile

src/ui/loginModal.ts
@@ -1,3 +1,4 @@
+import { Plugins } from '@capacitor/core'
 import * as h from 'mithril/hyperscript'
 import session from '../session'
 import { ErrorResponse } from '../http'
@@ -87,7 +88,7 @@ export default {
           i18n('forgotPassword') + ' ',
           h('br'),
           h('a', {
-            oncreate: helper.ontap(() => window.open(`https://lidraughts.org/password/reset`, '_blank', 'location=no'))
+            oncreate: helper.ontap(() => Plugins.Browser.open({ url: `https://lidraughts.org/password/reset` }))
           }, [i18n('passwordReset')])
         ])
       ])
@@ -101,11 +102,11 @@ function submit(form: HTMLFormElement) {
   const token = form['token'] ? form['token'].value : null
   if (!username || !password) return
   redraw()
-  window.Keyboard.hide()
+  Plugins.Keyboard.hide()
   session.login(username, password, token)
   .then(() => {
     close()
-    window.plugins.toast.show(i18n('loginSuccessful'), 'short', 'center')
+    Plugins.Toast.show({ text: i18n('loginSuccessful'), duration: 'short' })
     signals.afterLogin.dispatch()
     redraw()
     // reconnect socket to refresh friends...
@@ -140,7 +141,7 @@ function open() {
 }
 
 function close(fromBB?: string) {
-  window.Keyboard.hide()
+  Plugins.Keyboard.hide()
   if (fromBB !== 'backbutton' && isOpen) router.backbutton.stack.pop()
   isOpen = false
 }

src/ui/shared/round/view/button.tsx
@@ -1,3 +1,4 @@
+import { Plugins } from '@capacitor/core'
 import * as h from 'mithril/hyperscript'
 import * as throttle from 'lodash/throttle'
 import { handleXhrError, hasNetwork } from '../../../../utils'
@@ -34,15 +35,15 @@ export default {
     return h('button', {
       key: 'shareGameLink',
       oncreate: helper.ontap(() => {
-        window.plugins.socialsharing.share(null, null, null, gameApi.publicUrl(ctrl.data))
+        Plugins.Share.share({ url: gameApi.publicUrl(ctrl.data) })
       })
     }, [i18n('shareGameURL')])
   },
   sharePDN(ctrl: OnlineRound) {
     function handler() {
       getPDN(ctrl.data.game.id, ctrl.isAlgebraic())
-      .then((pdn: string) => window.plugins.socialsharing.share(pdn))
-      .catch(handleXhrError)
+        .then((pdn: string) => Plugins.Share.share({ text: pdn }))
+        .catch(handleXhrError)
     }
     return (
       <button key="sharePDN" oncreate={helper.ontap(handler)}>
@@ -315,7 +316,7 @@ export default {
       <button className="action_bar_button fa fa-pencil" key="notes"
         oncreate={helper.ontap(
           () => ctrl.notes && ctrl.notes.open(),
-          () => window.plugins.toast.show(i18n('notes'), 'short', 'bottom')
+          () => Plugins.Toast.show({ text: i18n('notes'), duration: 'short' })
         )} />
     )
   },