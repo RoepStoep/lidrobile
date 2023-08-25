vovagorodok/blidraughts

Implement code splitting

src/ui/editor/menu.ts
@@ -4,11 +4,11 @@ import popupWidget from '../shared/popup'
 import router from '../../router'
 import * as helper from '../helper'
 import h from 'mithril/hyperscript'
-import Editor, { MenuInterface } from './Editor'
+import EditorCtrl, { MenuInterface } from './EditorCtrl'
 
 export default {
 
-  controller: function(root: Editor) {
+  controller: function(root: EditorCtrl) {
     let isOpen = false
 
     function open() {
@@ -42,14 +42,14 @@ export default {
   }
 }
 
-function renderEditorMenu(ctrl: Editor) {
+function renderEditorMenu(ctrl: EditorCtrl) {
   return h('div.editorMenu', [
     renderSelectColorPosition(ctrl),
     renderCastlingOptions(ctrl)
   ])
 }
 
-export function renderSelectColorPosition(ctrl: Editor) {
+export function renderSelectColorPosition(ctrl: EditorCtrl) {
   const fen = ctrl.computeFen()
   return h('div.editorSelectors', [
     h('div.select_input', [
@@ -116,7 +116,7 @@ export function renderSelectColorPosition(ctrl: Editor) {
   ])
 }
 
-function renderCastlingOptions(ctrl: Editor) {
+function renderCastlingOptions(ctrl: EditorCtrl) {
   const white = [
     ['K', i18n('whiteCastlingKingside')],
     ['Q', i18n('whiteCastlingQueenside')],
@@ -133,7 +133,7 @@ function renderCastlingOptions(ctrl: Editor) {
   ])
 }
 
-function castlingButton(ctrl: Editor, c: string[]) {
+function castlingButton(ctrl: EditorCtrl, c: string[]) {
   const cur = ctrl.data.editor.castles[c[0]]
   return h('span', {
     className: cur() ? 'selected' : '',

src/ui/game.ts
@@ -1,26 +1,26 @@
 import * as Mithril from 'mithril'
 import { Plugins } from '@capacitor/core'
-import router from '../../router'
-import { hasNetwork, handleXhrError } from '../../utils'
-import { positionsCache } from '../../utils/gamePosition'
-import { getOfflineGameData, saveOfflineGameData, removeOfflineGameData } from '../../utils/offlineGames'
-import { game as gameXhr } from '../../xhr'
-import storage from '../../storage'
-import * as sleepUtils from '../../utils/sleep'
-import OnlineRound from '../shared/round/OnlineRound'
-import * as helper from '../helper'
-import * as gameApi from '../../lichess/game'
-import variantApi from '../../lichess/variant'
-import { OnlineGameData } from '../../lichess/interfaces/game'
-import sound from '../../sound'
-import vibrate from '../../vibrate'
-import i18n from '../../i18n'
-import socket from '../../socket'
-import { emptyFen } from '../../utils/fen'
-import roundView, { viewOnlyBoardContent } from '../shared/round/view/roundView'
-import gamesMenu from '../gamesMenu'
-import layout from '../layout'
-import { connectingHeader, loadingBackbutton } from '../shared/common'
+import router from '../router'
+import { hasNetwork, handleXhrError } from '../utils'
+import { positionsCache } from '../utils/gamePosition'
+import { getOfflineGameData, saveOfflineGameData, removeOfflineGameData } from '../utils/offlineGames'
+import { game as gameXhr } from '../xhr'
+import storage from '../storage'
+import * as sleepUtils from '../utils/sleep'
+import * as gameApi from '../lichess/game'
+import variantApi from '../lichess/variant'
+import { OnlineGameData } from '../lichess/interfaces/game'
+import sound from '../sound'
+import vibrate from '../vibrate'
+import i18n from '../i18n'
+import socket from '../socket'
+import { emptyFen } from '../utils/fen'
+import * as helper from './helper'
+import OnlineRound from './shared/round/OnlineRound'
+import roundView, { viewOnlyBoardContent } from './shared/round/view/roundView'
+import gamesMenu from './gamesMenu'
+import layout from './layout'
+import { connectingHeader, loadingBackbutton } from './shared/common'
 
 interface Attrs {
   id: string

RoepStoep/lidrobile

src/ui/editor/menu.ts
@@ -3,14 +3,14 @@ import i18n from '../../i18n'
 import popupWidget from '../shared/popup'
 import router from '../../router'
 import h from 'mithril/hyperscript'
-import Editor, { MenuInterface } from './Editor'
+import EditorCtrl, { MenuInterface } from './EditorCtrl'
 import { getInitialFen } from '../../lidraughts/variant'
 import redraw from '../../utils/redraw'
 import { fenCompare } from '../../utils/draughtsFormat'
 
 export default {
 
-  controller: function(root: Editor) {
+  controller: function(root: EditorCtrl) {
     let isOpen = false
 
     function open() {
@@ -44,13 +44,13 @@ export default {
   }
 }
 
-function renderEditorMenu(ctrl: Editor) {
+function renderEditorMenu(ctrl: EditorCtrl) {
   return h('div.editorMenu', [
     renderPositionSettings(ctrl)
   ])
 }
 
-export function renderPositionSettings(ctrl: Editor) {
+export function renderPositionSettings(ctrl: EditorCtrl) {
   const fen = ctrl.computeFen(false)
   return h('div.editorSelectors', [
     h('div.select_input', [

src/ui/game.ts
@@ -1,26 +1,26 @@
 import * as Mithril from 'mithril'
 import { Plugins } from '@capacitor/core'
-import router from '../../router'
-import { hasNetwork, handleXhrError } from '../../utils'
-import { positionsCache } from '../../utils/gamePosition'
-import { getOfflineGameData, saveOfflineGameData, removeOfflineGameData } from '../../utils/offlineGames'
-import { game as gameXhr } from '../../xhr'
-import storage from '../../storage'
-import * as sleepUtils from '../../utils/sleep'
-import OnlineRound from '../shared/round/OnlineRound'
-import * as helper from '../helper'
-import * as gameApi from '../../lidraughts/game'
-import variantApi from '../../lidraughts/variant'
-import { OnlineGameData } from '../../lidraughts/interfaces/game'
-import sound from '../../sound'
-import vibrate from '../../vibrate'
-import i18n from '../../i18n'
-import socket from '../../socket'
-import { emptyFen } from '../../utils/fen'
-import roundView, { viewOnlyBoardContent } from '../shared/round/view/roundView'
-import gamesMenu from '../gamesMenu'
-import layout from '../layout'
-import { connectingHeader, loadingBackbutton } from '../shared/common'
+import router from '../router'
+import { hasNetwork, handleXhrError } from '../utils'
+import { positionsCache } from '../utils/gamePosition'
+import { getOfflineGameData, saveOfflineGameData, removeOfflineGameData } from '../utils/offlineGames'
+import { game as gameXhr } from '../xhr'
+import storage from '../storage'
+import * as sleepUtils from '../utils/sleep'
+import * as gameApi from '../lidraughts/game'
+import variantApi from '../lidraughts/variant'
+import { OnlineGameData } from '../lidraughts/interfaces/game'
+import sound from '../sound'
+import vibrate from '../vibrate'
+import i18n from '../i18n'
+import socket from '../socket'
+import { emptyFen } from '../utils/fen'
+import * as helper from './helper'
+import OnlineRound from './shared/round/OnlineRound'
+import roundView, { viewOnlyBoardContent } from './shared/round/view/roundView'
+import gamesMenu from './gamesMenu'
+import layout from './layout'
+import { connectingHeader, loadingBackbutton } from './shared/common'
 
 interface Attrs {
   id: string