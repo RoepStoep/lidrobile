vovagorodok/blidraughts

Don't use in-app browser

src/ui/loginModal.ts
@@ -82,7 +82,7 @@ export default {
             oncreate: helper.ontap(signupModal.open)
           }, [i18n('signUp')]),
           h('a', {
-            oncreate: helper.ontap(() => Plugins.Browser.open({ url: `https://lichess.org/password/reset` }))
+            href: 'https://lichess.org/password/reset'
           }, [i18n('passwordReset')])
         ]),
       ])

src/ui/signupModal.ts
@@ -68,7 +68,7 @@ function renderForm() {
       'By registering, you agree to be bound by our ',
       h('a', {
         oncreate: helper.ontap(() =>
-          Plugins.Browser.open({ url: 'https://lichess.org/terms-of-service' })
+          window.open('https://lichess.org/terms-of-service', '_blank')
         )},
         'Terms of Service'
       ), '.'

RoepStoep/lidrobile

src/ui/loginModal.ts
@@ -82,7 +82,7 @@ export default {
             oncreate: helper.ontap(signupModal.open)
           }, [i18n('signUp')]),
           h('a', {
-            oncreate: helper.ontap(() => Plugins.Browser.open({ url: `https://lidraughts.org/password/reset` }))
+            href: 'https://lidraughts.org/password/reset'
           }, [i18n('passwordReset')])
         ]),
       ])

src/ui/signupModal.ts
@@ -68,7 +68,7 @@ function renderForm() {
       'By registering, you agree to be bound by our ',
       h('a', {
         oncreate: helper.ontap(() =>
-          Plugins.Browser.open({ url: 'https://lidraughts.org/terms-of-service' })
+          window.open('https://lidraughts.org/terms-of-service', '_blank')
         )},
         'Terms of Service'
       ), '.'