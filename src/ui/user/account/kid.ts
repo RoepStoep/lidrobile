vovagorodok/blidraughts

More translations and fixes

src/ui/user/account/kid.ts
@@ -1,5 +1,6 @@
 import * as Mithril from 'mithril'
 import h from 'mithril/hyperscript'
+import i18n from '../../../i18n'
 import session from '../../../session'
 import { StoredProp } from '../../../storage'
 import formWidgets from '../../shared/form'
@@ -19,7 +20,7 @@ const KidPrefScreen: Mithril.Component<{}, State> = {
   },
 
   view() {
-    const header = dropShadowHeader(null, backButton('Kid mode'))
+    const header = dropShadowHeader(null, backButton(i18n('kidMode')))
     return layout.free(header, renderBody(this))
   }
 }
@@ -29,13 +30,9 @@ export default KidPrefScreen
 function renderBody(ctrl: State) {
   return [
     h('div.native_scroller.page.settings_list.game', [
-      h('p.explanation', 'This is about safety. In kid mode, all site communications are disabled.  Enable this for your children and school students, to protect them from other Internet users.'),
-      h('p.list_item', [
-        'In kid mode, the logo above username in the menu is the ', h('span.kiddo', '😊'), ' icon.',
-        h('br'),
-        'So you know that it\'s enabled and your kids are safe.'
-      ]),
-      h('p.list_item', formWidgets.renderCheckbox('Enable kid mode', 'kidMode', ctrl.kidMode))
+      h('p.explanation', i18n('kidModeExplanation')),
+      h('p.list_item', h.trust(i18n('inKidModeTheLichessLogoGetsIconX', '<span class="kiddo">😊</span>'))),
+      h('p.list_item', formWidgets.renderCheckbox(i18n('enableKidMode'), 'kidMode', ctrl.kidMode))
     ])
   ]
 }

src/ui/user/userView.tsx
@@ -8,11 +8,11 @@ import { linkify } from '../../utils/html'
 import { perfTypes, provisionalDeviation } from '../../lichess/perfs'
 import { Perf } from '../../lichess/interfaces/user'
 import * as xhr from '../../xhr'
-import i18n, { formatDate, formatDuration, fromNow } from '../../i18n'
+import i18n, { plural, formatDate, formatDuration, fromNow } from '../../i18n'
 import countries from '../../utils/countries'
 import * as helper from '../helper'
 import session from '../../session'
-import { IUserCtrl, ProfileUser, isSessionUser, isFullUser } from './UserCtrl'
+import { IUserCtrl, ProfileUser, isFullUser } from './UserCtrl'
 
 export function header(user: ProfileUser, ctrl: IUserCtrl) {
   const title = userTitle(user.online!!, user.patron!!, user.username, user.title)
@@ -108,7 +108,7 @@ function renderProfile(user: ProfileUser) {
           </p>
           <p>{memberSince}</p>
           {user.seenAt ?
-          <p>Active <small>{fromNow(new Date(user.seenAt))}</small></p> : null
+          <p>{h.trust(i18n('lastSeenActive', `<small>${fromNow(new Date(user.seenAt))}</small>`))}</p> : null
           }
         </div>
       </section>
@@ -125,7 +125,7 @@ function renderWebsiteLinks(ctrl: IUserCtrl, user: ProfileUser) {
           <a className="external_link"
             oncreate={helper.ontapY(() => xhr.openWebsiteAuthPage(`/account/profile`))}
           >
-            Edit profile
+            {i18n('editProfile')}
           </a>
         </p> :
         <p>
@@ -150,20 +150,18 @@ function renderWebsiteLinks(ctrl: IUserCtrl, user: ProfileUser) {
 }
 
 function renderStats(user: ProfileUser) {
-  let totalPlayTime: string | null = null
   let tvTime: string | null = null
 
+  const totalPlayTime = user.playTime ? i18n('tpTimeSpentPlaying', formatDuration(user.playTime.total)) : null
+
   if (isFullUser(user)) {
-    totalPlayTime = user.playTime ? 'Time spent playing: ' + formatDuration(user.playTime.total) : null
-    tvTime = user.playTime && user.playTime.tv > 0 ? 'Time on TV: ' + formatDuration(user.playTime.tv) : null
-  } else if (isSessionUser(user)) {
-    totalPlayTime = user.playTime ? 'Time spent playing: ' + formatDuration(user.playTime.total) : null
+    tvTime = user.playTime && user.playTime.tv > 0 ? i18n('tpTimeSpentOnTV', formatDuration(user.playTime.tv)) : null
   }
 
   return (
     <section className="profileSection">
       {isFullUser(user) && user.completionRate ?
-      <p>Game completion rate: <strong>{user.completionRate}%</strong></p> : null
+      <p>{i18n('gameCompletionRate', user.completionRate)}</p> : null
       }
       {totalPlayTime ?
       <p>{totalPlayTime}</p> : null
@@ -241,13 +239,13 @@ function renderActions(ctrl: IUserCtrl, user: ProfileUser) {
         <div className="list_item nav"
           oncreate={helper.ontapY(ctrl.goToGames)}
         >
-          {i18n('viewAllNbGames', user.count.all)}
+          {plural('nbGamesInPlay', user.count.all, user.count.all)}
         </div> : null
       }
       <div className="list_item nav"
         oncreate={helper.ontapY(ctrl.followers)}
       >
-        {i18n('nbFollowers', user.nbFollowers || '')}
+        {plural('nbFollowers', user.nbFollowers, user.nbFollowers)}
       </div>
       { !ctrl.isMe() ? <div className="list_item nav" data-icon="1"
         oncreate={helper.ontapY(ctrl.goToUserTV)}
@@ -263,9 +261,10 @@ function renderActions(ctrl: IUserCtrl, user: ProfileUser) {
       </div> : null
       }
       { session.isConnected() && !ctrl.isMe() ?
-      <div className="list_item nav" data-icon="c"
+      <div className="list_item nav"
         oncreate={helper.ontapY(ctrl.composeMessage)}
       >
+        <span className="fa fa-comment" />
         {i18n('composeMessage')}
       </div> : null
       }

RoepStoep/lidrobile

src/ui/user/account/kid.ts
@@ -1,11 +1,11 @@
 import * as Mithril from 'mithril'
 import h from 'mithril/hyperscript'
+import i18n from '../../../i18n'
 import session from '../../../session'
 import { StoredProp } from '../../../storage'
 import formWidgets from '../../shared/form'
 import { dropShadowHeader, backButton } from '../../shared/common'
 import * as helper from '../../helper'
-import i18n from '../../../i18n'
 import layout from '../../layout'
 
 interface State {
@@ -31,7 +31,7 @@ function renderBody(ctrl: State) {
   return [
     h('div.native_scroller.page.settings_list.game', [
       h('p.explanation', i18n('kidModeExplanation')),
-      h('p.list_item', i18n('inKidModeTheLidraughtsLogoGetsIconX', '😊')),
+      h('p.list_item', h.trust(i18n('inKidModeTheLidraughtsLogoGetsIconX', '<span class="kiddo">😊</span>'))),
       h('p.list_item', formWidgets.renderCheckbox(i18n('enableKidMode'), 'kidMode', ctrl.kidMode))
     ])
   ]

src/ui/user/userView.tsx
@@ -7,11 +7,11 @@ import { linkify } from '../../utils/html'
 import { perfTypes, provisionalDeviation } from '../../lidraughts/perfs'
 import { Perf } from '../../lidraughts/interfaces/user'
 import * as xhr from '../../xhr'
-import i18n, { formatDate, formatDuration, fromNow } from '../../i18n'
+import i18n, { plural, formatDate, formatDuration, fromNow } from '../../i18n'
 import countries from '../../utils/countries'
 import * as helper from '../helper'
 import session from '../../session'
-import { IUserCtrl, ProfileUser, isSessionUser, isFullUser } from './UserCtrl'
+import { IUserCtrl, ProfileUser, isFullUser } from './UserCtrl'
 
 export function header(user: ProfileUser, ctrl: IUserCtrl) {
   const title = userTitle(user.online!!, user.patron!!, user.username, user.title)
@@ -104,7 +104,7 @@ function renderProfile(user: ProfileUser) {
           </p>
           <p>{memberSince}</p>
           {user.seenAt ?
-          <p>Active <small>{fromNow(new Date(user.seenAt))}</small></p> : null
+          <p>{h.trust(i18n('lastSeenActive', `<small>${fromNow(new Date(user.seenAt))}</small>`))}</p> : null
           }
         </div>
       </section>
@@ -146,8 +146,9 @@ function renderWebsiteLinks(ctrl: IUserCtrl, user: ProfileUser) {
 }
 
 function renderStats(user: ProfileUser) {
-  let totalPlayTime: string | null = null
   let tvTime: string | null = null
+
+  const totalPlayTime = user.playTime ? i18n('tpTimeSpentPlaying', formatDuration(user.playTime.total)) : null
 
   if (isFullUser(user)) {
     totalPlayTime = user.playTime ? i18n('tpTimeSpentPlaying', formatDuration(user.playTime.total)) : null
@@ -159,7 +160,7 @@ function renderStats(user: ProfileUser) {
   return (
     <section className="profileSection">
       {isFullUser(user) && user.completionRate ?
-      <p>{i18n('gameCompletionRate', '')}<strong>{user.completionRate}%</strong></p> : null
+      <p>{i18n('gameCompletionRate', user.completionRate)}</p> : null
       }
       {totalPlayTime ?
       <p>{totalPlayTime}</p> : null
@@ -243,13 +244,13 @@ function renderActions(ctrl: IUserCtrl, user: ProfileUser) {
         <div className="list_item nav"
           oncreate={helper.ontapY(ctrl.goToGames)}
         >
-          {i18n('nbGames', user.count.all)}
+          {plural('nbGames', user.count.all, user.count.all)}
         </div> : null
       }
       <div className="list_item nav"
         oncreate={helper.ontapY(ctrl.followers)}
       >
-        {i18n('nbFollowers', user.nbFollowers || '')}
+        {plural('nbFollowers', user.nbFollowers, user.nbFollowers)}
       </div>
       { !ctrl.isMe() ? <div className="list_item nav" data-icon="1"
         oncreate={helper.ontapY(ctrl.goToUserTV)}
@@ -265,9 +266,10 @@ function renderActions(ctrl: IUserCtrl, user: ProfileUser) {
       </div> : null
       }
       { session.isConnected() && !ctrl.isMe() ?
-      <div className="list_item nav" data-icon="c"
+      <div className="list_item nav"
         oncreate={helper.ontapY(ctrl.composeMessage)}
       >
+        <span className="fa fa-comment" />
         {i18n('composeMessage')}
       </div> : null
       }