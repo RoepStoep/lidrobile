vovagorodok/blidraughts

Fix share on android

src/ui/ai/AiRound.ts
@@ -174,7 +174,7 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
   public sharePGN = () => {
     this.replay.pgn(this.white(), this.black())
     .then((data: chess.PgnDumpResponse) =>
-      Plugins.Share.share({ text: data.pgn })
+      Plugins.LiShare.share({ text: data.pgn })
     )
   }
 

src/ui/analyse/menu.ts
@@ -109,7 +109,7 @@ function renderShareMenu(ctrl: AnalyseCtrl) {
     isOnlineAnalyseData(ctrl.data) ? h('button', {
       oncreate: helper.ontap(() => {
         ctrl.menu.close()
-        Plugins.Share.share({ url: gameApi.publicAnalyseUrl(ctrl.data) })
+        Plugins.LiShare.share({ url: gameApi.publicAnalyseUrl(ctrl.data) })
       })
     }, [i18n('shareGameURL')]) : null,
     ctrl.source === 'offline' ? h('button', {
@@ -130,7 +130,7 @@ function renderShareMenu(ctrl: AnalyseCtrl) {
     ctrl.isOfflineOrNotPlayable() ? h('button', {
       oncreate: helper.ontap(() => {
         ctrl.menu.close()
-        Plugins.Share.share({ text: ctrl.node.fen })
+        Plugins.LiShare.share({ text: ctrl.node.fen })
       }),
     }, 'Share current FEN') : null,
   ]))
@@ -146,7 +146,7 @@ function onlinePGNExport(ctrl: AnalyseCtrl, raw: boolean) {
       ctrl.menu.s.computingPGNAnnotated = false
       ctrl.menu.close()
       redraw()
-      Plugins.Share.share({ text: pgn })
+      Plugins.LiShare.share({ text: pgn })
     })
     .catch(e => {
       ctrl.menu.s.computingPGN = false
@@ -178,7 +178,7 @@ function offlinePgnExport(ctrl: AnalyseCtrl) {
       ctrl.menu.s.computingPGN = false
       ctrl.menu.close()
       redraw()
-      Plugins.Share.share({ text: res.pgn })
+      Plugins.LiShare.share({ text: res.pgn })
     })
     .catch(e => {
       ctrl.menu.s.computingPGN = false

src/ui/analyse/study/actionMenu.ts
@@ -116,7 +116,7 @@ function renderShareMenu(ctrl: AnalyseCtrl) {
     ctrl.study!.actionMenu.s.loadingChapterPGN = false
     ctrl.study!.actionMenu.s.loadingStudyPGN = false
     redraw()
-    Plugins.Share.share({ text: pgn })
+    Plugins.LiShare.share({ text: pgn })
   }
 
   function onPgnError(e: ErrorResponse) {
@@ -130,13 +130,13 @@ function renderShareMenu(ctrl: AnalyseCtrl) {
     h('button', {
       oncreate: helper.ontap(() => {
         const url = baseUrl + `study/${ctrl.study!.data.id}`
-        Plugins.Share.share({ url })
+        Plugins.LiShare.share({ url })
       })
     }, [i18n('Study URL')]),
     h('button', {
       oncreate: helper.ontap(() => {
         const url = baseUrl + `study/${ctrl.study!.data.id}/${ctrl.study!.data.chapter.id}`
-        Plugins.Share.share({ url })
+        Plugins.LiShare.share({ url })
       })
     }, [i18n('Current chapter URL')]),
     h('button', {

src/ui/otb/OtbRound.ts
@@ -204,7 +204,7 @@ export default class OtbRound implements OtbRoundInterface, PromotingInterface {
   public sharePGN = () => {
     this.replay.pgn('White', 'Black')
     .then((data: chess.PgnDumpResponse) =>
-      Plugins.Share.share({ text: data.pgn })
+      Plugins.LiShare.share({ text: data.pgn })
     )
   }
 

src/ui/shared/round/view/button.tsx
@@ -32,7 +32,7 @@ export default {
   shareLink(ctrl: OnlineRound) {
     return h('button', {
       oncreate: helper.ontap(() => {
-        Plugins.Share.share({ url: gameApi.publicUrl(ctrl.data) })
+        Plugins.LiShare.share({ url: gameApi.publicUrl(ctrl.data) })
       })
     }, [i18n('shareGameURL')])
   },
@@ -43,7 +43,7 @@ export default {
         handleXhrError(err)
         throw err
       })
-      .then((PGN: string) => Plugins.Share.share({ text: PGN }))
+      .then((PGN: string) => Plugins.LiShare.share({ text: PGN }))
     }
     return (
       <button oncreate={helper.ontap(handler)}>

RoepStoep/lidrobile

src/ui/ai/AiRound.ts
@@ -183,7 +183,7 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
   public sharePDN = () => {
     this.replay.pdn(this.white(), this.black())
     .then((data: draughts.PdnDumpResponse) =>
-      Plugins.Share.share({ text: data.pdn })
+      Plugins.LiShare.share({ text: data.pdn })
     )
   }
 

src/ui/analyse/menu.ts
@@ -110,7 +110,7 @@ function renderShareMenu(ctrl: AnalyseCtrl) {
     isOnlineAnalyseData(ctrl.data) ? h('button', {
       oncreate: helper.ontap(() => {
         ctrl.menu.close()
-        Plugins.Share.share({ url: gameApi.publicAnalyseUrl(ctrl.data) })
+        Plugins.LiShare.share({ url: gameApi.publicAnalyseUrl(ctrl.data) })
       })
     }, [i18n('shareGameURL')]) : null,
     ctrl.source === 'offline' ? h('button', {
@@ -131,7 +131,7 @@ function renderShareMenu(ctrl: AnalyseCtrl) {
     ctrl.isOfflineOrNotPlayable() ? h('button', {
       oncreate: helper.ontap(() => {
         ctrl.menu.close()
-        Plugins.Share.share({ text: toggleCoordinates(ctrl.node.fen, ctrl.isAlgebraic()) })
+        Plugins.LiShare.share({ text: toggleCoordinates(ctrl.node.fen, ctrl.isAlgebraic()) })
       }),
     }, 'Share current FEN') : null,
   ]))
@@ -147,7 +147,7 @@ function onlinePDNExport(ctrl: AnalyseCtrl, raw: boolean) {
       ctrl.menu.s.computingPDNAnnotated = false
       ctrl.menu.close()
       redraw()
-      Plugins.Share.share({ text: pdn })
+      Plugins.LiShare.share({ text: pdn })
     })
     .catch(e => {
       ctrl.menu.s.computingPDN = false
@@ -181,7 +181,7 @@ function offlinePdnExport(ctrl: AnalyseCtrl) {
       ctrl.menu.s.computingPDN = false
       ctrl.menu.close()
       redraw()
-      Plugins.Share.share({ text: res.pdn })
+      Plugins.LiShare.share({ text: res.pdn })
     })
     .catch(e => {
       ctrl.menu.s.computingPDN = false

src/ui/analyse/study/actionMenu.ts
@@ -116,7 +116,7 @@ function renderShareMenu(ctrl: AnalyseCtrl) {
     ctrl.study!.actionMenu.s.loadingChapterPDN = false
     ctrl.study!.actionMenu.s.loadingStudyPDN = false
     redraw()
-    Plugins.Share.share({ text: pdn })
+    Plugins.LiShare.share({ text: pdn })
   }
 
   function onPdnError(e: ErrorResponse) {
@@ -130,13 +130,13 @@ function renderShareMenu(ctrl: AnalyseCtrl) {
     h('button', {
       oncreate: helper.ontap(() => {
         const url = baseUrl + `study/${ctrl.study!.data.id}`
-        Plugins.Share.share({ url })
+        Plugins.LiShare.share({ url })
       })
     }, [i18n('studyUrl')]),
     h('button', {
       oncreate: helper.ontap(() => {
         const url = baseUrl + `study/${ctrl.study!.data.id}/${ctrl.study!.data.chapter.id}`
-        Plugins.Share.share({ url })
+        Plugins.LiShare.share({ url })
       })
     }, [i18n('currentChapterUrl')]),
     h('button', {

src/ui/otb/OtbRound.ts
@@ -202,7 +202,7 @@ export default class OtbRound implements OtbRoundInterface, PromotingInterface {
   public sharePDN = () => {
     this.replay.pdn('White', 'Black')
     .then((data: draughts.PdnDumpResponse) =>
-      Plugins.Share.share({ text: data.pdn })
+      Plugins.LiShare.share({ text: data.pdn })
     )
   }
 

src/ui/shared/round/view/button.tsx
@@ -32,7 +32,7 @@ export default {
   shareLink(ctrl: OnlineRound) {
     return h('button', {
       oncreate: helper.ontap(() => {
-        Plugins.Share.share({ url: gameApi.publicUrl(ctrl.data) })
+        Plugins.LiShare.share({ url: gameApi.publicUrl(ctrl.data) })
       })
     }, [i18n('shareGameURL')])
   },
@@ -43,7 +43,7 @@ export default {
           handleXhrError(err)
           throw err
         })
-        .then((pdn: string) => Plugins.Share.share({ text: pdn }))
+        .then((pdn: string) => Plugins.LiShare.share({ text: pdn }))
     }
     return (
       <button oncreate={helper.ontap(handler)}>