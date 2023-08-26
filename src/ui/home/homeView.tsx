vovagorodok/blidraughts

Implement new translation system

src/ui/home/homeView.tsx
@@ -105,8 +105,8 @@ const Stats = {
   },
   view() {
     return h('div.stats', [
-      h('div#nb_connected_players', h.trust(i18n('nbConnectedPlayers', '<strong>?</strong>'))),
-      h('div#nb_games_in_play', h.trust(i18n('nbGamesInPlay', '<strong>?</strong>'))),
+      h('div#nb_connected_players', h.trust(i18n('nbPlayers:other', '<strong>?</strong>'))),
+      h('div#nb_games_in_play', h.trust(i18n('nbGames:other', '<strong>?</strong>'))),
     ])
   }
 } as Mithril.Component<{}, { render: (p: PongMessage) => void }>

src/ui/lobby.ts
@@ -8,7 +8,7 @@ import settings from '../settings'
 import spinner from '../spinner'
 import router from '../router'
 import * as xhr from '../xhr'
-import i18n from '../i18n'
+import i18n, { plural } from '../i18n'
 import socket, { SocketIFace, SEEKING_SOCKET_NAME, RedirectObj } from '../socket'
 import { PongMessage, PoolMember, HumanSeekSetup, isPoolMember, isSeekSetup } from '../lichess/interfaces'
 import { OnlineGameData } from '../lichess/interfaces/game'
@@ -84,8 +84,8 @@ export default {
   view() {
 
     function content() {
-      const nbPlayersStr = i18n('nbConnectedPlayers', nbPlayers || '?')
-      const nbGamesStr = i18n('nbGamesInPlay', nbGames || '?')
+      const nbPlayersStr = plural('nbPlayers', nbPlayers, nbPlayers || '?')
+      const nbGamesStr = plural('nbGames', nbGames, nbGames || '?')
 
       if (currentSetup === null) {
         return h('div.lobby-waitingPopup', 'Something went wrong. Please try again')

RoepStoep/lidrobile

src/ui/home/homeView.tsx
@@ -109,8 +109,8 @@ const Stats = {
   },
   view() {
     return h('div.stats', [
-      h('div#nb_connected_players', h.trust(i18n('nbPlayers', '<strong>?</strong>'))),
-      h('div#nb_games_in_play', h.trust(i18n('nbGamesInPlay', '<strong>?</strong>'))),
+      h('div#nb_connected_players', h.trust(i18n('nbPlayers:other', '<strong>?</strong>'))),
+      h('div#nb_games_in_play', h.trust(i18n('nbGamesInPlay:other', '<strong>?</strong>'))),
     ])
   }
 } as Mithril.Component<{}, { render: (p: PongMessage) => void }>

src/ui/lobby.ts
@@ -8,7 +8,7 @@ import settings from '../settings'
 import spinner from '../spinner'
 import router from '../router'
 import * as xhr from '../xhr'
-import i18n from '../i18n'
+import i18n, { plural } from '../i18n'
 import socket, { SocketIFace, SEEKING_SOCKET_NAME, RedirectObj } from '../socket'
 import { PongMessage, PoolMember, HumanSeekSetup, isPoolMember, isSeekSetup } from '../lidraughts/interfaces'
 import { OnlineGameData } from '../lidraughts/interfaces/game'
@@ -84,8 +84,8 @@ export default {
   view() {
 
     function content() {
-      const nbPlayersStr = i18n('nbPlayers', nbPlayers || '?')
-      const nbGamesStr = i18n('nbGamesInPlay', nbGames || '?')
+      const nbPlayersStr = plural('nbPlayers', nbPlayers, nbPlayers || '?')
+      const nbGamesStr = plural('nbGamesInPlay', nbGames, nbGames || '?')
 
       if (currentSetup === null) {
         return h('div.lobby-waitingPopup', 'Something went wrong. Please try again')