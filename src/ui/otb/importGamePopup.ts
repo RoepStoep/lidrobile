vovagorodok/blidraughts

Toast with position

src/ui/otb/importGamePopup.ts
@@ -51,7 +51,7 @@ export default {
         const white = settings.otb.whitePlayer
         const black = settings.otb.blackPlayer
         return h('div', [
-          h('p', 'Import current game state with following player names to lichess?'),
+          h('p', 'Import current game state with following player names on lichess?'),
           h('form', [
             h('div.exchange', {
               oncreate: helper.ontap(() => {
@@ -104,7 +104,7 @@ export default {
                     })
                   }
                 )
-              }, 'Import to lichess')
+              }, 'Import on lichess')
           ])
         ])
       },

src/ui/shared/offlineRound/view.tsx
@@ -89,13 +89,13 @@ export function renderGameActionsBar(ctrl: OfflineRoundInterface) {
       <button className="action_bar_button fa fa-plus-circle"
         oncreate={helper.ontap(
           ctrl.newGameMenu.open,
-          () => Plugins.Toast.show({ text: i18n('createAGame'), duration: 'short' })
+          () => Plugins.LiToast.show({ text: i18n('createAGame'), duration: 'short', position: 'bottom' })
         )}
       />
       <button className="fa fa-share-alt action_bar_button"
         oncreate={helper.ontap(
           ctrl.sharePGN,
-          () => Plugins.Toast.show({ text: i18n('sharePGN'), duration: 'short' })
+          () => Plugins.LiToast.show({ text: i18n('sharePGN'), duration: 'short', position: 'bottom' })
         )}
       />
       <button className="action_bar_button" data-icon="A"

RoepStoep/lidrobile

src/ui/otb/importGamePopup.ts
@@ -51,7 +51,7 @@ export default {
         const white = settings.otb.whitePlayer
         const black = settings.otb.blackPlayer
         return h('div', [
-          h('p', 'Import current game state with following player names to lidraughts?'),
+          h('p', 'Import current game state with following player names on lidraughts?'),
           h('form', [
             h('div.exchange', {
               oncreate: helper.ontap(() => {
@@ -104,7 +104,7 @@ export default {
                     })
                   }
                 )
-              }, 'Import to lidraughts')
+              }, 'Import on lidraughts')
           ])
         ])
       },

src/ui/shared/offlineRound/view.tsx
@@ -71,13 +71,13 @@ export function renderGameActionsBar(ctrl: OfflineRoundInterface) {
       <button className="action_bar_button fa fa-plus-circle"
         oncreate={helper.ontap(
           ctrl.newGameMenu.open,
-          () => Plugins.Toast.show({ text: i18n('createAGame'), duration: 'short' })
+          () => Plugins.LiToast.show({ text: i18n('createAGame'), duration: 'short', position: 'bottom' })
         )}
       />
       <button className="fa fa-share-alt action_bar_button"
         oncreate={helper.ontap(
           ctrl.sharePDN,
-          () => Plugins.Toast.show({ text: i18n('sharePDN'), duration: 'short' })
+          () => Plugins.LiToast.show({ text: i18n('sharePDN'), duration: 'short', position: 'bottom' })
         )}
       />
       <button className="action_bar_button" data-icon="A"