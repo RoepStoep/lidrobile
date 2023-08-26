vovagorodok/blidraughts

Add featured game on home

src/ui/home/HomeCtrl.ts
@@ -1,15 +1,17 @@
 import { Plugins, AppState, NetworkStatus, PluginListenerHandle } from '@capacitor/core'
 import Zanimo from '../../utils/zanimo'
+import globalConfig from '../../config'
 import socket, { SocketIFace } from '../../socket'
 import redraw from '../../utils/redraw'
 import signals from '../../signals'
 import settings from '../../settings'
-import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr } from '../../xhr'
+import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr, featured as featuredXhr } from '../../xhr'
 import { hasNetwork, noop } from '../../utils'
 import { fromNow } from '../../i18n'
 import { isForeground } from '../../utils/appMode'
-import { PongMessage, TimelineEntry, DailyPuzzle, CorrespondenceSeek } from '../../lichess/interfaces'
+import { PongMessage, TimelineEntry, DailyPuzzle, CorrespondenceSeek, FeaturedGame, FeaturedPlayer } from '../../lichess/interfaces'
 import { TournamentListItem } from '../../lichess/interfaces/tournament'
+import { Player } from '../../lichess/interfaces/game'
 import { PuzzleData } from '../../lichess/interfaces/training'
 import session from '../../session'
 import { supportedTypes as supportedTimelineTypes } from '../timeline'
@@ -18,12 +20,13 @@ import { loadNewPuzzle } from '../training/offlineService'
 
 import { dailyPuzzle as dailyPuzzleXhr, featuredTournaments as featuredTournamentsXhr } from './homeXhr'
 
-
 export default class HomeCtrl {
   public selectedTab: number
 
   public socketIface?: SocketIFace
+  public tvFeed?: EventSource
 
+  public featured?: FeaturedGame
   public corresPool: ReadonlyArray<CorrespondenceSeek>
   public dailyPuzzle?: DailyPuzzle
   public featuredTournaments?: ReadonlyArray<TournamentListItem>
@@ -51,6 +54,9 @@ export default class HomeCtrl {
     this.appStateListener = Plugins.App.addListener('appStateChange', (state: AppState) => {
       console.debug('appStateChange')
       if (state.isActive) this.init()
+      else {
+        if (this.tvFeed) this.tvFeed.close()
+      }
     })
   }
 
@@ -61,6 +67,9 @@ export default class HomeCtrl {
   public unload = () => {
     this.networkListener.remove()
     this.appStateListener.remove()
+    if (this.tvFeed) {
+      this.tvFeed.close()
+    }
   }
 
   public init = () => {
@@ -76,23 +85,38 @@ export default class HomeCtrl {
         }
       })
 
+      this.tvFeed = new EventSource(globalConfig.apiEndPoint + '/tv/feed')
+      this.tvFeed.onmessage = (ev) => {
+        const data = JSON.parse(ev.data)
+        if (data.t === 'fen') {
+          if (this.featured) {
+            this.featured.fen = data.d.fen
+            this.featured.lastMove = data.d.lm
+            redraw()
+          }
+        }
+
+        if (data.t === 'featured') {
+          this.featured = undefined
+          // TODO use feed instead
+          this.getFeatured()
+        }
+      }
+
+      this.getFeatured()
+
       Promise.all([
         dailyPuzzleXhr(),
-        featuredTournamentsXhr()
+        featuredTournamentsXhr(),
+        timelineXhr()
       ])
       .then(results => {
-        const [dailyData, featuredTournamentsData] = results
+        const [dailyData, featuredTournamentsData, timeline] = results
         this.dailyPuzzle = dailyData.puzzle
         this.featuredTournaments = featuredTournamentsData.featured
-        redraw()
-      })
-      .catch(noop)
-
-      timelineXhr()
-      .then(data => {
-        this.timeline = data.entries
+        this.timeline = timeline.entries
           .filter((o: TimelineEntry) => supportedTimelineTypes.indexOf(o.type) !== -1)
-          .slice(0, 10)
+          .slice(0, 15)
           .map(o => {
             o.fromNow = fromNow(new Date(o.date))
             return o
@@ -139,6 +163,31 @@ export default class HomeCtrl {
     this.socketSend('joinSeek', seekId)
   }
 
+  private getFeatured = () => {
+    featuredXhr('best', false)
+    .then((data) => {
+      const opp = playerToFeatured(data.opponent)
+      const player = playerToFeatured(data.player)
+
+      this.featured = {
+        black: data.game.player === 'white' ? opp : player,
+        color: data.orientation,
+        fen: data.game.fen,
+        id: data.game.id,
+        lastMove: data.game.lastMove,
+        white: data.game.player === 'white' ? player : opp,
+      }
+      if (data.clock) {
+        this.featured.clock = {
+          increment: data.clock.increment,
+          initial: data.clock.initial
+        }
+      }
+
+      redraw()
+    })
+  }
+
   private reloadCorresPool = () => {
     if (this.selectedTab === 1) {
       corresSeeksXhr(false)
@@ -171,4 +220,14 @@ function fixSeeks(seeks: CorrespondenceSeek[]): CorrespondenceSeek[] {
     .filter(([_, key], i, a) => a.map(e => e[1]).indexOf(key) === i)
     .map(([s]) => s)
   ] as CorrespondenceSeek[]
+}
+
+function playerToFeatured(p: Player): FeaturedPlayer {
+  return {
+    name: p.name || p.username || p.user && p.user.username || '',
+    rating: p.rating || 0,
+    ratingDiff: p.ratingDiff || 0,
+    berserk: p.berserk,
+    title: p.user && p.user.title,
+  }
 }

src/ui/shared/miniBoard.tsx
@@ -3,6 +3,7 @@ import * as helper from '../helper'
 import ViewOnlyBoard from './ViewOnlyBoard'
 import { noop } from '../../utils'
 import { FeaturedGame } from '../../lichess/interfaces'
+import { time as renderTime } from '../../lichess/game'
 import h from 'mithril/hyperscript'
 
 export interface Attrs {
@@ -15,7 +16,6 @@ export interface Attrs {
   readonly customPieceTheme?: string
   readonly variant?: VariantKey
   readonly fixed?: boolean
-  readonly delay?: Millis
 }
 
 interface State {
@@ -63,19 +63,20 @@ function renderVsBloc(gameObj: FeaturedGame) {
           {player.rank ? `#${player.rank} ` : ''}
           {player.title ? <span className="userTitle">{player.title}&nbsp;</span> : null}
           {player.name}
+          <br/>
+          {player.rating}
+          {player.berserk ? <span className="berserk" data-icon="`" /> : null }
         </div>
+        { gameObj.clock ?
+          <div className="time">
+            {renderTime(gameObj)}
+          </div> : null
+        }
         <div className="opponent">
           {opponent.rank ? `#${opponent.rank} ` : ''}
           {opponent.title ? <span className="userTitle">{opponent.title}&nbsp;</span> : null}
           {opponent.name}
-        </div>
-      </div>
-      <div className="ratingAndTime">
-        <div>
-          {player.rating}
-          {player.berserk ? <span className="berserk" data-icon="`" /> : null }
-        </div>
-        <div>
+          <br/>
           {opponent.rating}
           {opponent.berserk ? <span className="berserk" data-icon="`" /> : null }
         </div>

RoepStoep/lidrobile

src/ui/home/HomeCtrl.ts
@@ -1,14 +1,15 @@
 import { Plugins, AppState, NetworkStatus, PluginListenerHandle } from '@capacitor/core'
 import Zanimo from '../../utils/zanimo'
+import globalConfig from '../../config'
 import socket, { SocketIFace } from '../../socket'
 import redraw from '../../utils/redraw'
 import signals from '../../signals'
 import settings from '../../settings'
-import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr } from '../../xhr'
+import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr, featured as featuredXhr } from '../../xhr'
 import { hasNetwork, noop } from '../../utils'
 import { fromNow } from '../../i18n'
 import { isForeground } from '../../utils/appMode'
-import { PongMessage, TimelineEntry, DailyPuzzleData, CorrespondenceSeek } from '../../lidraughts/interfaces'
+import { PongMessage, TimelineEntry, DailyPuzzleData, CorrespondenceSeek, FeaturedGame, FeaturedPlayer } from '../../lidraughts/interfaces'
 import { TournamentListItem } from '../../lidraughts/interfaces/tournament'
 import { PuzzleData } from '../../lidraughts/interfaces/training'
 import session from '../../session'
@@ -18,12 +19,13 @@ import { loadNewPuzzle } from '../training/offlineService'
 
 import { dailyPuzzle as dailyPuzzleXhr, featuredTournaments as featuredTournamentsXhr } from './homeXhr'
 
-
 export default class HomeCtrl {
   public selectedTab: number
 
   public socketIface?: SocketIFace
+  public tvFeed?: EventSource
 
+  public featured?: FeaturedGame
   public corresPool: ReadonlyArray<CorrespondenceSeek>
   public dailyPuzzle?: DailyPuzzleData
   public featuredTournaments?: ReadonlyArray<TournamentListItem>
@@ -51,6 +53,9 @@ export default class HomeCtrl {
     this.appStateListener = Plugins.App.addListener('appStateChange', (state: AppState) => {
       console.debug('appStateChange')
       if (state.isActive) this.init()
+      else {
+        if (this.tvFeed) this.tvFeed.close()
+      }
     })
   }
 
@@ -61,6 +66,9 @@ export default class HomeCtrl {
   public unload = () => {
     this.networkListener.remove()
     this.appStateListener.remove()
+    if (this.tvFeed) {
+      this.tvFeed.close()
+    }
   }
 
   public init = () => {
@@ -76,23 +84,38 @@ export default class HomeCtrl {
         }
       })
 
+      this.tvFeed = new EventSource(globalConfig.apiEndPoint + '/tv/feed')
+      this.tvFeed.onmessage = (ev) => {
+        const data = JSON.parse(ev.data)
+        if (data.t === 'fen') {
+          if (this.featured) {
+            this.featured.fen = data.d.fen
+            this.featured.lastMove = data.d.lm
+            redraw()
+          }
+        }
+
+        if (data.t === 'featured') {
+          this.featured = undefined
+          // TODO use feed instead
+          this.getFeatured()
+        }
+      }
+
+      this.getFeatured()
+
       Promise.all([
         dailyPuzzleXhr(),
-        featuredTournamentsXhr()
+        featuredTournamentsXhr(),
+        timelineXhr()
       ])
       .then(results => {
-        const [dailyData, featuredTournamentsData] = results
+        const [dailyData, featuredTournamentsData, timeline] = results
         this.dailyPuzzle = dailyData
         this.featuredTournaments = featuredTournamentsData.featured
-        redraw()
-      })
-      .catch(noop)
-
-      timelineXhr()
-      .then(data => {
-        this.timeline = data.entries
+        this.timeline = timeline.entries
           .filter((o: TimelineEntry) => supportedTimelineTypes.indexOf(o.type) !== -1)
-          .slice(0, 10)
+          .slice(0, 15)
           .map(o => {
             o.fromNow = fromNow(new Date(o.date))
             return o
@@ -139,6 +162,31 @@ export default class HomeCtrl {
     this.socketSend('joinSeek', seekId)
   }
 
+  private getFeatured = () => {
+    featuredXhr('best', false)
+    .then((data) => {
+      const opp = playerToFeatured(data.opponent)
+      const player = playerToFeatured(data.player)
+
+      this.featured = {
+        black: data.game.player === 'white' ? opp : player,
+        color: data.orientation,
+        fen: data.game.fen,
+        id: data.game.id,
+        lastMove: data.game.lastMove,
+        white: data.game.player === 'white' ? player : opp,
+      }
+      if (data.clock) {
+        this.featured.clock = {
+          increment: data.clock.increment,
+          initial: data.clock.initial
+        }
+      }
+
+      redraw()
+    })
+  }
+
   private reloadCorresPool = () => {
     if (this.selectedTab === 1) {
       corresSeeksXhr(false)
@@ -171,4 +219,14 @@ function fixSeeks(seeks: CorrespondenceSeek[]): CorrespondenceSeek[] {
     .filter(([_, key], i, a) => a.map(e => e[1]).indexOf(key) === i)
     .map(([s]) => s)
   ] as CorrespondenceSeek[]
+}
+
+function playerToFeatured(p: Player): FeaturedPlayer {
+  return {
+    name: p.name || p.username || p.user && p.user.username || '',
+    rating: p.rating || 0,
+    ratingDiff: p.ratingDiff || 0,
+    berserk: p.berserk,
+    title: p.user && p.user.title,
+  }
 }

src/ui/shared/miniBoard.tsx
@@ -3,6 +3,7 @@ import * as helper from '../helper'
 import ViewOnlyBoard from './ViewOnlyBoard'
 import { noop } from '../../utils'
 import { FeaturedGame } from '../../lidraughts/interfaces'
+import { time as renderTime } from '../../lidraughts/game'
 import h from 'mithril/hyperscript'
 
 export interface Attrs {
@@ -15,7 +16,6 @@ export interface Attrs {
   readonly customPieceTheme?: string
   readonly variant: VariantKey
   readonly fixed?: boolean
-  readonly delay?: Millis
 }
 
 interface State {
@@ -63,19 +63,20 @@ function renderVsBloc(gameObj: FeaturedGame) {
           {player.rank ? `#${player.rank} ` : ''}
           {player.title ? <span className="userTitle">{player.title}&nbsp;</span> : null}
           {player.name}
+          <br/>
+          {player.rating}
+          {player.berserk ? <span className="berserk" data-icon="`" /> : null }
         </div>
+        { gameObj.clock ?
+          <div className="time">
+            {renderTime(gameObj)}
+          </div> : null
+        }
         <div className="opponent">
           {opponent.rank ? `#${opponent.rank} ` : ''}
           {opponent.title ? <span className="userTitle">{opponent.title}&nbsp;</span> : null}
           {opponent.name}
-        </div>
-      </div>
-      <div className="ratingAndTime">
-        <div>
-          {player.rating}
-          {player.berserk ? <span className="berserk" data-icon="`" /> : null }
-        </div>
-        <div>
+          <br/>
           {opponent.rating}
           {opponent.berserk ? <span className="berserk" data-icon="`" /> : null }
         </div>