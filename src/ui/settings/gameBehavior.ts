vovagorodok/blidraughts

Rook castle settings
Closes #1206

src/ui/settings/gameBehavior.ts
@@ -3,49 +3,26 @@ import h from 'mithril/hyperscript'
 import layout from '../layout'
 import i18n from '../../i18n'
 import { hasNetwork } from '../../utils'
-import settings, { Prop } from '../../settings'
+import settings from '../../settings'
 import session from '../../session'
-import { Takeback, SubmitMove, AutoQueen, AutoThreefold, SubmitMoveChoices, TakebackChoices, AutoQueenChoices, AutoThreefoldChoices } from '../../lichess/prefs'
 import * as helper from '../helper'
 import { dropShadowHeader, backButton } from '../shared/common'
 import formWidgets from '../shared/form'
-
-interface Ctrl {
-  readonly premove: Prop<boolean>
-  readonly takeback: Prop<number>
-  readonly autoQueen: Prop<number>
-  readonly autoThreefold: Prop<number>
-  readonly submitMove: Prop<number>
-}
-
-interface State {
-  ctrl: Ctrl
-}
+import { prefsCtrl, renderLichessPrefs } from '../user/account/gameBehavior'
 
 export default {
   oncreate: helper.viewSlideIn,
 
-  oninit() {
-    this.ctrl = {
-      premove: session.lichessBackedProp<boolean>('prefs.premove', session.savePreferences, true),
-      takeback: session.lichessBackedProp<number>('prefs.takeback', session.savePreferences, Takeback.ALWAYS),
-      autoQueen: session.lichessBackedProp<number>('prefs.autoQueen', session.savePreferences, AutoQueen.PREMOVE),
-      autoThreefold: session.lichessBackedProp<number>('prefs.autoThreefold', session.savePreferences, AutoThreefold.TIME),
-      submitMove: session.lichessBackedProp<number>('prefs.submitMove', session.savePreferences, SubmitMove.CORRESPONDENCE_ONLY)
-    }
-  },
-
   view() {
-    const ctrl = this.ctrl
     const header = dropShadowHeader(null, backButton(i18n('gameBehavior')))
     return layout.free(header,
       h('ul.native_scroller.page.settings_list.game',
         renderAppPrefs().concat(hasNetwork() && session.isConnected() ?
-          renderLichessPrefs(ctrl) : []
+          renderLichessPrefs(prefsCtrl) : []
         )
     ))
   }
-} as Mithril.Component<{}, State>
+} as Mithril.Component<{}, {}>
 
 function renderAppPrefs() {
   return [
@@ -59,29 +36,14 @@ function renderAppPrefs() {
         settings.game.pieceMove
       )
     ),
+    h('li.list_item',
+      formWidgets.renderMultipleChoiceButton(
+        i18n('castleByMovingTheKingTwoSquaresOrOntoTheRook'), [
+          { label: i18n('castleByMovingTwoSquares'), value: 0 },
+          { label: i18n('castleByMovingOntoTheRook'), value: 1 },
+        ],
+        settings.game.rookCastle,
+      )
+    ),
   ]
 }
-
-function renderLichessPrefs(ctrl: Ctrl) {
-  return [
-    h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('premovesPlayingDuringOpponentTurn'), [
-        { label: i18n('no'), value: false },
-        { label: i18n('yes'), value: true },
-      ], ctrl.premove)),
-    h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('takebacksWithOpponentApproval'), TakebackChoices.map(formWidgets.lichessPropToOption), ctrl.takeback
-    )),
-    h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('promoteToQueenAutomatically'), AutoQueenChoices.map(formWidgets.lichessPropToOption), ctrl.autoQueen
-    )),
-    h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('claimDrawOnThreefoldRepetitionAutomatically').replace(/\%s/g, ''), AutoThreefoldChoices.map(formWidgets.lichessPropToOption), ctrl.autoThreefold
-    )),
-    h('li.list_item', [
-      h('div.label', i18n('moveConfirmation')),
-      h('div.select_input.no_label.settingsChoicesBlock', formWidgets.renderLichessPropSelect('', 'moveConfirmation', SubmitMoveChoices, ctrl.submitMove))
-    ])
-  ]
-}
-

src/ui/user/account/gameBehavior.ts
@@ -1,0 +1,51 @@
+import * as Mithril from 'mithril'
+import h from 'mithril/hyperscript'
+import layout from '../../layout'
+import i18n from '../../../i18n'
+import session from '../../../session'
+import { Takeback, SubmitMove, AutoQueen, AutoThreefold, SubmitMoveChoices, TakebackChoices, AutoQueenChoices, AutoThreefoldChoices } from '../../../lichess/prefs'
+import * as helper from '../../helper'
+import { dropShadowHeader, backButton } from '../../shared/common'
+import formWidgets from '../../shared/form'
+
+export default {
+  oncreate: helper.viewSlideIn,
+
+  view() {
+    const header = dropShadowHeader(null, backButton(i18n('gameBehavior')))
+    return layout.free(header,
+      h('ul.native_scroller.page.settings_list.game', renderLichessPrefs(prefsCtrl))
+    )
+  }
+} as Mithril.Component<{}, {}>
+
+export const prefsCtrl = {
+  premove: session.lichessBackedProp<boolean>('prefs.premove', session.savePreferences, true),
+  takeback: session.lichessBackedProp<number>('prefs.takeback', session.savePreferences, Takeback.ALWAYS),
+  autoQueen: session.lichessBackedProp<number>('prefs.autoQueen', session.savePreferences, AutoQueen.PREMOVE),
+  autoThreefold: session.lichessBackedProp<number>('prefs.autoThreefold', session.savePreferences, AutoThreefold.TIME),
+  submitMove: session.lichessBackedProp<number>('prefs.submitMove', session.savePreferences, SubmitMove.CORRESPONDENCE_ONLY),
+}
+
+export function renderLichessPrefs(ctrl: typeof prefsCtrl) {
+  return [
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('premovesPlayingDuringOpponentTurn'), [
+        { label: i18n('no'), value: false },
+        { label: i18n('yes'), value: true },
+      ], ctrl.premove)),
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('takebacksWithOpponentApproval'), TakebackChoices.map(formWidgets.lichessPropToOption), ctrl.takeback
+    )),
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('promoteToQueenAutomatically'), AutoQueenChoices.map(formWidgets.lichessPropToOption), ctrl.autoQueen
+    )),
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('claimDrawOnThreefoldRepetitionAutomatically').replace(/\%s/g, ''), AutoThreefoldChoices.map(formWidgets.lichessPropToOption), ctrl.autoThreefold
+    )),
+    h('li.list_item', [
+      h('div.label', i18n('moveConfirmation')),
+      h('div.select_input.no_label.settingsChoicesBlock', formWidgets.renderLichessPropSelect('', 'moveConfirmation', SubmitMoveChoices, ctrl.submitMove))
+    ]),
+  ]
+}

RoepStoep/lidrobile

src/ui/settings/gameBehavior.ts
@@ -3,47 +3,26 @@ import h from 'mithril/hyperscript'
 import layout from '../layout'
 import i18n from '../../i18n'
 import { hasNetwork } from '../../utils'
-import settings, { Prop } from '../../settings'
+import settings from '../../settings'
 import session from '../../session'
-import { Takeback, SubmitMove, AutoThreefold, SubmitMoveChoices, TakebackChoices, AutoThreefoldChoices } from '../../lidraughts/prefs'
 import * as helper from '../helper'
 import { dropShadowHeader, backButton } from '../shared/common'
 import formWidgets from '../shared/form'
-
-interface Ctrl {
-  readonly premove: Prop<boolean>
-  readonly takeback: Prop<number>
-  readonly autoThreefold: Prop<number>
-  readonly submitMove: Prop<number>
-}
-
-interface State {
-  ctrl: Ctrl
-}
+import { prefsCtrl, renderLidraughtsPrefs } from '../user/account/gameBehavior'
 
 export default {
   oncreate: helper.viewSlideIn,
 
-  oninit() {
-    this.ctrl = {
-      premove: session.lidraughtsBackedProp<boolean>('prefs.premove', session.savePreferences, true),
-      takeback: session.lidraughtsBackedProp<number>('prefs.takeback', session.savePreferences, Takeback.ALWAYS),
-      autoThreefold: session.lidraughtsBackedProp<number>('prefs.autoThreefold', session.savePreferences, AutoThreefold.TIME),
-      submitMove: session.lidraughtsBackedProp<number>('prefs.submitMove', session.savePreferences, SubmitMove.CORRESPONDENCE_ONLY)
-    }
-  },
-
   view() {
-    const ctrl = this.ctrl
     const header = dropShadowHeader(null, backButton(i18n('gameBehavior')))
     return layout.free(header,
       h('ul.native_scroller.page.settings_list.game',
         renderAppPrefs().concat(hasNetwork() && session.isConnected() ?
-          renderLidraughtsPrefs(ctrl) : []
+          renderLidraughtsPrefs(prefsCtrl) : []
         )
     ))
   }
-} as Mithril.Component<{}, State>
+} as Mithril.Component<{}, {}>
 
 function renderAppPrefs() {
   return [
@@ -65,25 +44,3 @@ function renderAppPrefs() {
     )),
   ]
 }
-
-function renderLidraughtsPrefs(ctrl: Ctrl) {
-  return [
-    h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('premovesPlayingDuringOpponentTurn'), [
-        { label: i18n('no'), value: false },
-        { label: i18n('yes'), value: true },
-      ], ctrl.premove)
-    ),
-    h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('takebacksWithOpponentApproval'), TakebackChoices.map(formWidgets.lidraughtsPropToOption), ctrl.takeback
-    )),
-    h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('claimDrawOnThreefoldRepetitionAutomatically').replace(/\%s/g, ''), AutoThreefoldChoices.map(formWidgets.lidraughtsPropToOption), ctrl.autoThreefold
-    )),
-    h('li.list_item', [
-      h('div.label', i18n('moveConfirmation')),
-      h('div.select_input.no_label.settingsChoicesBlock', formWidgets.renderLidraughtsPropSelect('', 'moveConfirmation', SubmitMoveChoices, ctrl.submitMove))
-    ])
-  ]
-}
-

src/ui/user/account/gameBehavior.ts
@@ -1,0 +1,47 @@
+import * as Mithril from 'mithril'
+import h from 'mithril/hyperscript'
+import layout from '../../layout'
+import i18n from '../../../i18n'
+import session from '../../../session'
+import { Takeback, SubmitMove, AutoThreefold, SubmitMoveChoices, TakebackChoices, AutoThreefoldChoices } from '../../../lidraughts/prefs'
+import * as helper from '../../helper'
+import { dropShadowHeader, backButton } from '../../shared/common'
+import formWidgets from '../../shared/form'
+
+export default {
+  oncreate: helper.viewSlideIn,
+
+  view() {
+    const header = dropShadowHeader(null, backButton(i18n('gameBehavior')))
+    return layout.free(header,
+      h('ul.native_scroller.page.settings_list.game', renderLidraughtsPrefs(prefsCtrl))
+    )
+  }
+} as Mithril.Component<{}, {}>
+
+export const prefsCtrl = {
+  premove: session.lidraughtsBackedProp<boolean>('prefs.premove', session.savePreferences, true),
+  takeback: session.lidraughtsBackedProp<number>('prefs.takeback', session.savePreferences, Takeback.ALWAYS),
+  autoThreefold: session.lidraughtsBackedProp<number>('prefs.autoThreefold', session.savePreferences, AutoThreefold.TIME),
+  submitMove: session.lidraughtsBackedProp<number>('prefs.submitMove', session.savePreferences, SubmitMove.CORRESPONDENCE_ONLY),
+}
+
+export function renderLidraughtsPrefs(ctrl: typeof prefsCtrl) {
+  return [
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('premovesPlayingDuringOpponentTurn'), [
+        { label: i18n('no'), value: false },
+        { label: i18n('yes'), value: true },
+      ], ctrl.premove)),
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('takebacksWithOpponentApproval'), TakebackChoices.map(formWidgets.lidraughtsPropToOption), ctrl.takeback
+    )),
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('claimDrawOnThreefoldRepetitionAutomatically').replace(/\%s/g, ''), AutoThreefoldChoices.map(formWidgets.lidraughtsPropToOption), ctrl.autoThreefold
+    )),
+    h('li.list_item', [
+      h('div.label', i18n('moveConfirmation')),
+      h('div.select_input.no_label.settingsChoicesBlock', formWidgets.renderLidraughtsPropSelect('', 'moveConfirmation', SubmitMoveChoices, ctrl.submitMove))
+    ]),
+  ]
+}