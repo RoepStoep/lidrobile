vovagorodok/blidraughts

WIP on downloading board themes

src/settings.ts
@@ -76,19 +76,32 @@ export default {
         { key: 'space', name: 'Space', ext: 'jpg' },
       ],
       availableBoardThemes: [
-        { name: 'Brown', key: 'brown', ext: 'svg' },
-        { name: 'Blue', key: 'blue', ext: 'svg' },
-        { name: 'Green', key: 'green', ext: 'svg' },
-        { name: 'Purple', key: 'purple', ext: 'svg' },
-        { name: 'Wood', key: 'wood', ext: 'jpg' },
-        { name: 'Wood 2', key: 'wood2', ext: 'jpg' },
-        { name: 'Wood 3', key: 'wood3', ext: 'jpg' },
-        { name: 'Maple', key: 'maple', ext: 'jpg' },
-        { name: 'Blue 2', key: 'blue3', ext: 'jpg' },
-        { name: 'Canvas', key: 'canvas', ext: 'jpg' },
-        { name: 'Metal', key: 'metal', ext: 'jpg' }
+        { key: 'blue', name: 'Blue', ext: 'svg' },
+        { key: 'blue2', name: 'Blue 2', ext: 'jpg' },
+        { key: 'blue3', name: 'Blue 3', ext: 'jpg' },
+        { key: 'blue-marble', name: 'Blue Marble', ext: 'jpg' },
+        { key: 'canvas', name: 'Canvas', ext: 'jpg' },
+        { key: 'wood', name: 'Wood', ext: 'jpg' },
+        { key: 'wood2', name: 'Wood 2', ext: 'jpg' },
+        { key: 'wood3', name: 'Wood 3', ext: 'jpg' },
+        { key: 'wood4', name: 'Wood 4', ext: 'jpg' },
+        { key: 'maple', name: 'Maple', ext: 'jpg' },
+        { key: 'maple2', name: 'Maple 2', ext: 'jpg' },
+        { key: 'brown', name: 'Brown', ext: 'svg' },
+        { key: 'leather', name: 'Leather', ext: 'jpg' },
+        { key: 'green', name: 'Green', ext: 'svg' },
+        { key: 'marble', name: 'Marble', ext: 'jpg' },
+        { key: 'green-plastic', name: 'Green Plastic', ext: 'png' },
+        { key: 'grey', name: 'Grey', ext: 'jpg' },
+        { key: 'metal', name: 'Metal', ext: 'jpg' },
+        { key: 'olive', name: 'Olive', ext: 'jpg' },
+        { key: 'newspaper', name: 'Newspaper', ext: 'png' },
+        { key: 'purple', name: 'Purple', ext: 'svg' },
+        { key: 'purple-diag', name: 'Purple 2', ext: 'png' },
+        { key: 'pink', name: 'Pink', ext: 'png' },
+        { key: 'ic', name: 'IC', ext: 'svg' },
       ],
-      bundledBoardThemes: ['brown', 'blue', 'green', 'purple'],
+      bundledBoardThemes: ['brown', 'blue', 'green', 'purple', 'ic'],
       availablePieceThemes: [
         ['cburnett', 'Colin M.L. Burnett'],
         ['merida' ],

RoepStoep/lidrobile

src/settings.ts
@@ -83,22 +83,23 @@ export default {
         { key: 'space', name: 'Space', ext: 'jpg' },
       ],
       availableBoardThemes: [
-        { name: 'Brown', key: 'brown', ext: 'svg' },
-        { name: 'Brown 2', key: 'brown2', ext: 'svg' },
-        { name: 'Blue', key: 'blue', ext: 'svg' },
-        { name: 'Green', key: 'green', ext: 'svg' },
-        { name: 'Grey', key: 'grey', ext: 'jpg' },
-        { name: 'Purple', key: 'purple', ext: 'svg' },
-        { name: 'Wood', key: 'wood', ext: 'jpg' },
-        { name: 'Wood 2', key: 'wood2', ext: 'jpg' },
-        { name: 'Wood 3', key: 'wood3', ext: 'jpg' },
-        { name: 'Maple', key: 'maple', ext: 'jpg' },
-        { name: 'Blue 2', key: 'blue3', ext: 'jpg' },
-        { name: 'Canvas', key: 'canvas', ext: 'jpg' },
-        { name: 'Metal', key: 'metal', ext: 'jpg' },
-        { name: 'Match', key: 'match', ext: 'svg' }
+        { key: 'brown', name: 'Brown', ext: 'svg' },
+        { key: 'brown2', name: 'Brown 2', ext: 'svg' },
+        { key: 'blue', name: 'Blue', ext: 'svg' },
+        { key: 'green', name: 'Green', ext: 'svg' },
+        { key: 'grey', name: 'Grey', ext: 'jpg' },
+        { key: 'purple', name: 'Purple', ext: 'svg' },
+        { key: 'wood', name: 'Wood', ext: 'jpg' },
+        { key: 'wood2', name: 'Wood 2', ext: 'jpg' },
+        { key: 'wood3', name: 'Wood 3', ext: 'jpg' },
+        { key: 'maple', name: 'Maple', ext: 'jpg' },
+        { key: 'blue3', name: 'Blue 2', ext: 'jpg' },
+        { key: 'canvas', name: 'Canvas', ext: 'jpg' },
+        { key: 'metal', name: 'Metal', ext: 'jpg' },
+        { key: 'match', name: 'Match', ext: 'svg' },
+        { key: 'ic', name: 'IC', ext: 'svg' },
       ],
-      bundledBoardThemes: ['brown', 'blue', 'green', 'purple'],
+      bundledBoardThemes: ['brown', 'brown2', 'blue', 'green', 'purple', 'match', 'ic'],
       availablePieceThemes: [
         ['wide_crown', 'Standard'],
         ['wide', 'Standard no crown'],