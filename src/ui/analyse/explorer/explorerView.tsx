vovagorodok/blidraughts

Fix plural interface, improve study translation

src/ui/analyse/explorer/explorerView.tsx
@@ -89,7 +89,7 @@ function winner(stm: string, move: TablebaseMoveStats) {
 
 function showDtm(stm: string, move: TablebaseMoveStats) {
   if (move.dtm) return h('result.' + winner(stm, move), {
-    title: plural('mateInXHalfMoves', Math.abs(move.dtm), Math.abs(move.dtm)),
+    title: plural('mateInXHalfMoves', Math.abs(move.dtm)),
   }, 'DTM ' + Math.abs(move.dtm))
   else return null
 }
@@ -108,7 +108,7 @@ function showDtz(stm: string, move: TablebaseMoveStats) {
     else return h('result.' + winner(stm, move), i18n('pawnMove'))
   }
   else return h('result.' + winner(stm, move), {
-    title: plural('nextCaptureOrPawnMoveInXHalfMoves', Math.abs(move.dtz), Math.abs(move.dtz))
+    title: plural('nextCaptureOrPawnMoveInXHalfMoves', Math.abs(move.dtz))
   }, 'DTZ ' + Math.abs(move.dtz))
 }
 

src/ui/lobby.ts
@@ -84,8 +84,8 @@ export default {
   view() {
 
     function content() {
-      const nbPlayersStr = plural('nbPlayers', nbPlayers, nbPlayers || '?')
-      const nbGamesStr = plural('nbGames', nbGames, nbGames || '?')
+      const nbPlayersStr = plural('nbPlayers', nbPlayers, nbPlayers ? undefined : '?')
+      const nbGamesStr = plural('nbGames', nbGames, nbGames ? undefined : '?')
 
       if (currentSetup === null) {
         return h('div.lobby-waitingPopup', 'Something went wrong. Please try again')
@@ -133,7 +133,7 @@ function renderCustomSetup(setup: HumanSeekSetup) {
   if (timeMode === 1) {
     time = utils.displayTime(String(minutes)) + '+' + setup.increment
   } else if (timeMode === 2) {
-    time = plural('nbDays', setup.days, setup.days)
+    time = plural('nbDays', setup.days)
   } else {
     time = '∞'
   }

RoepStoep/lidrobile

src/ui/analyse/explorer/explorerView.tsx
@@ -89,7 +89,7 @@ function winner(stm: string, move: TablebaseMoveStats) {
 
 function showDtw(stm: string, move: TablebaseMoveStats) {
   if (move.dtm) return h('result.' + winner(stm, move), {
-    title: plural('winInXHalfMoves', Math.abs(move.dtm), Math.abs(move.dtm)),
+    title: plural('winInXHalfMoves', Math.abs(move.dtm)),
   }, 'DTW ' + Math.abs(move.dtm))
   else return null
 }
@@ -107,7 +107,7 @@ function showDtz(stm: string, move: TablebaseMoveStats) {
     else return h('result.' + winner(stm, move), i18n('pawnMove'))
   }
   else return h('result.' + winner(stm, move), {
-    title: plural('nextCaptureOrPawnMoveInXHalfMoves', Math.abs(move.dtz), Math.abs(move.dtz))
+    title: plural('nextCaptureOrPawnMoveInXHalfMoves', Math.abs(move.dtz))
   }, 'DTZ ' + Math.abs(move.dtz))
 }
 

src/ui/lobby.ts
@@ -84,8 +84,8 @@ export default {
   view() {
 
     function content() {
-      const nbPlayersStr = plural('nbPlayers', nbPlayers, nbPlayers || '?')
-      const nbGamesStr = plural('nbGamesInPlay', nbGames, nbGames || '?')
+      const nbPlayersStr = plural('nbPlayers', nbPlayers, nbPlayers ? undefined : '?')
+      const nbGamesStr = plural('nbGamesInPlay', nbGames, nbGames ? undefined : '?')
 
       if (currentSetup === null) {
         return h('div.lobby-waitingPopup', 'Something went wrong. Please try again')
@@ -133,7 +133,7 @@ function renderCustomSetup(setup: HumanSeekSetup) {
   if (timeMode === 1) {
     time = utils.displayTime(String(minutes)) + '+' + setup.increment
   } else if (timeMode === 2) {
-    time = plural('nbDays', setup.days, setup.days)
+    time = plural('nbDays', setup.days)
   } else {
     time = '∞'
   }