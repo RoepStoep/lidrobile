vovagorodok/blidraughts

Use same rating range selection as website

src/session.ts
@@ -9,7 +9,7 @@ import i18n from './i18n'
 import push from './push'
 import settings, { Prop } from './settings'
 import { TempBan, LobbyData, NowPlayingGame } from './lichess/interfaces'
-import { PlayTime } from './lichess/interfaces/user'
+import { PlayTime, Perfs } from './lichess/interfaces/user'
 import friendsApi from './lichess/friends'
 import challengesApi from './lichess/challenges'
 import storage from './storage'
@@ -45,7 +45,7 @@ export interface Session {
   readonly patron: boolean
   readonly language?: string
   readonly profile?: Profile
-  readonly perfs: any
+  readonly perfs: Perfs
   readonly createdAt: number
   readonly seenAt: number
   readonly playTime: PlayTime

src/ui/ai/newAiGame.tsx
@@ -3,7 +3,7 @@ import router from '../../router'
 import i18n from '../../i18n'
 import settings from '../../settings'
 import { prop, Prop } from '~/utils'
-import getVariant, { specialFenVariants } from '../../lichess/variant'
+import { getVariant, specialFenVariants } from '../../lichess/variant'
 import formWidgets from '../shared/form'
 import ViewOnlyBoard from '../shared/ViewOnlyBoard'
 import popupWidget from '../shared/popup'

src/ui/game/game.ts
@@ -12,7 +12,7 @@ import * as sleepUtils from '../../utils/sleep'
 import { isOnlineGameData, OnlineGameData } from '../../lichess/interfaces/game'
 import { ChallengeData } from '../../lichess/interfaces/challenge'
 import * as gameApi from '../../lichess/game'
-import variantApi from '../../lichess/variant'
+import { getVariant } from '../../lichess/variant'
 import socket from '../../socket'
 import * as helper from '../helper'
 import roundView, { viewOnlyBoardContent } from '../shared/round/view/roundView'
@@ -113,7 +113,7 @@ function loadRound(
     ) {
       sound.dong()
       vibrate.quick()
-      const variant = variantApi(data.game.variant.key)
+      const variant = getVariant(data.game.variant.key)
       const storageKey = variantStorageKey(data.game.variant.key)
       if (
         variant.alert && [1, 3].indexOf(variant.id) === -1 &&

src/ui/otb/newOtbGame.tsx
@@ -4,7 +4,7 @@ import redraw from '../../utils/redraw'
 import i18n from '../../i18n'
 import router from '../../router'
 import settings from '../../settings'
-import getVariant, { specialFenVariants } from '../../lichess/variant'
+import { getVariant, specialFenVariants } from '../../lichess/variant'
 import ViewOnlyBoard from '../shared/ViewOnlyBoard'
 import formWidgets from '../shared/form'
 import popupWidget from '../shared/popup'

RoepStoep/lidrobile

src/session.ts
@@ -9,7 +9,7 @@ import i18n from './i18n'
 import push from './push'
 import settings, { Prop } from './settings'
 import { TempBan, LobbyData, NowPlayingGame } from './lidraughts/interfaces'
-import { PlayTime } from './lidraughts/interfaces/user'
+import { PlayTime, Perfs } from './lidraughts/interfaces/user'
 import friendsApi from './lidraughts/friends'
 import challengesApi from './lidraughts/challenges'
 import storage from './storage'
@@ -46,7 +46,7 @@ export interface Session {
   readonly patron: boolean
   readonly language?: string
   readonly profile?: Profile
-  readonly perfs: any
+  readonly perfs: Perfs
   readonly createdAt: number
   readonly seenAt: number
   readonly playTime: PlayTime

src/ui/ai/newAiGame.tsx
@@ -3,7 +3,7 @@ import router from '../../router'
 import i18n from '../../i18n'
 import settings from '../../settings'
 import { prop, Prop } from '~/utils'
-import getVariant, { specialFenVariants } from '../../lidraughts/variant'
+import { getVariant, specialFenVariants } from '../../lidraughts/variant'
 import formWidgets from '../shared/form'
 import ViewOnlyBoard from '../shared/ViewOnlyBoard'
 import popupWidget from '../shared/popup'

src/ui/game/game.ts
@@ -13,7 +13,7 @@ import * as sleepUtils from '../../utils/sleep'
 import { isOnlineGameData, OnlineGameData } from '../../lidraughts/interfaces/game'
 import { ChallengeData } from '../../lidraughts/interfaces/challenge'
 import * as gameApi from '../../lidraughts/game'
-import variantApi from '../../lidraughts/variant'
+import { getVariant } from '../../lidraughts/variant'
 import socket from '../../socket'
 import * as helper from '../helper'
 import roundView, { viewOnlyBoardContent } from '../shared/round/view/roundView'
@@ -114,7 +114,7 @@ function loadRound(
     ) {
       sound.dong()
       vibrate.quick()
-      const variant = variantApi(data.game.variant.key)
+      const variant = getVariant(data.game.variant.key)
       const storageKey = variantStorageKey(data.game.variant.key)
       if (
         variant.alert && [1, 3].indexOf(variant.id) === -1 &&

src/ui/otb/newOtbGame.tsx
@@ -4,7 +4,7 @@ import redraw from '../../utils/redraw'
 import i18n from '../../i18n'
 import router from '../../router'
 import settings from '../../settings'
-import getVariant, { specialFenVariants } from '../../lidraughts/variant'
+import { getVariant, specialFenVariants } from '../../lidraughts/variant'
 import ViewOnlyBoard from '../shared/ViewOnlyBoard'
 import formWidgets from '../shared/form'
 import popupWidget from '../shared/popup'