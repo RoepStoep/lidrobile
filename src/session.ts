vovagorodok/blidraughts

Improve preferences
Add more clock and game display prefs from lichess
Revamp style

src/session.ts
@@ -11,11 +11,11 @@ import settings, { Prop } from './settings'
 import { TempBan, LobbyData, NowPlayingGame } from './lichess/interfaces'
 import { PlayTime, Perfs } from './lichess/interfaces/user'
 import friendsApi from './lichess/friends'
+import { PrefValue } from './lichess/prefs'
 import challengesApi from './lichess/challenges'
 import storage from './storage'
 import asyncStorage from './asyncStorage'
 
-type PrefValue = number | string | boolean
 interface Prefs {
   [key: string]: PrefValue
 }
@@ -120,7 +120,7 @@ function myTurnGames() {
 }
 
 function showSavedPrefToast(data: string): string {
-  Plugins.LiToast.show({ text: '✓ Your preferences have been saved on lichess server.', duration: 'short' })
+  Plugins.LiToast.show({ text: '✓ lichess.org: ' + i18n('yourPreferencesHaveBeenSaved'), duration: 'short' })
   return data
 }
 
@@ -143,7 +143,7 @@ function numValue(v: string | boolean | number): string {
 }
 
 function makeReducer(prefix: string) {
-  return function(acc: Prefs, [k, v]: [string, PrefValue]) {
+  return function(acc: Partial<Prefs>, [k, v]: [string, PrefValue]) {
     return {
       ...acc,
       [prefix + k]: numValue(v)
@@ -151,7 +151,7 @@ function makeReducer(prefix: string) {
   }
 }
 
-function savePreferences(): Promise<string> {
+const savePreferences = throttle((): Promise<string> => {
 
   const prefs = session && session.prefs || {}
 
@@ -194,9 +194,9 @@ function savePreferences(): Promise<string> {
     body: serializeQueryParameters({ ...rest, ...display, ...behavior })
   }, true)
   .then(showSavedPrefToast)
-}
+}, 1000)
 
-function lichessBackedProp<T extends string | number | boolean>(path: string, prefRequest: () => Promise<string>, defaultVal: T): Prop<T> {
+function lichessBackedProp<T extends PrefValue>(path: string, defaultVal: T): Prop<T> {
   return function() {
     if (arguments.length) {
       let oldPref: T
@@ -204,10 +204,10 @@ function lichessBackedProp<T extends string | number | boolean>(path: string, pr
         oldPref = <T>getAtPath(session, path)
         setAtPath(session, path, arguments[0])
       }
-      prefRequest()
+      savePreferences()
       .catch((err) => {
         if (session) setAtPath(session, path, oldPref)
-        handleXhrError(err)
+        if (hasNetwork()) handleXhrError(err)
       })
     }
 
@@ -357,7 +357,6 @@ export default {
   restoreStoredSession,
   refresh: throttle(refresh, 1000),
   backgroundRefresh: throttle(backgroundRefresh, 1000),
-  savePreferences: throttle(savePreferences, 1000),
   get: getSession,
   getUserId,
   appUser(fallback: string) {

src/ui/settings/clock.ts
@@ -1,0 +1,38 @@
+import h from 'mithril/hyperscript'
+import i18n from '../../i18n'
+import settings from '../../settings'
+import session from '../../session'
+import { hasNetwork } from '../../utils'
+import * as helper from '../helper'
+import { dropShadowHeader, backButton } from '../shared/common'
+import formWidgets from '../shared/form'
+import layout from '../layout'
+import { prefsCtrl, render as renderLichessPrefs } from '../user/account/clock'
+
+export default {
+  oncreate: helper.viewSlideIn,
+
+  view() {
+    const header = dropShadowHeader(null, backButton(i18n('clock')))
+    return layout.free(header,
+      h('ul.native_scroller.page.settings_list.multiChoices',
+        renderAppPrefs().concat(hasNetwork() && session.isConnected() ?
+          renderLichessPrefs(prefsCtrl) : []
+        )
+    ))
+  }
+} as Mithril.Component<{}, {}>
+
+function renderAppPrefs() {
+  return [
+    h('li.list_item',
+      formWidgets.renderMultipleChoiceButton(
+        'Clock position', [
+          { label: 'Left', value: 'left' },
+          { label: 'Right', value: 'right' },
+        ],
+        settings.game.clockPosition
+      )
+    ),
+  ]
+}

src/ui/user/account/gameBehavior.ts
@@ -13,38 +13,34 @@ export default {
   view() {
     const header = dropShadowHeader(null, backButton(i18n('gameBehavior')))
     return layout.free(header,
-      h('ul.native_scroller.page.settings_list.game', renderLichessPrefs(prefsCtrl))
+      h('ul.native_scroller.page.settings_list.game', render(prefsCtrl))
     )
   }
 } as Mithril.Component<{}, {}>
 
 export const prefsCtrl = {
-  premove: session.lichessBackedProp<boolean>('prefs.premove', session.savePreferences, true),
-  takeback: session.lichessBackedProp<number>('prefs.takeback', session.savePreferences, Takeback.ALWAYS),
-  autoQueen: session.lichessBackedProp<number>('prefs.autoQueen', session.savePreferences, AutoQueen.PREMOVE),
-  autoThreefold: session.lichessBackedProp<number>('prefs.autoThreefold', session.savePreferences, AutoThreefold.TIME),
-  submitMove: session.lichessBackedProp<number>('prefs.submitMove', session.savePreferences, SubmitMove.CORRESPONDENCE_ONLY),
+  premove: session.lichessBackedProp<boolean>('prefs.premove', true),
+  takeback: session.lichessBackedProp<number>('prefs.takeback', Takeback.ALWAYS),
+  autoQueen: session.lichessBackedProp<number>('prefs.autoQueen', AutoQueen.PREMOVE),
+  autoThreefold: session.lichessBackedProp<number>('prefs.autoThreefold', AutoThreefold.TIME),
+  submitMove: session.lichessBackedProp<number>('prefs.submitMove', SubmitMove.CORRESPONDENCE_ONLY),
 }
 
-export function renderLichessPrefs(ctrl: typeof prefsCtrl) {
+export function render(ctrl: typeof prefsCtrl) {
   return [
     h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('premovesPlayingDuringOpponentTurn'), [
-        { label: i18n('no'), value: false },
-        { label: i18n('yes'), value: true },
-      ], ctrl.premove)),
+      i18n('premovesPlayingDuringOpponentTurn'), formWidgets.booleanChoice, ctrl.premove)),
     h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('takebacksWithOpponentApproval'), TakebackChoices.map(formWidgets.lichessPropToOption), ctrl.takeback
+      i18n('takebacksWithOpponentApproval'), TakebackChoices, ctrl.takeback
     )),
     h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('promoteToQueenAutomatically'), AutoQueenChoices.map(formWidgets.lichessPropToOption), ctrl.autoQueen
+      i18n('promoteToQueenAutomatically'), AutoQueenChoices, ctrl.autoQueen
     )),
     h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('claimDrawOnThreefoldRepetitionAutomatically').replace(/\%s/g, ''), AutoThreefoldChoices.map(formWidgets.lichessPropToOption), ctrl.autoThreefold
+      i18n('claimDrawOnThreefoldRepetitionAutomatically').replace(/\%s/g, ''), AutoThreefoldChoices, ctrl.autoThreefold
     )),
-    h('li.list_item', [
-      h('div.label', i18n('moveConfirmation')),
-      h('div.select_input.no_label.settingsChoicesBlock', formWidgets.renderLichessPropSelect('', 'moveConfirmation', SubmitMoveChoices, ctrl.submitMove))
-    ]),
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('moveConfirmation'), SubmitMoveChoices, ctrl.submitMove
+    )),
   ]
 }

src/ui/user/account/privacy.ts
@@ -2,7 +2,7 @@ import h from 'mithril/hyperscript'
 import i18n from '../../../i18n'
 import session from '../../../session'
 import { Prop } from '../../../settings'
-import { LichessPropOption, ChallengeChoices, Challenge } from '../../../lichess/prefs'
+import { ChallengeChoices, Challenge } from '../../../lichess/prefs'
 import { dropShadowHeader, backButton } from '../../shared/common'
 import formWidgets from '../../shared/form'
 import layout from '../../layout'
@@ -18,8 +18,8 @@ export default {
 
   oninit() {
     this.ctrl = {
-      follow: session.lichessBackedProp<boolean>('prefs.follow', session.savePreferences, true),
-      challenge: session.lichessBackedProp<number>('prefs.challenge', session.savePreferences, Challenge.ALWAYS)
+      follow: session.lichessBackedProp<boolean>('prefs.follow', true),
+      challenge: session.lichessBackedProp<number>('prefs.challenge', Challenge.ALWAYS)
     }
   },
 
@@ -33,12 +33,8 @@ export default {
 function renderBody(ctrl: Ctrl) {
   return [
     h('ul.native_scroller.page.settings_list.game', [
-      h('li.list_item', formWidgets.renderCheckbox(i18n('letOtherPlayersFollowYou'),
-        'follow', ctrl.follow)),
-      h('li.list_item', [
-        h('div.label', i18n('letOtherPlayersChallengeYou')),
-        h('div.select_input.no_label.settingsChoicesBlock', formWidgets.renderLichessPropSelect('', 'challenge', <Array<LichessPropOption>>ChallengeChoices, ctrl.challenge))
-      ])
+      h('li.list_item', formWidgets.renderMultipleChoiceButton(i18n('letOtherPlayersFollowYou'), formWidgets.booleanChoice, ctrl.follow)),
+      h('li.list_item', formWidgets.renderMultipleChoiceButton(i18n('letOtherPlayersChallengeYou'), ChallengeChoices, ctrl.challenge))
     ])
   ]
 }

RoepStoep/lidrobile

src/session.ts
@@ -11,11 +11,11 @@ import settings, { Prop } from './settings'
 import { TempBan, LobbyData, NowPlayingGame } from './lidraughts/interfaces'
 import { PlayTime, Perfs } from './lidraughts/interfaces/user'
 import friendsApi from './lidraughts/friends'
+import { PrefValue } from './lidraughts/prefs'
 import challengesApi from './lidraughts/challenges'
 import storage from './storage'
 import asyncStorage from './asyncStorage'
 
-type PrefValue = number | string | boolean
 interface Prefs {
   [key: string]: PrefValue
 }
@@ -121,7 +121,7 @@ function myTurnGames() {
 }
 
 function showSavedPrefToast(data: string): string {
-  Plugins.LiToast.show({ text: '✓ Your preferences have been saved on lidraughts server.', duration: 'short' })
+  Plugins.LiToast.show({ text: '✓ lidraughts.org: ' + i18n('yourPreferencesHaveBeenSaved'), duration: 'short' })
   return data
 }
 
@@ -144,7 +144,7 @@ function numValue(v: string | boolean | number): string {
 }
 
 function makeReducer(prefix: string) {
-  return function(acc: Prefs, [k, v]: [string, PrefValue]) {
+  return function(acc: Partial<Prefs>, [k, v]: [string, PrefValue]) {
     return {
       ...acc,
       [prefix + k]: numValue(v)
@@ -152,7 +152,7 @@ function makeReducer(prefix: string) {
   }
 }
 
-function savePreferences(): Promise<string> {
+const savePreferences = throttle((): Promise<string> => {
 
   const prefs = session && session.prefs || {}
 
@@ -197,9 +197,9 @@ function savePreferences(): Promise<string> {
     body: serializeQueryParameters({ ...rest, ...display, ...behavior })
   }, true)
   .then(showSavedPrefToast)
-}
+}, 1000)
 
-function lidraughtsBackedProp<T extends string | number | boolean>(path: string, prefRequest: () => Promise<string>, defaultVal: T): Prop<T> {
+function lidraughtsBackedProp<T extends PrefValue>(path: string, defaultVal: T): Prop<T> {
   return function() {
     if (arguments.length) {
       let oldPref: T
@@ -207,10 +207,10 @@ function lidraughtsBackedProp<T extends string | number | boolean>(path: string,
         oldPref = <T>getAtPath(session, path)
         setAtPath(session, path, arguments[0])
       }
-      prefRequest()
+      savePreferences()
       .catch((err) => {
         if (session) setAtPath(session, path, oldPref)
-        handleXhrError(err)
+        if (hasNetwork()) handleXhrError(err)
       })
     }
 
@@ -360,7 +360,6 @@ export default {
   restoreStoredSession,
   refresh: throttle(refresh, 1000),
   backgroundRefresh: throttle(backgroundRefresh, 1000),
-  savePreferences: throttle(savePreferences, 1000),
   get: getSession,
   getUserId,
   appUser(fallback: string) {

src/ui/settings/clock.ts
@@ -1,0 +1,38 @@
+import h from 'mithril/hyperscript'
+import i18n from '../../i18n'
+import settings from '../../settings'
+import session from '../../session'
+import { hasNetwork } from '../../utils'
+import * as helper from '../helper'
+import { dropShadowHeader, backButton } from '../shared/common'
+import formWidgets from '../shared/form'
+import layout from '../layout'
+import { prefsCtrl, render as renderDraughtsPrefs } from '../user/account/clock'
+
+export default {
+  oncreate: helper.viewSlideIn,
+
+  view() {
+    const header = dropShadowHeader(null, backButton(i18n('clock')))
+    return layout.free(header,
+      h('ul.native_scroller.page.settings_list.multiChoices',
+        renderAppPrefs().concat(hasNetwork() && session.isConnected() ?
+          renderDraughtsPrefs(prefsCtrl) : []
+        )
+    ))
+  }
+} as Mithril.Component<{}, {}>
+
+function renderAppPrefs() {
+  return [
+    h('li.list_item',
+      formWidgets.renderMultipleChoiceButton(
+        'Clock position', [
+          { label: 'Left', value: 'left' },
+          { label: 'Right', value: 'right' },
+        ],
+        settings.game.clockPosition
+      )
+    ),
+  ]
+}

src/ui/user/account/gameBehavior.ts
@@ -13,34 +13,30 @@ export default {
   view() {
     const header = dropShadowHeader(null, backButton(i18n('gameBehavior')))
     return layout.free(header,
-      h('ul.native_scroller.page.settings_list.game', renderLidraughtsPrefs(prefsCtrl))
+      h('ul.native_scroller.page.settings_list.game', render(prefsCtrl))
     )
   }
 } as Mithril.Component<{}, {}>
 
 export const prefsCtrl = {
-  premove: session.lidraughtsBackedProp<boolean>('prefs.premove', session.savePreferences, true),
-  takeback: session.lidraughtsBackedProp<number>('prefs.takeback', session.savePreferences, Takeback.ALWAYS),
-  autoThreefold: session.lidraughtsBackedProp<number>('prefs.autoThreefold', session.savePreferences, AutoThreefold.TIME),
-  submitMove: session.lidraughtsBackedProp<number>('prefs.submitMove', session.savePreferences, SubmitMove.CORRESPONDENCE_ONLY),
+  premove: session.lidraughtsBackedProp<boolean>('prefs.premove', true),
+  takeback: session.lidraughtsBackedProp<number>('prefs.takeback', Takeback.ALWAYS),
+  autoThreefold: session.lidraughtsBackedProp<number>('prefs.autoThreefold', AutoThreefold.TIME),
+  submitMove: session.lidraughtsBackedProp<number>('prefs.submitMove', SubmitMove.CORRESPONDENCE_ONLY),
 }
 
-export function renderLidraughtsPrefs(ctrl: typeof prefsCtrl) {
+export function render(ctrl: typeof prefsCtrl) {
   return [
     h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('premovesPlayingDuringOpponentTurn'), [
-        { label: i18n('no'), value: false },
-        { label: i18n('yes'), value: true },
-      ], ctrl.premove)),
+      i18n('premovesPlayingDuringOpponentTurn'), formWidgets.booleanChoice, ctrl.premove)),
     h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('takebacksWithOpponentApproval'), TakebackChoices.map(formWidgets.lidraughtsPropToOption), ctrl.takeback
+      i18n('takebacksWithOpponentApproval'), TakebackChoices, ctrl.takeback
     )),
     h('li.list_item', formWidgets.renderMultipleChoiceButton(
-      i18n('claimDrawOnThreefoldRepetitionAutomatically').replace(/\%s/g, ''), AutoThreefoldChoices.map(formWidgets.lidraughtsPropToOption), ctrl.autoThreefold
+      i18n('claimDrawOnThreefoldRepetitionAutomatically').replace(/\%s/g, ''), AutoThreefoldChoices, ctrl.autoThreefold
     )),
-    h('li.list_item', [
-      h('div.label', i18n('moveConfirmation')),
-      h('div.select_input.no_label.settingsChoicesBlock', formWidgets.renderLidraughtsPropSelect('', 'moveConfirmation', SubmitMoveChoices, ctrl.submitMove))
-    ]),
+    h('li.list_item', formWidgets.renderMultipleChoiceButton(
+      i18n('moveConfirmation'), SubmitMoveChoices, ctrl.submitMove
+    )),
   ]
 }

src/ui/user/account/privacy.ts
@@ -2,7 +2,7 @@ import h from 'mithril/hyperscript'
 import i18n from '../../../i18n'
 import session from '../../../session'
 import { Prop } from '../../../settings'
-import { LidraughtsPropOption, ChallengeChoices, Challenge } from '../../../lidraughts/prefs'
+import { ChallengeChoices, Challenge } from '../../../lidraughts/prefs'
 import { dropShadowHeader, backButton } from '../../shared/common'
 import formWidgets from '../../shared/form'
 import layout from '../../layout'
@@ -18,8 +18,8 @@ export default {
 
   oninit() {
     this.ctrl = {
-      follow: session.lidraughtsBackedProp<boolean>('prefs.follow', session.savePreferences, true),
-      challenge: session.lidraughtsBackedProp<number>('prefs.challenge', session.savePreferences, Challenge.ALWAYS)
+      follow: session.lidraughtsBackedProp<boolean>('prefs.follow', true),
+      challenge: session.lidraughtsBackedProp<number>('prefs.challenge', Challenge.ALWAYS)
     }
   },
 
@@ -33,12 +33,8 @@ export default {
 function renderBody(ctrl: Ctrl) {
   return [
     h('ul.native_scroller.page.settings_list.game', [
-      h('li.list_item', formWidgets.renderCheckbox(i18n('letOtherPlayersFollowYou'),
-        'follow', ctrl.follow)),
-      h('li.list_item', [
-        h('div.label', i18n('letOtherPlayersChallengeYou')),
-        h('div.select_input.no_label.settingsChoicesBlock', formWidgets.renderLidraughtsPropSelect('', 'challenge', <Array<LidraughtsPropOption>>ChallengeChoices, ctrl.challenge))
-      ])
+      h('li.list_item', formWidgets.renderMultipleChoiceButton(i18n('letOtherPlayersFollowYou'), formWidgets.booleanChoice, ctrl.follow)),
+      h('li.list_item', formWidgets.renderMultipleChoiceButton(i18n('letOtherPlayersChallengeYou'), ChallengeChoices, ctrl.challenge))
     ])
   ]
 }