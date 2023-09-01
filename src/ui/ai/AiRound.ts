Mageek627/lichobile

More wip on sf12

src/ui/ai/AiRound.ts
@@ -87,10 +87,9 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
         this.startNewGame(currentVariant)
       }
     }
-
   }
 
-  public init(data: OfflineGameData, situations: Array<chess.GameSituation>, ply: number) {
+  private init(data: OfflineGameData, situations: Array<chess.GameSituation>, ply: number) {
     this.newGameMenu.close()
     this.actions.close()
     this.data = data
@@ -117,11 +116,8 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
       ground.reload(this.chessground, this.data, this.replay.situation())
     }
 
-    this.save()
-    redraw()
-
     if (this.engine && this.engine.variant === variant) {
-      this.engine.init()
+      this.engine.newGame()
       .then(() => {
         if (this.isEngineToMove()) {
           this.engineMove()
@@ -130,7 +126,15 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
     } else {
       this.engine = new Engine(this, variant)
       this.engine.init()
+      .then(() => {
+        if (this.isEngineToMove()) {
+          this.engineMove()
+        }
+      })
     }
+
+    this.save()
+    redraw()
   }
 
   // clockType preceded by underscore until we implement AI timed games

src/ui/ai/engine.ts
@@ -1,3 +1,4 @@
+import { Capacitor } from '@capacitor/core'
 import { AiRoundInterface } from '../shared/round'
 import { Stockfish, getNbCores, getMaxMemory } from '../../stockfish'
 
@@ -24,9 +25,7 @@ export default class Engine {
 
   constructor(readonly ctrl: AiRoundInterface, readonly variant: VariantKey) {
     this.stockfish = new Stockfish(variant)
-  }
 
-  public init() {
     this.stockfish.addListener(line => {
       const match = line.match(/^bestmove (\w{4})|^bestmove ([PNBRQ]@\w{2})/)
       if (match) {
@@ -34,14 +33,16 @@ export default class Engine {
         else if (match[2]) this.ctrl.onEngineDrop(match[2])
       }
     })
+  }
 
+  public init() {
     return this.stockfish.plugin.start()
     .then(() => {
       return this.stockfish.send('uci')
       .then(() => this.stockfish.setOption('Threads', getNbCores()))
       .then(async () => {
         const mem = await getMaxMemory()
-        this.stockfish.setOption('Hash', mem)
+        if (Capacitor.platform !== 'web') this.stockfish.setOption('Hash', mem)
       })
     })
     .catch(console.error.bind(console))

RoepStoep/lidrobile

src/ui/ai/AiRound.ts
@@ -87,10 +87,9 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
         this.startNewGame(currentVariant)
       }
     }
-
   }
 
-  public init(data: OfflineGameData, situations: Array<draughts.GameSituation>, ply: number) {
+  private init(data: OfflineGameData, situations: Array<draughts.GameSituation>, ply: number) {
     this.newGameMenu.close()
     this.actions.close()
     this.data = data
@@ -121,11 +120,8 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
       this.engineMove()
     }
 
-    this.save()
-    redraw()
-
     if (this.engine && this.engine.variant === variant) {
-      this.engine.init()
+      this.engine.newGame()
       .then(() => {
         if (this.isEngineToMove()) {
           this.engineMove()
@@ -134,7 +130,15 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
     } else {
       this.engine = new Engine(this, variant)
       this.engine.init()
+      .then(() => {
+        if (this.isEngineToMove()) {
+          this.engineMove()
+        }
+      })
     }
+
+    this.save()
+    redraw()
   }
 
   // clockType preceded by underscore until we implement AI timed games

src/ui/ai/engine.ts
@@ -1,3 +1,4 @@
+import { Capacitor } from '@capacitor/core'
 import { AiRoundInterface } from '../shared/round'
 import { Scan, getNbCores, getMaxMemory, parsePV, parseVariant, scanFen } from '../utils/scan'
 
@@ -46,23 +47,23 @@ export const levelToRating: LevelToNumber = {
 }
 
 export default class Engine {
-  private const uciCache: any = {}
+  private uciCache: any = {}
   private searchFen: string = ''
   private level = 1
   private scan: Scan
 
   constructor(readonly ctrl: AiRoundInterface, readonly variant: VariantKey) {
     this.scan = new Scan(variant)
-  }
-
-  public init() {
+  
     this.scan.addListener(line => {
       const match = line.match(/^done move=([0-9\-xX\s]+)/)
       if (match) {
         ctrl.onEngineMove(parsePV(this.searchFen, match[1], this.scan.variant === 'frisian' || this.scan.variant === 'frysk', this.uciCache)[0])
       }
     })
+  }
 
+  public init() {
     return Scan.start(parseVariant(this.scan.variant))
       .then(() => {
         return this.scan.send('hub')
@@ -70,7 +71,7 @@ export default class Engine {
           .then(() => this.scan.setOption('threads', getNbCores()))
           .then(async () => {
             const { value: mem } = await getMaxMemory()
-            this.scan.setOption('hash', mem)
+            if (Capacitor.platform !== 'web') this.scan.setOption('hash', mem)
           })
       })
       .catch(console.error.bind(console))