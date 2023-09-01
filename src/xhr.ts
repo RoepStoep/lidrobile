vovagorodok/blidraughts

Fix searching new opponent when using a hook

src/xhr.ts
@@ -10,6 +10,7 @@ import { TimelineData, LobbyData, HookData, Pool, HumanSeekSetup, Correspondence
 import { ChallengeData, ChallengesData, Challenge } from './lichess/interfaces/challenge'
 import { OnlineGameData } from './lichess/interfaces/game'
 import { MiniUser } from './lichess/interfaces/user'
+import { makeRatingRange } from './lichess/setup'
 
 interface GameSetup {
   variant: string
@@ -47,67 +48,23 @@ export function newAiGame(fen?: string | false): Promise<OnlineGameData> {
 export function seekGame(setup: HumanSeekSetup): Promise<HookData> {
   const { ratingRangeMin, ratingRangeMax, ...rest } = setup
   const user = session.get()
-  let body: string
-  if (user && ratingRangeMin !== undefined && ratingRangeMax !== undefined) {
-    let perfKey: PerfKey = 'correspondence'
-    switch (setup.variant) {
-      case 1:
-      case 3: {
-        if (setup.timeMode === 1) {
-          const time = setup.time * 60 + setup.increment * 40
-          if (time < 30) perfKey = 'ultraBullet'
-          else if (time < 180) perfKey = 'bullet'
-          else if (time < 480) perfKey = 'blitz'
-          else if (time < 1500) perfKey = 'rapid'
-          else perfKey = 'classical'
-        }
-        break
-      }
-      case 2:
-        perfKey = 'chess960'
-        break
-      case 4:
-        perfKey = 'kingOfTheHill'
-        break
-      case 5:
-        perfKey = 'threeCheck'
-        break
-      case 6:
-        perfKey = 'antichess'
-        break
-      case 7:
-        perfKey = 'atomic'
-        break
-      case 8:
-        perfKey = 'horde'
-        break
-      case 9:
-        perfKey = 'racingKings'
-        break
-      case 10:
-        perfKey = 'crazyhouse'
-        break
-    }
-    const bodySetup = {
-      ratingRange_range_min: ratingRangeMin,
-      ratingRange_range_max: ratingRangeMax,
-      ...rest,
-    }
-    const perf = user.perfs[perfKey]
-    if (perf) {
-      body = JSON.stringify({
-        ...bodySetup,
-        ratingRange: `${perf.rating + ratingRangeMin}-${perf.rating + ratingRangeMax}`,
-      })
-    } else {
-      body = JSON.stringify(bodySetup)
-    }
-  } else {
-    body = JSON.stringify({ ...rest })
-  }
+  const ratingRange = user ? makeRatingRange(user, setup) : null
+  const body = ratingRange ? {
+    ...rest,
+    ratingRange,
+  } : { ...rest }
+
   return fetchJSON('/setup/hook/' + currentSri(), {
     method: 'POST',
-    body
+    body: JSON.stringify(body),
+  }, true)
+}
+
+export function newOpponentHook(gameId: string, setup: HumanSeekSetup): Promise<HookData> {
+  const user = session.get()
+  const rr = user ? makeRatingRange(user, setup) : null
+  return fetchJSON(`/setup/hook/${currentSri()}/like/${gameId}?rr=${rr || ''}`, {
+    method: 'POST',
   }, true)
 }
 

RoepStoep/lidrobile

src/xhr.ts
@@ -10,6 +10,7 @@ import { TimelineData, LobbyData, HookData, Pool, HumanSeekSetup, Correspondence
 import { ChallengeData, ChallengesData, Challenge } from './lidraughts/interfaces/challenge'
 import { OnlineGameData } from './lidraughts/interfaces/game'
 import { MiniUser } from './lidraughts/interfaces/user'
+import { makeRatingRange } from './lidraughts/setup'
 
 interface GameSetup {
   variant: string
@@ -47,61 +48,23 @@ export function newAiGame(fen?: string): Promise<OnlineGameData> {
 export function seekGame(setup: HumanSeekSetup): Promise<HookData> {
   const { ratingRangeMin, ratingRangeMax, ...rest } = setup
   const user = session.get()
-  let body: string
-  if (user && ratingRangeMin !== undefined && ratingRangeMax !== undefined) {
-    let perfKey: PerfKey = 'correspondence'
-    switch (setup.variant) {
-      case 1:
-      case 3: {
-        if (setup.timeMode === 1) {
-          const time = setup.time * 60 + setup.increment * 40
-          if (time < 30) perfKey = 'ultraBullet'
-          else if (time < 180) perfKey = 'bullet'
-          else if (time < 480) perfKey = 'blitz'
-          else if (time < 1500) perfKey = 'rapid'
-          else perfKey = 'classical'
-        }
-        break
-      }
-      case 6:
-        perfKey = 'antidraughts'
-        break
-      case 8:
-        perfKey = 'frysk'
-        break
-      case 9:
-        perfKey = 'breakthrough'
-        break
-      case 10:
-        perfKey = 'frisian'
-        break
-      case 11:
-        perfKey = 'russian'
-        break
-      case 12:
-        perfKey = 'brazilian'
-        break
-    }
-    const bodySetup = {
-      ratingRange_range_min: ratingRangeMin,
-      ratingRange_range_max: ratingRangeMax,
-      ...rest,
-    }
-    const perf = user.perfs[perfKey]
-    if (perf) {
-      body = JSON.stringify({
-        ...bodySetup,
-        ratingRange: `${perf.rating + ratingRangeMin}-${perf.rating + ratingRangeMax}`,
-      })
-    } else {
-      body = JSON.stringify(bodySetup)
-    }
-  } else {
-    body = JSON.stringify({ ...rest })
-  }
+  const ratingRange = user ? makeRatingRange(user, setup) : null
+  const body = ratingRange ? {
+    ...rest,
+    ratingRange,
+  } : { ...rest }
+
   return fetchJSON('/setup/hook/' + currentSri(), {
     method: 'POST',
-    body
+    body: JSON.stringify(body),
+  }, true)
+}
+
+export function newOpponentHook(gameId: string, setup: HumanSeekSetup): Promise<HookData> {
+  const user = session.get()
+  const rr = user ? makeRatingRange(user, setup) : null
+  return fetchJSON(`/setup/hook/${currentSri()}/like/${gameId}?rr=${rr || ''}`, {
+    method: 'POST',
   }, true)
 }
 