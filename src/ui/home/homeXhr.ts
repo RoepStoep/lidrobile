vovagorodok/blidraughts

Add featured streamers on home page

src/ui/home/homeXhr.ts
@@ -1,5 +1,5 @@
 import { fetchJSON } from '../../http'
-import { OnlineGameData } from '../../lichess/interfaces/game'
+import { Streamer } from '../../lichess/interfaces'
 import { PuzzleData } from '../../lichess/interfaces/training'
 import { TournamentListItem } from '../../lichess/interfaces/tournament'
 
@@ -7,8 +7,8 @@ interface FeaturedTournamentData {
   featured: TournamentListItem[]
 }
 
-export function featured(feedback: boolean): Promise<OnlineGameData> {
-  return fetchJSON('/tv', undefined, feedback)
+export function featuredStreamers(): Promise<readonly Streamer[]> {
+  return fetchJSON('/api/streamer/featured', undefined)
 }
 
 export function dailyPuzzle(): Promise<PuzzleData> {

RoepStoep/lidrobile

src/ui/home/homeXhr.ts
@@ -1,14 +1,14 @@
 import { fetchJSON } from '../../http'
+import { Streamer } from '../../lidraughts/interfaces'
 import { DailyPuzzleData } from '../../lidraughts/interfaces'
-import { OnlineGameData } from '../../lidraughts/interfaces/game'
 import { TournamentListItem } from '../../lidraughts/interfaces/tournament'
 
 interface FeaturedTournamentData {
   featured: TournamentListItem[]
 }
 
-export function featured(feedback: boolean): Promise<OnlineGameData> {
-  return fetchJSON('/tv', undefined, feedback)
+export function featuredStreamers(): Promise<readonly Streamer[]> {
+  return fetchJSON('/api/streamer/featured', undefined)
 }
 
 export function dailyPuzzle(): Promise<DailyPuzzleData> {