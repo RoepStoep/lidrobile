vovagorodok/blidraughts

Stockfish client refactoring

src/ui/ai/engine.ts
@@ -1,23 +1,24 @@
 import { Capacitor } from '@capacitor/core'
 import { AiRoundInterface } from '../shared/round'
-import { StockfishWrapper, getNbCores, getMaxMemory } from '../../stockfish'
+import { StockfishPlugin, getNbCores, getMaxMemory } from '../../stockfish'
 
 export default class Engine {
   private level = 1
-  private stockfish: StockfishWrapper
+  private stockfish: StockfishPlugin
   private isInit = false
   private listener: (e: Event) => void
 
   constructor(readonly ctrl: AiRoundInterface, readonly variant: VariantKey) {
     this.listener = (e: Event) => {
       const line = (e as any).output
+      console.debug('[stockfish >>] ' + line)
       const bmMatch = line.match(/^bestmove (\w{4})|^bestmove ([PNBRQ]@\w{2})/)
       if (bmMatch) {
         if (bmMatch[1]) this.ctrl.onEngineMove(bmMatch[1])
         else if (bmMatch[2]) this.ctrl.onEngineDrop(bmMatch[2])
       }
     }
-    this.stockfish = new StockfishWrapper(variant)
+    this.stockfish = new StockfishPlugin(variant)
   }
 
   public async init(): Promise<void> {

RoepStoep/lidrobile

src/ui/ai/engine.ts
@@ -1,6 +1,6 @@
 import { Capacitor } from '@capacitor/core'
 import { AiRoundInterface } from '../shared/round'
-import { Scan, getNbCores, getMaxMemory, parsePV, parseVariant, scanFen } from '../../scan'
+import { ScanPlugin, getNbCores, getMaxMemory, parsePV, parseVariant, scanFen } from '../../scan'
 
 interface LevelToNumber {
   [index: number]: number
@@ -50,19 +50,20 @@ export default class Engine {
   private uciCache: any = {}
   private searchFen: string = ''
   private level = 1
-  private scan: Scan
+  private scan: ScanPlugin
   private isInit = false
   private listener: (e: Event) => void
 
   constructor(readonly ctrl: AiRoundInterface, readonly variant: VariantKey) {
     this.listener = (e: Event) => {
       const line = (e as any).output
+      console.debug('[scan >>] ' + line)
       const bmMatch = line.match(/^done move=([0-9\-xX\s]+)/)
       if (bmMatch) {
         this.ctrl.onEngineMove(parsePV(this.searchFen, bmMatch[1], this.scan.variant === 'frisian' || this.scan.variant === 'frysk', this.uciCache)[0])
       }
     }
-    this.scan = new Scan(variant)
+    this.scan = new ScanPlugin(variant)
   }
 
   public async init(): Promise<void> {