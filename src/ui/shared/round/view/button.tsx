vovagorodok/blidraughts

Add orientation to game share URL
Closes #1705.

src/ui/shared/round/view/button.tsx
@@ -37,7 +37,8 @@ export default {
   shareLink(ctrl: OnlineRound) {
     return h('button', {
       oncreate: helper.ontap(() => {
-        Plugins.LiShare.share({ url: gameApi.publicUrl(ctrl.data) })
+        const orientation = ctrl.chessground.state.orientation
+        Plugins.LiShare.share({ url: `${gameApi.publicUrl(ctrl.data)}/${orientation}` })
       })
     }, [i18n('shareGameURL')])
   },

RoepStoep/lidrobile

src/ui/shared/round/view/button.tsx
@@ -37,7 +37,8 @@ export default {
   shareLink(ctrl: OnlineRound) {
     return h('button', {
       oncreate: helper.ontap(() => {
-        Plugins.LiShare.share({ url: gameApi.publicUrl(ctrl.data) })
+        const orientation = ctrl.draughtsground.state.orientation
+        Plugins.LiShare.share({ url: `${gameApi.publicUrl(ctrl.data)}/${orientation}` })
       })
     }, [i18n('shareGameURL')])
   },