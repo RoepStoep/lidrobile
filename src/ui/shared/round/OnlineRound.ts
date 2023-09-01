vovagorodok/blidraughts

Cancel premove when accepting a takeback
Closes #1593.

src/ui/shared/round/OnlineRound.ts
@@ -670,6 +670,11 @@ export default class OnlineRound implements OnlineRoundInterface {
     this.appStateListener.remove()
   }
 
+  public acceptTakeback(): void {
+    this.chessground.cancelPremove()
+    this.socket.iface.send('takeback-yes')
+  }
+
   private makeCorrespondenceClock() {
     if (this.data.correspondence && !this.correspondenceClock)
       this.correspondenceClock = new CorresClockCtrl(

RoepStoep/lidrobile

src/ui/shared/round/OnlineRound.ts
@@ -640,6 +640,11 @@ export default class OnlineRound implements OnlineRoundInterface {
     this.appStateListener.remove()
   }
 
+  public acceptTakeback(): void {
+    this.draughtsground.cancelPremove()
+    this.socket.iface.send('takeback-yes')
+  }
+
   private makeCorrespondenceClock() {
     if (this.data.correspondence && !this.correspondenceClock)
       this.correspondenceClock = new CorresClockCtrl(