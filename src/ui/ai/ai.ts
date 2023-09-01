Mageek627/lichobile

Display ai round event if engine not loaded

src/ui/ai/ai.ts
@@ -45,7 +45,7 @@ export default {
   view({ attrs }) {
     let content: Mithril.Children, header: Mithril.Children
 
-    if (this.round && this.round.data && this.round.chessground && this.round.engine) {
+    if (this.round && this.round.data && this.round.chessground) {
       header = renderHeader(h(GameTitle, { data: this.round.data }))
       content = renderContent(this.round)
     } else {

RoepStoep/lidrobile

src/ui/ai/ai.ts
@@ -45,7 +45,7 @@ export default {
   view({ attrs }) {
     let content: Mithril.Children, header: Mithril.Children
 
-    if (this.round && this.round.data && this.round.draughtsground && this.round.engine) {
+    if (this.round && this.round.data && this.round.draughtsground) {
       header = renderHeader(h(GameTitle, { data: this.round.data }))
       content = renderContent(this.round)
     } else {