vovagorodok/blidraughts

Update stockfish to avoid crashes on notifyListeners

src/ui/ai/engine.ts
@@ -16,7 +16,7 @@ export default class Engine {
       if (!this.isInit) {
         await this.stockfish.start()
         this.isInit = true
-        this.stockfish.addListener(line => {
+        this.stockfish.onOutput(line => {
           const bmMatch = line.match(/^bestmove (\w{4})|^bestmove ([PNBRQ]@\w{2})/)
           if (bmMatch) {
             if (bmMatch[1]) this.ctrl.onEngineMove(bmMatch[1])

RoepStoep/lidrobile

src/ui/ai/engine.ts
@@ -62,7 +62,7 @@ export default class Engine {
       if (!this.isInit) {
         await this.scan.start(parseVariant(this.scan.variant))
         this.isInit = true 
-        this.scan.addListener(line => {
+        this.scan.onOutput(line => {
           const bmMatch = line.match(/^done move=([0-9\-xX\s]+)/)
           if (bmMatch) {
             this.ctrl.onEngineMove(parsePV(this.searchFen, bmMatch[1], this.scan.variant === 'frisian' || this.scan.variant === 'frysk', this.uciCache)[0])