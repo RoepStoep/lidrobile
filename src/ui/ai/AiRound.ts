Mageek627/lichobile

WIP on stockfish 12

src/ui/ai/AiRound.ts
@@ -142,7 +142,8 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
       payload.fen = setupFen
     }
 
-    chess.init(payload)
+    this.engine.newGame()
+    .then(() => chess.init(payload))
     .then((data: chess.InitResponse) => {
       this.init(makeData({
         id: 'offline_ai',

src/ui/ai/engine.ts
@@ -1,3 +1,4 @@
+import { Plugins } from '@capacitor/core'
 import { AiRoundInterface } from '../shared/round'
 import { send, getNbCores, setOption, setVariant } from '../../utils/stockfish'
 
@@ -20,6 +21,7 @@ const levelToDepth: LevelToDepth = {
 
 export interface EngineInterface {
   init(): Promise<void>
+  newGame(): Promise<void>
   search(initialFen: string, moves: string): void
   setLevel(level: number): Promise<void>
   prepare(variant: VariantKey): Promise<void>
@@ -31,24 +33,27 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
 
   return {
     init() {
-      return Stockfish.init()
-      .then(onInit)
-      .catch(console.error.bind(console))
-    },
-
-    search(initialFen: string, moves: string) {
-      Stockfish.output((msg: string) => {
-        console.debug('[stockfish >>] ' + msg)
-        const match = msg.match(/^bestmove (\w{4})|^bestmove ([PNBRQ]@\w{2})/)
+      Plugins.Stockfish.addListener('output', ({ line }: { line: string }) => {
+        console.debug('[stockfish >>] ' + line)
+        const match = line.match(/^bestmove (\w{4})|^bestmove ([PNBRQ]@\w{2})/)
         if (match) {
           if (match[1]) ctrl.onEngineMove(match[1])
           else if (match[2]) ctrl.onEngineDrop(match[2])
         }
       })
 
+      return Plugins.Stockfish.start()
+      .then(onInit)
+      .catch(console.error.bind(console))
+    },
+
+    newGame() {
+      return send('ucinewgame')
+    },
+
+    search(initialFen: string, moves: string) {
       // console.info('engine search pos: ', `position fen ${initialFen} moves ${moves}`)
-      setOption('Threads', getNbCores())
-      .then(() => send(`position fen ${initialFen} moves ${moves}`))
+      send(`position fen ${initialFen} moves ${moves}`)
       .then(() => send(`go movetime ${moveTime(level)} depth ${depth(level)}`))
     },
 
@@ -62,13 +67,19 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
     },
 
     exit() {
-      return Stockfish.exit()
+      Plugins.Stockfish.removeAllListeners()
+      return Plugins.Stockfish.exit()
     }
   }
 }
 
 function onInit() {
   return send('uci')
+  .then(() => setOption('Threads', getNbCores()))
+  .then(async () => {
+    const { value: mem }= await Plugins.Stockfish.getMaxMemory()
+    setOption('Hash', mem)
+  })
   .then(() => setOption('Ponder', 'false'))
 }
 

RoepStoep/lidrobile

src/ui/ai/AiRound.ts
@@ -144,7 +144,8 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
       payload.fen = setupFen
     }
 
-    draughts.init(payload)
+    this.engine.newGame(variant)
+    .then(() => draughts.init(payload))
     .then((data: draughts.InitResponse) => {
       this.init(makeData({
         id: 'offline_ai',

src/ui/ai/engine.ts
@@ -1,3 +1,4 @@
+import { Plugins } from '@capacitor/core'
 import { AiRoundInterface } from '../shared/round'
 import { send, getNbCores, setOption, parsePV, parseVariant, scanFen } from '../../utils/scan'
 
@@ -47,6 +48,7 @@ export const levelToRating: LevelToNumber = {
 
 export interface EngineInterface {
   init(variant: VariantKey): Promise<void>
+  newGame(): Promise<void>
   search(level: number, initialFen: string, currentFen: string, moves: string[]): void
   exit(): Promise<void>
   variant(): VariantKey
@@ -55,25 +57,32 @@ export interface EngineInterface {
 export default function(ctrl: AiRoundInterface): EngineInterface {
   const uciCache: any = {}
   let initVariant: VariantKey = ctrl.data ? ctrl.data.game.variant.key : 'standard'
+  let searchFen: string = ''
   let level = 1
 
   return {
     init(v: VariantKey) {
       initVariant = v
-      return Scan.init(parseVariant(initVariant))
+
+      Plugins.Scan.addListener('output', ({ line }: { line: string }) => {
+        console.debug('[scan >>] ' + line)
+        const match = line.match(/^done move=([0-9\-xX\s]+)/)
+        if (match) {
+          ctrl.onEngineMove(parsePV(searchFen, match[1], initVariant === 'frisian' || initVariant === 'frysk', uciCache)[0])
+        }
+      })
+
+      return Plugins.Scan.start(parseVariant(initVariant))
         .then(onInit)
         .catch(console.error.bind(console))
     },
 
-    search(l: number, initialFen: string, currentFen: string, moves: string[]) {
-      Scan.output((msg: string) => {
-        console.debug('[scan >>] ' + msg)
-        const match = msg.match(/^done move=([0-9\-xX\s]+)/)
-        if (match) {
-          ctrl.onEngineMove(parsePV(currentFen, match[1], initVariant === 'frisian' || initVariant === 'frysk', uciCache)[0])
-        }
-      })
+    newGame() {
+      return send('new-game')
+    },
 
+    search(l: number, initialFen: string, currentFen: string, moves: string[]) {
+      searchFen = currentFen
       initialFen = scanFen(initialFen)
       level = l
 
@@ -119,9 +128,7 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
       });
 
       // TODO: Movetimes might need a different approach
-      setOption('threads', getNbCores())
-        .then(() => send('new-game'))
-        .then(() => send('pos pos=' + initialFen + (scanMoves.length != 0 ? (' moves="' + scanMoves.join(' ') + '"') : '')))
+      send('pos pos=' + initialFen + (scanMoves.length != 0 ? (' moves="' + scanMoves.join(' ') + '"') : ''))
         .then(() => handicap ? send(`level handicap=${handicap}`) : Promise.resolve())
         .then(() => ply ? send(`level ply=${ply}`) : Promise.resolve())
         .then(() => nodes ? send(`level nodes=${nodes}`) : Promise.resolve())
@@ -134,7 +141,8 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
     },
 
     exit() {
-      return Scan.exit()
+      Plugins.Scan.removeAllListeners()
+      return Plugins.Scan.exit()
     },
 
     variant() { 
@@ -146,9 +154,9 @@ export default function(ctrl: AiRoundInterface): EngineInterface {
 function onInit() {
   return send('hub')
     .then(() => send('init'))
+    .then(() => setOption('threads', getNbCores()))
+    .then(async () => {
+      const { value: mem }= await Plugins.Scan.getMaxMemory()
+      setOption('hash', mem)
+    })
 }
-
-/*function moveTime(level: number) {
-  const maxMoveTime = 8000
-  return level * maxMoveTime / 8
-}*/