vovagorodok/blidraughts

Don't set treads and hash before each search

src/ui/analyse/ceval/CevalCtrl.ts
@@ -1,6 +1,5 @@
 import settings from '../../../settings'
 import { Tree } from '../../shared/tree'
-import { getMaxMemory, getNbCores } from '../../../stockfish'
 import StockfishEngine from './StockfishEngine'
 import { Opts, Work, ICevalCtrl } from './interfaces'
 
@@ -14,7 +13,7 @@ export default function CevalCtrl(
   const minDepth = 6
   const maxDepth = 22
 
-  const engine = StockfishEngine(opts.variant)
+  const engine = StockfishEngine(opts.variant, opts.cores, opts.hashSize)
 
   let started = false
   let isEnabled = settings.analyse.enableCeval()
@@ -31,7 +30,6 @@ export default function CevalCtrl(
     if (!enabled()) {
       return
     }
-    const hash = await getMaxMemory()
     const step = nodes[nodes.length - 1]
     if (step.ceval && step.ceval.depth >= maxDepth) {
       return
@@ -41,8 +39,6 @@ export default function CevalCtrl(
       currentFen: step.fen,
       moves: nodes.slice(1).map((s) => fixCastle(s.uci!, s.san!)),
       maxDepth: forceMaxLevel ? 18 : effectiveMaxDepth(),
-      cores: forceMaxLevel ? getNbCores() : opts.cores,
-      hash,
       path,
       ply: step.ply,
       multiPv: forceMaxLevel ? 1 : opts.multiPv,

RoepStoep/lidrobile

src/ui/analyse/ceval/CevalCtrl.ts
@@ -1,6 +1,5 @@
 import settings from '../../../settings'
 import { Tree } from '../../shared/tree'
-import { getMaxMemory, getNbCores } from '../../../scan'
 import ScanEngine from './ScanEngine'
 import { Opts, Work, ICevalCtrl } from './interfaces'
 
@@ -14,7 +13,7 @@ export default function CevalCtrl(
   const minDepth = 6
   const maxDepth = opts.variant === 'antidraughts' ? 11 : 22
 
-  const engine = ScanEngine(opts.variant)
+  const engine = ScanEngine(opts.variant, opts.cores, opts.hashSize)
 
   let started = false
   let isEnabled = settings.analyse.enableCeval()
@@ -31,7 +30,6 @@ export default function CevalCtrl(
     if (!enabled()) {
       return
     }
-    const hash = await getMaxMemory()
     const step = nodes[nodes.length - 1]
     if (step.ceval && step.ceval.depth >= effectiveMaxDepth()) {
       return
@@ -41,8 +39,6 @@ export default function CevalCtrl(
       currentFen: step.fen,
       moves: new Array<string>(),
       maxDepth: forceMaxLevel ? 18 : effectiveMaxDepth(),
-      cores: forceMaxLevel ? getNbCores() : opts.cores,
-      hash,
       path,
       ply: step.ply,
       multiPv: 1, // forceMaxLevel ? 1 : opts.multiPv,