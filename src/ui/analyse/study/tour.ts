vovagorodok/blidraughts

Enforce single quotes

src/ui/analyse/study/tour.ts
@@ -22,9 +22,9 @@ export default function startTour(ctrl: StudyCtrl) {
 
     tour.addStep('members', {
       title: 'Study members',
-      text: `<i data-icon='v'></i> Spectators can view the study and talk in the chat.<br>` +
-        `<br><i class='fa fa-user'></i> Contributors can make moves and update the study.` +
-        `<br><br>Studies are for now read-only in the application, so you must go to lichess.org to contribute to them. More features will come later in lichess app.`,
+      text: '<i data-icon=\'v\'></i> Spectators can view the study and talk in the chat.<br>' +
+        '<br><i class=\'fa fa-user\'></i> Contributors can make moves and update the study.' +
+        '<br><br>Studies are for now read-only in the application, so you must go to lichess.org to contribute to them. More features will come later in lichess app.',
       when: {
         'before-show': () => {
           ctrl.sideMenu.open()

RoepStoep/lidrobile

src/ui/analyse/study/tour.ts
@@ -22,9 +22,9 @@ export default function startTour(ctrl: StudyCtrl) {
 
     tour.addStep('members', {
       title: 'Study members',
-      text: `<i data-icon='v'></i> Spectators can view the study and talk in the chat.<br>` +
-        `<br><i class='fa fa-user'></i> Contributors can make moves and update the study.` +
-        `<br><br>Studies are for now read-only in the application, so you must go to lidraughts.org to contribute to them. More features will come later in lidraughts app.`,
+      text: '<i data-icon=\'v\'></i> Spectators can view the study and talk in the chat.<br>' +
+        '<br><i class=\'fa fa-user\'></i> Contributors can make moves and update the study.' +
+        '<br><br>Studies are for now read-only in the application, so you must go to lidraughts.org to contribute to them. More features will come later in lidraughts app.',
       when: {
         'before-show': () => {
           ctrl.sideMenu.open()