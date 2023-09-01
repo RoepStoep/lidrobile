vovagorodok/blidraughts

Stronger types for mini user XHR

src/ui/shared/PlayerPopup.tsx
@@ -8,7 +8,7 @@ import i18n, { i18nVdom } from '../../i18n'
 import spinner from '../../spinner'
 import { playerName } from '../../lichess/player'
 import { Player } from '../../lichess/interfaces/game'
-import { Score } from '../../lichess/interfaces/user'
+import { MiniUser, Score } from '../../lichess/interfaces/user'
 import * as helper from '../helper'
 
 import popupWidget from './popup'
@@ -39,7 +39,7 @@ export default {
   }
 } as Mithril.Component<Attrs>
 
-function content(mini: any, player: Player, opponent: Player, score?: Score) {
+function content(mini: MiniUser, player: Player, opponent: Player, score?: Score) {
   const user = player.user
   if (!mini || !user) {
     return (
@@ -92,7 +92,7 @@ function content(mini: any, player: Player, opponent: Player, score?: Score) {
       }
       { sessionUserId !== undefined && showYourScore ?
         <div className="score_wrapper">
-          {i18nVdom('yourScore', <span className="score">{`${mini.crosstable.users[sessionUserId]} - ${mini.crosstable.users[user.id]}`}</span>)}
+          {i18nVdom('yourScore', <span className="score">{`${mini.crosstable!.users[sessionUserId]} - ${mini.crosstable!.users[user.id]}`}</span>)}
         </div> : null
       }
       { !showYourScore && oppUser && score && score.nbGames > 0 ?

src/xhr.ts
@@ -9,6 +9,7 @@ import session from './session'
 import { TimelineData, LobbyData, HookData, Pool, HumanSeekSetup, CorrespondenceSeek, ApiStatus } from './lichess/interfaces'
 import { ChallengeData, ChallengesData, Challenge } from './lichess/interfaces/challenge'
 import { OnlineGameData } from './lichess/interfaces/game'
+import { MiniUser } from './lichess/interfaces/user'
 
 interface GameSetup {
   variant: string
@@ -207,7 +208,7 @@ export function setServerLang(lang: string): Promise<void> {
   }
 }
 
-export function miniUser(userId: string) {
+export function miniUser(userId: string): Promise<MiniUser> {
   return fetchJSON(`/@/${userId}/mini`)
 }
 

RoepStoep/lidrobile

src/ui/shared/PlayerPopup.tsx
@@ -7,7 +7,7 @@ import i18n, { i18nVdom } from '../../i18n'
 import spinner from '../../spinner'
 import { playerName } from '../../lidraughts/player'
 import { Player } from '../../lidraughts/interfaces/game'
-import { Score } from '../../lidraughts/interfaces/user'
+import { MiniUser, Score } from '../../lidraughts/interfaces/user'
 import * as helper from '../helper'
 
 import popupWidget from './popup'
@@ -38,7 +38,7 @@ export default {
   }
 } as Mithril.Component<Attrs>
 
-function content(mini: any, player: Player, opponent: Player, score?: Score) {
+function content(mini: MiniUser, player: Player, opponent: Player, score?: Score) {
   const user = player.user
   if (!mini || !user) {
     return (
@@ -87,7 +87,7 @@ function content(mini: any, player: Player, opponent: Player, score?: Score) {
       }
       { sessionUserId !== undefined && showYourScore ?
         <div className="score_wrapper">
-          {i18nVdom('yourScore', <span className="score">{`${mini.crosstable.users[sessionUserId]} - ${mini.crosstable.users[user.id]}`}</span>)}
+          {i18nVdom('yourScore', <span className="score">{`${mini.crosstable!.users[sessionUserId]} - ${mini.crosstable!.users[user.id]}`}</span>)}
         </div> : null
       }
       { !showYourScore && oppUser && score && score.nbGames > 0 ?

src/xhr.ts
@@ -9,6 +9,7 @@ import session from './session'
 import { TimelineData, LobbyData, HookData, Pool, HumanSeekSetup, CorrespondenceSeek, ApiStatus } from './lidraughts/interfaces'
 import { ChallengeData, ChallengesData, Challenge } from './lidraughts/interfaces/challenge'
 import { OnlineGameData } from './lidraughts/interfaces/game'
+import { MiniUser } from './lidraughts/interfaces/user'
 
 interface GameSetup {
   variant: string
@@ -201,7 +202,7 @@ export function setServerLang(lang: string): Promise<void> {
   }
 }
 
-export function miniUser(userId: string) {
+export function miniUser(userId: string): Promise<MiniUser> {
   return fetchJSON(`/@/${userId}/mini`)
 }
 