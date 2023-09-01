vovagorodok/blidraughts

Wip on home featured game

src/ui/home/HomeCtrl.ts
@@ -1,15 +1,17 @@
 import { Plugins, AppState, NetworkStatus, PluginListenerHandle } from '@capacitor/core'
+import throttle from 'lodash-es/throttle'
 import debounce from 'lodash-es/debounce'
 import Zanimo from '../../utils/zanimo'
 import socket, { SocketIFace } from '../../socket'
 import redraw from '../../utils/redraw'
 import signals from '../../signals'
 import settings from '../../settings'
-import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr } from '../../xhr'
+import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr, featured as featuredGameXhr } from '../../xhr'
 import { hasNetwork, noop } from '../../utils'
 import { fromNow } from '../../i18n'
 import { isForeground } from '../../utils/appMode'
-import { Streamer, PongMessage, TimelineEntry, CorrespondenceSeek } from '../../lichess/interfaces'
+import { Streamer, PongMessage, TimelineEntry, CorrespondenceSeek, FeaturedGame2, FeaturedPlayer } from '../../lichess/interfaces'
+import { Player } from '../../lichess/interfaces/game'
 import { TournamentListItem } from '../../lichess/interfaces/tournament'
 import { PuzzleData } from '../../lichess/interfaces/training'
 import session from '../../session'
@@ -18,6 +20,19 @@ import offlinePuzzleDB from '../training/database'
 import { loadNewPuzzle } from '../training/offlineService'
 
 import { dailyPuzzle as dailyPuzzleXhr, featuredTournaments as featuredTournamentsXhr, featuredStreamers as featuredStreamersXhr } from './homeXhr'
+
+interface FeaturedFenData {
+  bc: number
+  fen: string
+  id: string
+  lm: string
+  wc: number
+}
+
+interface FeaturedFinished {
+  id: string
+  win?: 'b' | 'w'
+}
 
 export default class HomeCtrl {
   public selectedTab: number
@@ -28,6 +43,7 @@ export default class HomeCtrl {
 
   public corresPool: ReadonlyArray<CorrespondenceSeek>
   public dailyPuzzle?: PuzzleData
+  public featuredGame?: FeaturedGame2
   public featuredTournaments?: readonly TournamentListItem[]
   public featuredStreamers?: readonly Streamer[]
   public timeline?: readonly TimelineEntry[]
@@ -81,7 +97,10 @@ export default class HomeCtrl {
 
   public init = () => {
     if (isForeground()) {
-      this.socket = socket.createLobby('homeLobby', this.reloadCorresPool, {
+      this.socket = socket.createLobby('homeLobby', () => {
+        this.reloadCorresPool()
+        this.getFeatured()
+      }, {
         redirect: socket.redirectToGame,
         reload_seeks: this.reloadCorresPool,
         resync: () => lobbyXhr().then(d => {
@@ -89,7 +108,29 @@ export default class HomeCtrl {
         }),
         n: (_: any, d: PongMessage) => {
           signals.homePong.dispatch(d)
-        }
+        },
+        featured: () => {
+          this.getFeatured()
+        },
+        fen: (d: FeaturedFenData) => {
+          if (this.featuredGame && this.featuredGame.id === d.id) {
+            this.featuredGame.fen = d.fen
+            this.featuredGame.lastMove = d.lm
+            this.featuredGame.c = {
+              black: d.bc,
+              white: d.wc,
+            }
+            redraw()
+          }
+        },
+        finish: (d: FeaturedFinished) => {
+          if (this.featuredGame && this.featuredGame.id === d.id) {
+            this.featuredGame.finished = true
+            if (d.win === 'b') this.featuredGame.winner = 'black'
+            else if (d.win === 'w') this.featuredGame.winner = 'white'
+            redraw()
+          }
+        },
       })
 
       Promise.all([
@@ -172,6 +213,31 @@ export default class HomeCtrl {
       })
     }
   }
+
+  private getFeatured = throttle(() => {
+    featuredGameXhr('best', false)
+    .then((data) => {
+      const opp = playerToFeatured(data.opponent)
+      const player = playerToFeatured(data.player)
+
+      this.featuredGame = {
+        c: data.clock ? {
+          white: Math.round(data.clock.white),
+          black: Math.round(data.clock.black),
+        } : undefined,
+        black: data.game.player === 'white' ? opp : player,
+        orientation: data.orientation,
+        fen: data.game.fen,
+        id: data.game.id,
+        lastMove: data.game.lastMove,
+        white: data.game.player === 'white' ? player : opp,
+      }
+
+      this.socketSend('startWatching', data.game.id)
+
+      this.redrawIfNotScrolling()
+    })
+  }, 1000)
 }
 
 function seekUserId(seek: CorrespondenceSeek) {
@@ -195,4 +261,14 @@ function fixSeeks(seeks: CorrespondenceSeek[]): CorrespondenceSeek[] {
     .filter(([_, key], i, a) => a.map(e => e[1]).indexOf(key) === i)
     .map(([s]) => s)
   ] as CorrespondenceSeek[]
+}
+
+function playerToFeatured(p: Player): FeaturedPlayer {
+  return {
+    name: p.name || p.username || p.user && p.user.username || '',
+    rating: p.rating || 0,
+    ratingDiff: p.ratingDiff || 0,
+    berserk: p.berserk,
+    title: p.user && p.user.title,
+  }
 }

src/ui/shared/miniBoard.tsx
@@ -1,16 +1,17 @@
-import * as helper from '../helper'
-import ViewOnlyBoard from './ViewOnlyBoard'
-import { noop } from '../../utils'
-import { FeaturedGame } from '../../lichess/interfaces'
-import { time as renderTime } from '../../lichess/game'
 import h from 'mithril/hyperscript'
+import * as helper from '../helper'
+import { noop, formatTimeInSecs } from '../../utils'
+import { FeaturedGame2 } from '../../lichess/interfaces'
+import ViewOnlyBoard from './ViewOnlyBoard'
+import CountdownTimer from './CountdownTimer'
 
 export interface Attrs {
   readonly fen: string
   readonly orientation: Color
   readonly link?: () => void
-  readonly gameObj?: FeaturedGame
-  readonly boardTitle?: Mithril.Children
+  readonly gameObj?: FeaturedGame2
+  readonly topText?: string
+  readonly bottomText?: string
   readonly lastMove?: string
   readonly customPieceTheme?: string
   readonly variant?: VariantKey
@@ -30,10 +31,15 @@ const MiniBoard: Mithril.Component<Attrs, State> = {
   },
   view({ attrs }) {
 
-    const { gameObj, boardTitle } = attrs
+    const { gameObj, topText, bottomText } = attrs
+    const isWhite = gameObj?.orientation === 'white'
 
     return (
       <div className="mini_board_container">
+        {gameObj ?
+          renderPlayer(gameObj, isWhite ? 'black' : 'white') :
+          topText ? h('div.mini_board__text', topText) : null
+        }
         <div className="mini_board" oncreate={helper.ontapY(() => this.link())}>
           <div className="mini_board_helper">
             <div className="mini_board_wrapper">
@@ -41,47 +47,44 @@ const MiniBoard: Mithril.Component<Attrs, State> = {
             </div>
           </div>
         </div>
-        { gameObj ?
-          renderVsBloc(gameObj) : boardTitle ?
-            <div className="vsbloc">
-              {boardTitle}
-            </div> : null
+        {gameObj ?
+          renderPlayer(gameObj, isWhite ? 'white' : 'black') :
+          bottomText ? h('div.mini_board__text', bottomText) : null
         }
       </div>
     )
   }
 }
 
-function renderVsBloc(gameObj: FeaturedGame) {
-  const player = gameObj.orientation === 'white' ? gameObj.white : gameObj.black
-  const opponent = gameObj.orientation === 'white' ? gameObj.black : gameObj.white
-  return (
-    <div className="vsbloc">
-      <div className="antagonists">
-        <div className="player">
-          {player.rank ? `#${player.rank} ` : ''}
-          {player.name}
-          <br/>
-          {player.title ? <span className="userTitle">{player.title}&nbsp;</span> : null}
-          {player.rating}
-          {player.berserk ? <span className="berserk" data-icon="`" /> : null }
-        </div>
-        { gameObj.clock ?
-          <div className="time">
-            {renderTime(gameObj)}
-          </div> : null
-        }
-        <div className="opponent">
-          {opponent.rank ? `#${opponent.rank} ` : ''}
-          {opponent.name}
-          <br/>
-          {opponent.title ? <span className="userTitle">{opponent.title}&nbsp;</span> : null}
-          {opponent.rating}
-          {opponent.berserk ? <span className="berserk" data-icon="`" /> : null }
-        </div>
-      </div>
-    </div>
-  )
+function fenColor(fen: string) {
+  return fen.indexOf(' b') > 0 ? 'black' : 'white'
+}
+
+function renderPlayer(gameObj: FeaturedGame2, color: Color) {
+  const player = gameObj[color]
+  const time = gameObj.c && gameObj.c[color]
+  const turn = fenColor(gameObj.fen)
+  return h('div.mini_board__player', [
+    h('span.mini_board__user', [
+      player.rank ? `#${player.rank} ` : '',
+      player.title ? h('span.userTitle', player.title + ' ') : null,
+      player.name,
+      h('span.rating', player.rating),
+      player.berserk ? h('span.berserk[data-icon=`]') : null,
+    ]),
+    gameObj.finished ? renderScore(color, gameObj.winner) :
+      time && !isNaN(time) ? renderTime(color, time, turn) : null
+  ])
+}
+
+function renderScore(color: Color, winner?: Color) {
+  return h('span.score', winner ? (color === winner ? '1' : '0') : '½')
+}
+
+function renderTime(color: Color, time: number, turnColor: Color) {
+  return turnColor === color ?
+    h(CountdownTimer, { seconds: time }) :
+    h('span.mini_board__clock', formatTimeInSecs(time))
 }
 
 export default MiniBoard

RoepStoep/lidrobile

src/ui/home/HomeCtrl.ts
@@ -1,15 +1,17 @@
 import { Plugins, AppState, NetworkStatus, PluginListenerHandle } from '@capacitor/core'
+import throttle from 'lodash-es/throttle'
 import debounce from 'lodash-es/debounce'
 import Zanimo from '../../utils/zanimo'
 import socket, { SocketIFace } from '../../socket'
 import redraw from '../../utils/redraw'
 import signals from '../../signals'
 import settings from '../../settings'
-import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr } from '../../xhr'
+import { timeline as timelineXhr, seeks as corresSeeksXhr, lobby as lobbyXhr, featured as featuredGameXhr } from '../../xhr'
 import { hasNetwork, noop } from '../../utils'
 import { fromNow } from '../../i18n'
 import { isForeground } from '../../utils/appMode'
-import { Streamer, PongMessage, TimelineEntry, DailyPuzzleData, CorrespondenceSeek, } from '../../lidraughts/interfaces'
+import { Streamer, PongMessage, TimelineEntry, DailyPuzzleData, CorrespondenceSeek, FeaturedGame2, FeaturedPlayer } from '../../lidraughts/interfaces'
+import { Player } from '../../lidraughts/interfaces/game'
 import { TournamentListItem } from '../../lidraughts/interfaces/tournament'
 import { PuzzleData } from '../../lidraughts/interfaces/training'
 import session from '../../session'
@@ -18,6 +20,19 @@ import offlinePuzzleDB from '../training/database'
 import { loadNewPuzzle } from '../training/offlineService'
 
 import { dailyPuzzle as dailyPuzzleXhr, featuredTournaments as featuredTournamentsXhr, featuredStreamers as featuredStreamersXhr } from './homeXhr'
+
+interface FeaturedFenData {
+  bc: number
+  fen: string
+  id: string
+  lm: string
+  wc: number
+}
+
+interface FeaturedFinished {
+  id: string
+  win?: 'b' | 'w'
+}
 
 export default class HomeCtrl {
   public selectedTab: number
@@ -28,6 +43,7 @@ export default class HomeCtrl {
 
   public corresPool: ReadonlyArray<CorrespondenceSeek>
   public dailyPuzzle?: DailyPuzzleData
+  public featuredGame?: FeaturedGame2
   public featuredTournaments?: readonly TournamentListItem[]
   public featuredStreamers?: readonly Streamer[]
   public timeline?: readonly TimelineEntry[]
@@ -81,7 +97,10 @@ export default class HomeCtrl {
 
   public init = () => {
     if (isForeground()) {
-      this.socket = socket.createLobby('homeLobby', this.reloadCorresPool, {
+      this.socket = socket.createLobby('homeLobby', () => {
+        this.reloadCorresPool()
+        this.getFeatured()
+      }, {
         redirect: socket.redirectToGame,
         reload_seeks: this.reloadCorresPool,
         resync: () => lobbyXhr().then(d => {
@@ -89,7 +108,29 @@ export default class HomeCtrl {
         }),
         n: (_: any, d: PongMessage) => {
           signals.homePong.dispatch(d)
-        }
+        },
+        featured: () => {
+          this.getFeatured()
+        },
+        fen: (d: FeaturedFenData) => {
+          if (this.featuredGame && this.featuredGame.id === d.id) {
+            this.featuredGame.fen = d.fen
+            this.featuredGame.lastMove = d.lm
+            this.featuredGame.c = {
+              black: d.bc,
+              white: d.wc,
+            }
+            redraw()
+          }
+        },
+        finish: (d: FeaturedFinished) => {
+          if (this.featuredGame && this.featuredGame.id === d.id) {
+            this.featuredGame.finished = true
+            if (d.win === 'b') this.featuredGame.winner = 'black'
+            else if (d.win === 'w') this.featuredGame.winner = 'white'
+            redraw()
+          }
+        },
       })
 
       Promise.all([
@@ -172,6 +213,32 @@ export default class HomeCtrl {
       })
     }
   }
+
+  private getFeatured = throttle(() => {
+    featuredGameXhr('best', false)
+    .then((data) => {
+      const opp = playerToFeatured(data.opponent)
+      const player = playerToFeatured(data.player)
+
+      this.featuredGame = {
+        c: data.clock ? {
+          white: Math.round(data.clock.white),
+          black: Math.round(data.clock.black),
+        } : undefined,
+        black: data.game.player === 'white' ? opp : player,
+        orientation: data.orientation,
+        fen: data.game.fen,
+        variant: data.game.variant.key,
+        id: data.game.id,
+        lastMove: data.game.lastMove,
+        white: data.game.player === 'white' ? player : opp,
+      }
+
+      this.socketSend('startWatching', data.game.id)
+
+      this.redrawIfNotScrolling()
+    })
+  }, 1000)
 }
 
 function seekUserId(seek: CorrespondenceSeek) {
@@ -195,4 +262,14 @@ function fixSeeks(seeks: CorrespondenceSeek[]): CorrespondenceSeek[] {
     .filter(([_, key], i, a) => a.map(e => e[1]).indexOf(key) === i)
     .map(([s]) => s)
   ] as CorrespondenceSeek[]
+}
+
+function playerToFeatured(p: Player): FeaturedPlayer {
+  return {
+    name: p.name || p.username || p.user && p.user.username || '',
+    rating: p.rating || 0,
+    ratingDiff: p.ratingDiff || 0,
+    berserk: p.berserk,
+    title: p.user && p.user.title,
+  }
 }

src/ui/shared/miniBoard.tsx
@@ -1,16 +1,17 @@
-import * as helper from '../helper'
-import ViewOnlyBoard from './ViewOnlyBoard'
-import { noop } from '../../utils'
-import { FeaturedGame } from '../../lidraughts/interfaces'
-import { time as renderTime } from '../../lidraughts/game'
 import h from 'mithril/hyperscript'
+import * as helper from '../helper'
+import { noop, formatTimeInSecs } from '../../utils'
+import { FeaturedGame2 } from '../../lidraughts/interfaces'
+import ViewOnlyBoard from './ViewOnlyBoard'
+import CountdownTimer from './CountdownTimer'
 
 export interface Attrs {
   readonly fen: string
   readonly orientation: Color
   readonly link?: () => void
-  readonly gameObj?: FeaturedGame
-  readonly boardTitle?: Mithril.Children
+  readonly gameObj?: FeaturedGame2
+  readonly topText?: string
+  readonly bottomText?: string
   readonly lastMove?: string
   readonly customPieceTheme?: string
   readonly variant: VariantKey
@@ -30,10 +31,15 @@ const MiniBoard: Mithril.Component<Attrs, State> = {
   },
   view({ attrs }) {
 
-    const { gameObj, boardTitle } = attrs
+    const { gameObj, topText, bottomText } = attrs
+    const isWhite = gameObj?.orientation === 'white'
 
     return (
       <div className="mini_board_container">
+        {gameObj ?
+          renderPlayer(gameObj, isWhite ? 'black' : 'white') :
+          topText ? h('div.mini_board__text', topText) : null
+        }
         <div className="mini_board" oncreate={helper.ontapY(() => this.link())}>
           <div className="mini_board_helper">
             <div className="mini_board_wrapper">
@@ -41,47 +47,44 @@ const MiniBoard: Mithril.Component<Attrs, State> = {
             </div>
           </div>
         </div>
-        { gameObj ?
-          renderVsBloc(gameObj) : boardTitle ?
-            <div className="vsbloc">
-              {boardTitle}
-            </div> : null
+        {gameObj ?
+          renderPlayer(gameObj, isWhite ? 'white' : 'black') :
+          bottomText ? h('div.mini_board__text', bottomText) : null
         }
       </div>
     )
   }
 }
 
-function renderVsBloc(gameObj: FeaturedGame) {
-  const player = gameObj.orientation === 'white' ? gameObj.white : gameObj.black
-  const opponent = gameObj.orientation === 'white' ? gameObj.black : gameObj.white
-  return (
-    <div className="vsbloc">
-      <div className="antagonists">
-        <div className="player">
-          {player.rank ? `#${player.rank} ` : ''}
-          {player.name}
-          <br/>
-          {player.title ? <span className="userTitle">{player.title}&nbsp;</span> : null}
-          {player.rating}
-          {player.berserk ? <span className="berserk" data-icon="`" /> : null }
-        </div>
-        { gameObj.clock ?
-          <div className="time">
-            {renderTime(gameObj)}
-          </div> : null
-        }
-        <div className="opponent">
-          {opponent.rank ? `#${opponent.rank} ` : ''}
-          {opponent.name}
-          <br/>
-          {opponent.title ? <span className="userTitle">{opponent.title}&nbsp;</span> : null}
-          {opponent.rating}
-          {opponent.berserk ? <span className="berserk" data-icon="`" /> : null }
-        </div>
-      </div>
-    </div>
-  )
+function fenColor(fen: string) {
+  return fen.indexOf(' b') > 0 ? 'black' : 'white'
+}
+
+function renderPlayer(gameObj: FeaturedGame2, color: Color) {
+  const player = gameObj[color]
+  const time = gameObj.c && gameObj.c[color]
+  const turn = fenColor(gameObj.fen)
+  return h('div.mini_board__player', [
+    h('span.mini_board__user', [
+      player.rank ? `#${player.rank} ` : '',
+      player.title ? h('span.userTitle', player.title + ' ') : null,
+      player.name,
+      h('span.rating', player.rating),
+      player.berserk ? h('span.berserk[data-icon=`]') : null,
+    ]),
+    gameObj.finished ? renderScore(color, gameObj.winner) :
+      time && !isNaN(time) ? renderTime(color, time, turn) : null
+  ])
+}
+
+function renderScore(color: Color, winner?: Color) {
+  return h('span.score', winner ? (color === winner ? '1' : '0') : '½')
+}
+
+function renderTime(color: Color, time: number, turnColor: Color) {
+  return turnColor === color ?
+    h(CountdownTimer, { seconds: time }) :
+    h('span.mini_board__clock', formatTimeInSecs(time))
 }
 
 export default MiniBoard