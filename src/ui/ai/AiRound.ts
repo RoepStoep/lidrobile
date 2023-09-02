vovagorodok/blidraughts

WIP on migrating to capacitor 3.0

src/ui/ai/AiRound.ts
@@ -1,4 +1,5 @@
 import { Plugins } from '@capacitor/core'
+import { Share } from '@capacitor/share'
 import i18n from '../../i18n'
 import Chessground from '../../chessground/Chessground'
 import router from '../../router'
@@ -184,7 +185,7 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
   public sharePGN = () => {
     this.replay.pgn(this.white(), this.black())
     .then((data: chess.PgnDumpResponse) =>
-      Plugins.LiShare.share({ text: data.pgn })
+      Share.share({ text: data.pgn })
     )
   }
 

src/ui/shared/offlineRound/view.tsx
@@ -1,4 +1,4 @@
-import { Plugins } from '@capacitor/core'
+import { Toast } from '@capacitor/toast'
 import h from 'mithril/hyperscript'
 import * as utils from '../../../utils'
 import i18n from '../../../i18n'
@@ -87,13 +87,13 @@ export function renderGameActionsBar(ctrl: OfflineRoundInterface) {
       <button className="action_bar_button fa fa-plus-circle"
         oncreate={helper.ontap(
           ctrl.newGameMenu.open,
-          () => Plugins.LiToast.show({ text: i18n('createAGame'), duration: 'short', position: 'bottom' })
+          () => Toast.show({ text: i18n('createAGame'), duration: 'short', position: 'bottom' })
         )}
       />
       <button className="fa fa-share-alt action_bar_button"
         oncreate={helper.ontap(
           ctrl.sharePGN,
-          () => Plugins.LiToast.show({ text: i18n('sharePGN'), duration: 'short', position: 'bottom' })
+          () => Toast.show({ text: i18n('sharePGN'), duration: 'short', position: 'bottom' })
         )}
       />
       <button className="action_bar_button" data-icon="A"

RoepStoep/lidrobile

src/ui/ai/AiRound.ts
@@ -1,4 +1,5 @@
 import { Plugins } from '@capacitor/core'
+import { Share } from '@capacitor/share'
 import i18n from '../../i18n'
 import Draughtsground from '../../draughtsground/Draughtsground'
 import router from '../../router'
@@ -194,7 +195,7 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
   public sharePDN = () => {
     this.replay.pdn(this.white(), this.black())
     .then((data: draughts.PdnDumpResponse) =>
-      Plugins.LiShare.share({ text: data.pdn })
+      Share.share({ text: data.pdn })
     )
   }
 

src/ui/shared/offlineRound/view.tsx
@@ -1,4 +1,4 @@
-import { Plugins } from '@capacitor/core'
+import { Toast } from '@capacitor/toast'
 import h from 'mithril/hyperscript'
 import i18n from '../../../i18n'
 import * as gameApi from '../../../lidraughts/game'
@@ -59,13 +59,13 @@ export function renderGameActionsBar(ctrl: OfflineRoundInterface) {
       <button className="action_bar_button fa fa-plus-circle"
         oncreate={helper.ontap(
           ctrl.newGameMenu.open,
-          () => Plugins.LiToast.show({ text: i18n('createAGame'), duration: 'short', position: 'bottom' })
+          () => Toast.show({ text: i18n('createAGame'), duration: 'short', position: 'bottom' })
         )}
       />
       <button className="fa fa-share-alt action_bar_button"
         oncreate={helper.ontap(
           ctrl.sharePDN,
-          () => Plugins.LiToast.show({ text: i18n('sharePDN'), duration: 'short', position: 'bottom' })
+          () => Toast.show({ text: i18n('sharePDN'), duration: 'short', position: 'bottom' })
         )}
       />
       <button className="action_bar_button" data-icon="A"