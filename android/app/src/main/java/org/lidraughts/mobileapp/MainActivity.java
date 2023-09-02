vovagorodok/blidraughts

WIP on capacitor3 android

android/app/src/main/java/org/lichess/mobileapp/MainActivity.java
@@ -9,24 +9,18 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.PatternMatcher;
 import android.util.Log;
 import android.webkit.WebView;
 
+import com.capacitorjs.plugins.dialog.Dialog;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Dialogs;
-import com.getcapacitor.Plugin;
 import com.getcapacitor.community.keepawake.KeepAwake;
-import com.getcapacitor.plugin.Storage;
 
-import org.lichess.plugin.SoundEffect;
 import org.lichess.mobileapp.stockfish.Stockfish;
 import org.lichess.mobileapp.stockfish.StockfishVariants;
-import org.lichess.mobileapp.BuildConfig;
 
 import java.io.File;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class MainActivity extends BridgeActivity {
@@ -38,15 +32,10 @@ public class MainActivity extends BridgeActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      add(LiBuildConfig.class);
-      add(SoundEffect.class);
-      add(KeepAwake.class);
-      add(Stockfish.class);
-      add(StockfishVariants.class);
-    }});
+    registerPlugin(LiBuildConfig.class);
+    registerPlugin(KeepAwake.class);
+    registerPlugin(Stockfish.class);
+    registerPlugin(StockfishVariants.class);
 
     this.versionUpdateActions();
 
@@ -67,7 +56,7 @@ public class MainActivity extends BridgeActivity {
           String message = "lichess needs a recent version of the rendering engine which is provided by" +
                   " the '" + appName   + "' application. The version you're using is too old ("
                   + pInfo.versionName + "). Please update it or lichess will not work.";
-          Dialogs.confirm(this, message, title, "OK", "Cancel", new Dialogs.OnResultListener() {
+          Dialog.confirm(this, message, title, "OK", "Cancel", new Dialog.OnResultListener() {
             @Override
             public void onResult(boolean value, boolean didCancel, String inputValue) {
               if (!didCancel) {

RoepStoep/lidrobile

android/app/src/main/java/org/lidraughts/mobileapp/MainActivity.java
@@ -9,23 +9,17 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.PatternMatcher;
 import android.util.Log;
 import android.webkit.WebView;
 
+import com.capacitorjs.plugins.dialog.Dialog;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Dialogs;
-import com.getcapacitor.Plugin;
 import com.getcapacitor.community.keepawake.KeepAwake;
-import com.getcapacitor.plugin.Storage;
 
-import org.lichess.plugin.SoundEffect;
 import org.lidraughts.mobileapp.scan.Scan;
-import org.lidraughts.mobileapp.BuildConfig;
 
 import java.io.File;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class MainActivity extends BridgeActivity {
@@ -37,13 +31,8 @@ public class MainActivity extends BridgeActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      add(SoundEffect.class);
-      add(KeepAwake.class);
-      add(Scan.class);
-    }});
+    registerPlugin(KeepAwake.class);
+    registerPlugin(Scan.class);
 
     this.versionUpdateActions();
 
@@ -64,7 +53,7 @@ public class MainActivity extends BridgeActivity {
           String message = "lidraughts needs a recent version of the rendering engine which is provided by" +
                   " the '" + appName   + "' application. The version you're using is too old ("
                   + pInfo.versionName + "). Please update it or lidraughts will not work.";
-          Dialogs.confirm(this, message, title, "OK", "Cancel", new Dialogs.OnResultListener() {
+          Dialog.confirm(this, message, title, "OK", "Cancel", new Dialog.OnResultListener() {
             @Override
             public void onResult(boolean value, boolean didCancel, String inputValue) {
               if (!didCancel) {