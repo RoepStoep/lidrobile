vovagorodok/blidraughts

Switch to rich player search result API

src/ui/players/PlayersCtrl.ts
@@ -2,14 +2,14 @@ import debounce from 'lodash-es/debounce'
 import redraw from '../../utils/redraw'
 import * as utils from '../../utils'
 import router from '../../router'
-import { User, Rankings } from '../../lichess/interfaces/user'
+import { User, Rankings, SearchUser } from '../../lichess/interfaces/user'
 import * as xhr from './playerXhr'
 
 export default class PlayersCtrl {
   public currentTab: number
 
   public isSearchOpen = false
-  public searchResults: readonly string[] = []
+  public searchResults: readonly SearchUser[] = []
   public players?: readonly User[]
   public leaderboard?: Rankings
 

src/ui/players/playerXhr.ts
@@ -1,10 +1,13 @@
 import { fetchJSON } from '../../http'
-import { User, Rankings } from '../../lichess/interfaces/user'
+import { User, Rankings, SearchUser } from '../../lichess/interfaces/user'
 
-export function autocomplete(term: string): Promise<ReadonlyArray<string>> {
-  return fetchJSON('/player/autocomplete?friend=1', {
+type SearchResult = ReadonlyArray<SearchUser>
+
+export async function autocomplete(term: string): Promise<SearchResult> {
+  const data = await fetchJSON<{result: SearchResult}>('/player/autocomplete?friend=1&object=1', {
     query: { term },
   })
+  return data.result
 }
 
 export function onlinePlayers(): Promise<ReadonlyArray<User>> {

RoepStoep/lidrobile

src/ui/players/PlayersCtrl.ts
@@ -2,14 +2,14 @@ import debounce from 'lodash-es/debounce'
 import redraw from '../../utils/redraw'
 import * as utils from '../../utils'
 import router from '../../router'
-import { User, Rankings } from '../../lidraughts/interfaces/user'
+import { User, Rankings, SearchUser } from '../../lidraughts/interfaces/user'
 import * as xhr from './playerXhr'
 
 export default class PlayersCtrl {
   public currentTab: number
 
   public isSearchOpen = false
-  public searchResults: readonly string[] = []
+  public searchResults: readonly SearchUser[] = []
   public players?: readonly User[]
   public leaderboard?: Rankings
 

src/ui/players/playerXhr.ts
@@ -1,10 +1,13 @@
 import { fetchJSON } from '../../http'
-import { User, Rankings } from '../../lidraughts/interfaces/user'
+import { User, Rankings, SearchUser } from '../../lidraughts/interfaces/user'
 
-export function autocomplete(term: string): Promise<ReadonlyArray<string>> {
-  return fetchJSON('/player/autocomplete?friend=1', {
+type SearchResult = ReadonlyArray<SearchUser>
+
+export async function autocomplete(term: string): Promise<SearchResult> {
+  const data = await fetchJSON<{result: SearchResult}>('/player/autocomplete?friend=1&object=1', {
     query: { term },
   })
+  return data.result
 }
 
 export function onlinePlayers(): Promise<ReadonlyArray<User>> {