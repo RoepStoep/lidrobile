vovagorodok/blidraughts

Refactor stockfish, restore getMaxMemory

src/main.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@capacitor/splash-screen'
 
 import appInit from './app'
 import { init as settingsInit } from './settings'
+import { StockfishVariants } from './stockfish'
 import { init as i18nInit } from './i18n'
 import { init as themeInit } from './theme'
 import routes from './routes'
@@ -35,9 +36,7 @@ settingsInit()
   Capacitor.getPlatform() === 'ios' ?
     CPUInfo.nbCores().then((r: { value: number }) => r.value).catch(() => 1) :
     Promise.resolve((<XNavigator>navigator).hardwareConcurrency || 1),
-  // TODO migrate
-  // Plugins.StockfishVariants.getMaxMemory().then((r: { value: number }) => r.value).catch(() => 16),
-  Promise.resolve(16),
+  StockfishVariants.getMaxMemory().then((r: { value: number }) => r.value).catch(() => 16),
   Capacitor.getPlatform() === 'android' ?
     LiBuildConfig.get() : Promise.resolve({
       NNUE: false

src/stockfish/index.ts
@@ -1,9 +1,9 @@
 import { Capacitor, registerPlugin } from '@capacitor/core'
 import { Stockfish, StockfishPlugin as IStockfishPlugin } from 'capacitor-stockfish'
-import { VariantKey } from './lichess/interfaces/variant'
+import { VariantKey } from '../lichess/interfaces/variant'
 
-const StockfishVariants = registerPlugin<IStockfishPlugin>('StockfishVariants', {
-  web: () => import('./stockfishVariantsWeb').then(m => new m.StockfishVariantsWeb()),
+export const StockfishVariants = registerPlugin<IStockfishPlugin>('StockfishVariants', {
+  web: () => import('./StockfishVariantsWeb').then(m => new m.StockfishVariantsWeb()),
 })
 
 export class StockfishPlugin {

RoepStoep/lidrobile

src/main.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@capacitor/splash-screen'
 
 import appInit from './app'
 import { init as settingsInit } from './settings'
+import { Scan } from './scan'
 import { init as i18nInit } from './i18n'
 import { init as themeInit } from './theme'
 import routes from './routes'
@@ -31,9 +32,7 @@ settingsInit()
   Capacitor.getPlatform() === 'ios' ?
     CPUInfo.nbCores().then((r: { value: number }) => r.value).catch(() => 1) :
     Promise.resolve((<XNavigator>navigator).hardwareConcurrency || 1),
-  // TODO migrate
-  // Plugins.Scan.getMaxMemory().then((r: { value: number }) => r.value).catch(() => 16),
-  Promise.resolve(16)
+  Scan.getMaxMemory().then((r: { value: number }) => r.value).catch(() => 16),
 ]))
 .then(([ai, di, did, c, m]) => appInit(ai, di, did, c, m))
 .then(() => {

src/scan/index.ts
@@ -1,9 +1,9 @@
-import { Capacitor, registerPlugin } from '@capacitor/core'
-import { Scan, ScanPlugin as IScanPlugin } from 'capacitor-scan'
-import { VariantKey } from './lidraughts/interfaces/variant'
+import { registerPlugin } from '@capacitor/core'
+import { ScanPlugin as IScanPlugin } from 'capacitor-scan'
+import { VariantKey } from '../lidraughts/interfaces/variant'
 
-const ScanWeb = registerPlugin<IScanPlugin>('Scan', {
-  web: () => import('./stockfishVariantsWeb').then(m => new m.StockfishVariantsWeb()),
+export const Scan = registerPlugin<IScanPlugin>('Scan', {
+  web: () => import('./ScanWeb').then(m => new m.ScanWeb()),
 })
 
 export class ScanPlugin {
@@ -11,7 +11,7 @@ export class ScanPlugin {
   public variant: VariantKey
 
   constructor(readonly v: VariantKey) {
-    this.plugin = Capacitor.getPlatform() !== 'web' ? Scan : ScanWeb
+    this.plugin = Scan
     this.variant = v
   }
 