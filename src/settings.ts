vovagorodok/blidraughts

add Blindfold Chess mode

src/settings.ts
@@ -135,6 +135,7 @@ export default {
     magnified: prop<boolean>('pieceMagnified', true),
     pieceNotation: prop<boolean>('pieceNotation', true),
     zenMode: prop<boolean>('zenMode', false),
+    blindfoldChess: prop<boolean>('blindfoldChess', false),
     clockPosition: prop<'right' | 'left'>('game.inversedClockPos', 'right'),
     pieceMove: prop<'tap' | 'drag' | 'both'>('game.pieceMove', 'both'),
     rookCastle: prop<0 | 1>('game.rookCastle', 1),

src/ui/settings/gameDisplay.ts
@@ -59,6 +59,9 @@ function renderBody() {
       ),
       h('li.list_item', [
         formWidgets.renderMultipleChoiceButton(i18n('zenMode'), formWidgets.booleanChoice, settings.game.zenMode),
+      ]),
+      h('li.list_item', [
+        formWidgets.renderMultipleChoiceButton(i18n('blindfoldChess'), formWidgets.booleanChoice, settings.game.blindfoldChess),
       ])
    ])
   ]

src/ui/shared/Board.tsx
@@ -20,6 +20,7 @@ interface State {
   boardOnRemove(): void
   boardTheme: string
   pieceTheme: string
+  blindfoldChess: boolean
   shapesCleared: boolean
   bounds?: ClientRect
   onResize: () => void
@@ -57,6 +58,7 @@ export default {
     this.shapesCleared = false
     this.pieceTheme = settings.general.theme.piece()
     this.boardTheme = settings.general.theme.board()
+    this.blindfoldChess = settings.game.blindfoldChess()
   },
 
   onbeforeupdate({ attrs }, { attrs: oldattrs }) {
@@ -76,6 +78,7 @@ export default {
       'orientation-' + chessground.state.orientation,
       `board-${this.boardTheme}`,
       customPieceTheme || this.pieceTheme,
+      `blindfold-${this.blindfoldChess}`,
       variant
     ].join(' ')
 

RoepStoep/lidrobile

src/settings.ts
@@ -113,6 +113,7 @@ export default {
     coordSystem: prop<number>('coordSystem', 0),
     magnified: prop<boolean>('pieceMagnified', true),
     zenMode: prop<boolean>('zenMode', false),
+    blindfoldDraughts: prop<boolean>('blindfoldDraughts', false),
     kingMoves: prop<boolean>('kingMoves', true),
     draughtsResult: prop<boolean>('draughtsResult', true),
     clockPosition: prop<'right' | 'left'>('game.inversedClockPos', 'right'),

src/ui/settings/gameDisplay.ts
@@ -76,6 +76,9 @@ function renderBody() {
       ),
       h('li.list_item', [
         formWidgets.renderMultipleChoiceButton(i18n('zenMode'), formWidgets.booleanChoice, settings.game.zenMode),
+      ]),
+      h('li.list_item', [
+        formWidgets.renderMultipleChoiceButton(i18n('blindfoldDraughts'), formWidgets.booleanChoice, settings.game.blindfoldDraughts),
       ])
    ])
   ]

src/ui/shared/Board.tsx
@@ -21,6 +21,7 @@ interface State {
   boardOnRemove(): void
   boardTheme: string
   pieceTheme: string
+  blindfoldDraughts: boolean
   shapesCleared: boolean
   bounds?: ClientRect
   onResize: () => void
@@ -58,6 +59,7 @@ export default {
     this.shapesCleared = false
     this.pieceTheme = settings.general.theme.piece()
     this.boardTheme = settings.general.theme.board()
+    this.blindfoldDraughts = settings.game.blindfoldDraughts()
   },
 
   onbeforeupdate({ attrs }, { attrs: oldattrs }) {
@@ -77,6 +79,7 @@ export default {
       'orientation-' + draughtsground.state.orientation,
       `board-${this.boardTheme}`,
       customPieceTheme || this.pieceTheme,
+      `blindfold-${this.blindfoldDraughts}`,
       variant,
       'is' + docVariant.board.key
     ].join(' ')